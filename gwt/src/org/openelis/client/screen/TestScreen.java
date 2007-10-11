3015cfb6470aba5bda2c61da51557c917685b32d
Travis Schmidt <travis.schmidt@gmail.com>
2007-10-11 16:47:59 +0000

    Initial Commit of OpenELIS-GWT project.  Contains only one dummy screen to test setup

---
+++ gwt/src/org/openelis/client/screen/TestScreen.java
@@ -0,0 +1,17 @@
+package org.openelis.client.screen;
+
+import org.openelis.gwt.client.screen.ScreenForm;
+
+public class TestScreen extends ScreenForm {
+    
+    public TestScreen() {
+        super("TestScreen.xml");
+        rpc.action = "TestScreen";
+    }
+    
+    public void afterSubmit(String method, boolean Success) {
+        if(method.equals("draw"))
+            message.setText("Done");
+    }
+
+}
