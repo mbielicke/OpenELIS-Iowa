96f35631fed054dcd325c32edaab3fd2af89202d
tschmidt@uhl.uiowa.edu <tschmidt@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2007-10-19 13:13:34 +0000

    Setup Application for internationalization and entered an example in the test screen

---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/client/main/constants/OpenELISConstants.java
@@ -0,0 +1,7 @@
+package org.openelis.client.main.constants;
+
+import com.google.gwt.i18n.client.ConstantsWithLookup;
+
+public interface OpenELISConstants extends ConstantsWithLookup {
+    String greeting();
+}
