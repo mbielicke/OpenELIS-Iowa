f78f0bf3860680962bad862035434de615afd533
Travis Schmidt <travis.schmidt@gmail.com>
2007-10-30 21:20:43 +0000

    Changes for AppServiceInt

--- gwt/src/org/openelis/client/main/service/OpenELISService.java
+++ gwt/src/org/openelis/client/main/service/OpenELISService.java
@@ -3,18 +3,28 @@ package org.openelis.client.main.service;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
-public class OpenELISService {
-    private static OpenELISServiceIntAsync ttService = (OpenELISServiceIntAsync)GWT.create(OpenELISServiceInt.class);
+import org.openelis.gwt.client.services.AppServiceInt;
+import org.openelis.gwt.client.services.AppServiceIntAsync;
 
-    private static ServiceDefTarget target = (ServiceDefTarget)ttService;
+public class OpenELISService {
+    private static OpenELISServiceIntAsync OpenElisService = (OpenELISServiceIntAsync)GWT.create(OpenELISServiceInt.class);
+    private static AppServiceIntAsync AppElisService = (AppServiceIntAsync)GWT.create(AppServiceInt.class);
+    
+    private static ServiceDefTarget targetOpen = (ServiceDefTarget)OpenElisService;
+    private static ServiceDefTarget targetApp = (ServiceDefTarget)AppElisService;
 
     public static void init() {
         String url = GWT.getModuleBaseURL();
         url += "/OpenELISService";
-        target.setServiceEntryPoint(url);
+        targetOpen.setServiceEntryPoint(url);
+        targetApp.setServiceEntryPoint(url);
     }
 
     public static OpenELISServiceIntAsync getInstance() {
-        return ttService;
+        return OpenElisService;
+    }
+    
+    public static AppServiceIntAsync getAppServInstance() {
+        return AppElisService;
     }
 }
