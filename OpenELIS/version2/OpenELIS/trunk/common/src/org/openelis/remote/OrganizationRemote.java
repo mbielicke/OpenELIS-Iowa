2a6f6c0377206c364c5ca6c781853d1b54928e83
tbrady@uhl.uiowa.edu <tbrady@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2007-11-20 19:52:32 +0000

    coded the update method

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/OrganizationRemote.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/OrganizationRemote.java
@@ -5,8 +5,6 @@ import javax.ejb.Remote;
 
 import org.openelis.domain.NoteDO;
 import org.openelis.domain.OrganizationAddressDO;
-import org.openelis.domain.OrganizationContactDO;
-import org.openelis.domain.OrganizationTableRowDO;
 
 @Remote
 public interface OrganizationRemote {
@@ -18,10 +16,16 @@ public interface OrganizationRemote {
 	public List getOrganizationNameListByLetter(String letter, int startPos, int maxResults);
 	
 	//method to return org name, address
-	public OrganizationAddressDO getOrganizationAddress(Integer organizationId);
+	public OrganizationAddressDO getOrganizationAddress(Integer organizationId, boolean unlock);
+	
+	//update initial call for org
+	public OrganizationAddressDO getOrganizationAddressUpdate(Integer id) throws Exception;
+	
+	//commit a change to org, or insert a new org
+	public Integer updateOrganization(OrganizationAddressDO organizationDO, NoteDO noteDO, List contacts);
 	
 	//method to return just notes
-	public List getOrganizationNotes(Integer organizationId);
+	public List getOrganizationNotes(Integer organizationId, boolean topLevel);
 	
 	//method to return just contacts
 	public List getOrganizationContacts(Integer organizationId);
