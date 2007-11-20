a28898587fd5681ea38ed61a62c7c72ff7d18dfb
tbrady@uhl.uiowa.edu <tbrady@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2007-11-20 20:01:12 +0000

    initial

---
+++ OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/bean/HistoryBean.java
@@ -0,0 +1,84 @@
+package org.openelis.bean;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+import org.openelis.entity.History;
+import org.openelis.local.HistoryLocal;
+import org.openelis.utils.Auditable;
+
+import edu.uiowa.uhl.security.domain.SystemUserDO;
+import edu.uiowa.uhl.security.local.SystemUserUtilLocal;
+
+@Stateless
+public class HistoryBean implements HistoryLocal {
+
+	    @Resource
+	    SessionContext ctx;
+
+	    @PersistenceContext(unitName = "openelis")
+	    EntityManager manager;
+
+	    @EJB
+	    private SystemUserUtilLocal sysUser;
+
+	    private Logger log = Logger.getLogger(this.getClass());
+
+	    public void write(Auditable aud, String operation) {
+	        try {
+	            log.debug("Entering write");
+	            String change = null;
+	            if (operation.equals("U")) {
+	                change = aud.getChangeXML();
+	                log.debug("Changes : " + change);
+	            }
+	            History history = new History();
+	            history.setReferenceId(aud.getId());
+	            Query query = manager.createNamedQuery("getTableId");
+	            query.setParameter("name", aud.getTableName());
+	            history.setReferenceTable((Integer)query.getSingleResult());
+	            
+	            //FIXME activity will be a dictionary entry eventually
+	            history.setActivity(1);
+	            history.setTimestamp(new Date());
+	            try{
+	                String princ = ctx.getCallerPrincipal().getName();
+	                if(princ.equals(""))
+	                    history.setSystemUser(0);
+	                else
+	                    history.setSystemUser(getSystemUserId());
+	            }catch(Exception e){
+	                history.setSystemUser(0);
+	            }
+	            if (change != null)
+	                history.setChanges(change);
+	            manager.persist(history);
+	            log.debug("Exiting write");
+	        } catch (Exception e) {
+	            log.error(e.getMessage());
+	            e.printStackTrace();
+	        }
+	    }
+
+	    private Integer getSystemUserId(){
+	        log.debug(ctx.getCallerPrincipal().getName()+" in LockBean "+ctx.toString());
+	            try {
+	                SystemUserDO systemUserDO = sysUser.getSystemUser(ctx.getCallerPrincipal()
+	                                                                     .getName());
+	                return systemUserDO.getId();
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	                return null;
+	            } finally {
+	            }
+	            
+	        }
+}
