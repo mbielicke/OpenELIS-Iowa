f78c00aa1ddd4a29d196dfe506c609e9e720b5ab
tbrady <tbrady@uhl.uiowa.edu>
2007-11-20 20:01:56 +0000

    added named query

--- ejb/src/org/openelis/entity/ReferenceTable.java
+++ ejb/src/org/openelis/entity/ReferenceTable.java
@@ -5,22 +5,23 @@ package org.openelis.entity;
   * ReferenceTable Entity POJO for database 
   */
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.openelis.util.Datetime;
-import org.openelis.util.XMLUtil;
-
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import org.openelis.util.XMLUtil;
 import org.openelis.utils.AuditUtil;
 import org.openelis.utils.Auditable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+@NamedQueries( {@NamedQuery(name = "getTableId", query = "select id from ReferenceTable where name = :name")})
 @Entity
 @Table(name="reference_table")
 @EntityListeners({AuditUtil.class})
