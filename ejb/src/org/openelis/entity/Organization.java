d7ba8b9194e7a0878c31e0ccc9df98958a2b2227
tbrady <tbrady@uhl.uiowa.edu>
2007-11-20 20:00:33 +0000

    added named queries

--- ejb/src/org/openelis/entity/Organization.java
+++ ejb/src/org/openelis/entity/Organization.java
@@ -5,22 +5,47 @@ package org.openelis.entity;
   * Organization Entity POJO for database 
   */
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.openelis.util.Datetime;
-import org.openelis.util.XMLUtil;
 
-import java.util.Date;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import org.openelis.util.XMLUtil;
 import org.openelis.utils.AuditUtil;
 import org.openelis.utils.Auditable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+
+
+@NamedQueries({@NamedQuery(name = "getOrganizationNameRows", query = "select	new org.openelis.domain.OrganizationTableRowDO(id,name) " + "from Organization order by name"),
+	           @NamedQuery(name = "getOrganizationNameRowsByLetter", query = "select	new org.openelis.domain.OrganizationTableRowDO(o.id,o.name) " + "from Organization o where o.name like :letter order by name"),
+	           @NamedQuery(name = "getOrganizationAndAddress", query = "select new org.openelis.domain.OrganizationAddressDO(orgz.id,orgz.parentOrganization,pOrg.name,orgz.name,orgz.isActive,addr.id," +
+	           		              "addr.referenceId,addr.referenceTable," +
+			   		              "addr.type,addr.multipleUnit,addr.streetAddress,addr.city,addr.state,addr.zipCode,addr.workPhone,addr.homePhone,addr.cellPhone,addr.faxPhone,addr.email,addr.country)" +                                                                              
+			                      "  from Organization orgz left join orgz.parentOrg porg, Address addr where addr.id = orgz.address and orgz.id = :id"),
+			   @NamedQuery(name = "getOrganizationContacts", query = "select new org.openelis.domain.OrganizationContactDO(contact.id,contact.organization,contact.contactType,contact.name,addr.id," +
+	           		              "addr.referenceId,addr.referenceTable," +
+			   		              "addr.type,addr.multipleUnit,addr.streetAddress,addr.city,addr.state,addr.zipCode,addr.workPhone,addr.homePhone,addr.cellPhone,addr.faxPhone,addr.email,addr.country)" +
+						           "  from OrganizationContact contact, Organization orgz, Address addr where addr.id = contact.address and " +
+						           " orgz.id = contact.organization and orgz.id = :id"),
+			   @NamedQuery(name = "getOrganizationNotesTopLevel", query = "select n.id, n.timestamp, n.subject " + 
+					   "  from Note n where n.referenceTable = (select id from ReferenceTable where name='organization') and n.referenceId = :id"),
+			   @NamedQuery(name = "getOrganizationNotesSecondLevel", query = "select n.id, n.systemUser, n.text " +
+					   "  from Note n where n.referenceTable = (select id from ReferenceTable where name='organization') and n.id = :id")})
+                                                                  
 @Entity
 @Table(name="organization")
 @EntityListeners({AuditUtil.class})
@@ -43,6 +68,28 @@ public class Organization implements Auditable, Cloneable {
   @Column(name="address")
   private Integer address;             
 
+  @OneToMany(fetch = FetchType.LAZY)
+  @JoinColumn(name = "organization")
+  private Collection<OrganizationContact> organizationContact;
+//  @OneToOne(fetch = FetchType.LAZY)
+//@JoinColumn(name = "reference_id", insertable = true, updatable = true)
+//  private Address address;
+  
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_organization", insertable = false, updatable = false)
+  private Organization parentOrg;
+  
+  public Collection<OrganizationContact> getOrganizationContact() {
+	return organizationContact;
+  }
+
+  public void setOrganizationContact(Collection<OrganizationContact> organizationContact) {
+	this.organizationContact = organizationContact;
+  }
+  
+//  @OneToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "reference_table", insertable = false, updatable = false)
+//  private OrganizationContact referenceTable;
 
   @Transient
   private Organization original;
@@ -51,6 +98,7 @@ public class Organization implements Auditable, Cloneable {
   public Integer getId() {
     return id;
   }
+  
   protected void setId(Integer id) {
     if((id == null && this.id != null) || 
        (id != null && !id.equals(this.id)))
@@ -120,9 +168,9 @@ public class Organization implements Auditable, Cloneable {
       }      
 
       if((name == null && original.name != null) || 
-         (name != null && !name.equals(original.name))){
+         (name != null && !name.equals(original.name.trim()))){
         Element elem = doc.createElement("name");
-        elem.appendChild(doc.createTextNode(original.name.toString()));
+        elem.appendChild(doc.createTextNode(original.name.toString().trim()));
         root.appendChild(elem);
       }      
 
@@ -131,7 +179,14 @@ public class Organization implements Auditable, Cloneable {
         Element elem = doc.createElement("is_active");
         elem.appendChild(doc.createTextNode(original.isActive.toString()));
         root.appendChild(elem);
-      }      
+      }
+      
+      if((address == null && original.address != null) || 
+    		  (address != null && !address.equals(original.address))){
+    	  Element elem = doc.createElement("address");
+          elem.appendChild(doc.createTextNode(original.address.toString()));
+          root.appendChild(elem);    	  
+      }
 
       if((address == null && original.address != null) || 
          (address != null && !address.equals(original.address))){
@@ -151,5 +206,5 @@ public class Organization implements Auditable, Cloneable {
   public String getTableName() {
     return "organization";
   }
-  
+
 }   
