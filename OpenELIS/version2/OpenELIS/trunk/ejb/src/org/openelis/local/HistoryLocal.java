8d9105d689aab69c13aa038acb4cd54048c4421d
tbrady@uhl.uiowa.edu <tbrady@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2007-12-03 22:22:43 +0000

    history bean no longer inserts null records. It also recieves its operation and changexml from the auditUtil class now

--- OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/local/HistoryLocal.java
+++ OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/local/HistoryLocal.java
@@ -7,5 +7,5 @@ import org.openelis.utils.Auditable;
 @Local
 public interface HistoryLocal {
 
-    public void write(Auditable aud, String operation);
+    public void write(Auditable aud, Integer operation, String changes);
 }
