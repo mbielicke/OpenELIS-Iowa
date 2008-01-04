48429d60fc325475df621b4920c1cc0faa9801bb
akampoow@uhl.uiowa.edu <akampoow@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-01-04 16:24:58 +0000

    initial  commit

---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/client/dataEntry/screen/dictionary/DictionaryServletInt.java
@@ -0,0 +1,7 @@
+package org.openelis.client.dataEntry.screen.dictionary;
+
+import org.openelis.gwt.client.services.AppScreenFormServiceInt;
+
+public interface DictionaryServletInt extends AppScreenFormServiceInt {
+
+}
