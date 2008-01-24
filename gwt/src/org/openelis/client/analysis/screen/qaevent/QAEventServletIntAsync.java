10762fae971654ad1afa9ad05e0bc89fd5180276
akampoow <akampoow@gmail.com>
2008-01-24 22:46:09 +0000

    initial

---
+++ gwt/src/org/openelis/client/analysis/screen/qaevent/QAEventServletIntAsync.java
@@ -0,0 +1,7 @@
+package org.openelis.client.analysis.screen.qaevent;
+
+import org.openelis.gwt.client.services.AppScreenFormServiceIntAsync;
+
+public interface QAEventServletIntAsync extends AppScreenFormServiceIntAsync{
+
+}
