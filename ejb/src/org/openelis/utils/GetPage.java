0a7ff1619391845039e5f8aec13ea37d7430d91d
tbrady <tbrady@uhl.uiowa.edu>
2008-01-28 21:50:44 +0000

    if size is 0 we dont want to throw the exception

--- ejb/src/org/openelis/utils/GetPage.java
+++ ejb/src/org/openelis/utils/GetPage.java
@@ -7,6 +7,8 @@ public class GetPage {
 
 	public static List getPage(List resultList, int first, int max){
 		List returnList = new ArrayList();
+		if(resultList.size() == 0)
+			return returnList;
 		
 		if(first > resultList.size()-1)
 			return null;
