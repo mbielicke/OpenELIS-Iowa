8e0e08cf9018a76ac6f1652ccbc2e371bf37203c
akampoow <akampoow@gmail.com>
2008-01-28 16:28:41 +0000

    initial

---
+++ common/src/org/openelis/remote/QaEventRemote.java
@@ -0,0 +1,36 @@
+package org.openelis.remote;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import org.openelis.domain.QaEventDO;
+
+@Remote
+public interface QaEventRemote {
+    // method to return list of QaEvent ids and QaEvent names by the letter they start with
+    public List getQaEventNameListByLetter(String letter, int startPos, int maxResults);
+   
+    // method to return QaEvent 
+    public QaEventDO getQaEvent(Integer qaEventId, boolean unlock);
+    
+    //  update initial call for QaEvent
+    public QaEventDO getQaEventUpdate(Integer id) throws Exception;
+     
+    //  commit a change to QaEvent, or insert a new provider
+    public Integer updateQaEvent(QaEventDO qaEventDO);
+    
+    //  method to query for QaEvent
+    public List query(HashMap fields, int first, int max) throws Exception;
+    
+    //a way for the servlet to get the system user id
+    public Integer getSystemUserId();
+    
+    //method to get all the dictionary entries for QaEvents
+    public List<Object[]> getQaEventTypes();
+    
+    //method to get all the tests for a given QaEvent
+    public List<Object[]> getTests(Integer Id);
+    
+}
