8182b3d9fd7e25b18597ce838794caff8c256155
akampoow@uhl.uiowa.edu <akampoow@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-01-30 20:00:28 +0000

    initial commit of QAEventsNamesTable

--- OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/client/analysis/screen/qaevent/QAEventServletIntAsync.java
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/client/analysis/screen/qaevent/QAEventServletIntAsync.java
@@ -2,6 +2,8 @@ package org.openelis.client.analysis.screen.qaevent;
 
 import org.openelis.gwt.client.services.AppScreenFormServiceIntAsync;
 
-public interface QAEventServletIntAsync extends AppScreenFormServiceIntAsync{
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
+public interface QAEventServletIntAsync extends AppScreenFormServiceIntAsync{
+    public void getInitialModel(String cat, AsyncCallback callback);
 }
