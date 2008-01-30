7e65aca6645ed3001d4cc5afef346679d41138d7
akampoow <akampoow@gmail.com>
2008-01-30 20:00:28 +0000

    initial commit of QAEventsNamesTable

--- gwt/src/org/openelis/client/analysis/screen/qaevent/QAEventServletInt.java
+++ gwt/src/org/openelis/client/analysis/screen/qaevent/QAEventServletInt.java
@@ -1,8 +1,9 @@
 package org.openelis.client.analysis.screen.qaevent;
 
 import org.openelis.gwt.client.services.AppScreenFormServiceInt;
+import org.openelis.gwt.common.data.DataModel;
 
 public interface QAEventServletInt extends AppScreenFormServiceInt {
-    
+    public DataModel getInitialModel(String cat);
 
 }
