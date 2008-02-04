656a7d8d5cacd973ecbd8ecc3f4e027df09152e9
tbrady@uhl.uiowa.edu <tbrady@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-02-04 14:46:06 +0000

    initial

---
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/StandardNoteRemote.java
@@ -0,0 +1,25 @@
+package org.openelis.remote;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import org.openelis.domain.StandardNoteDO;
+
+@Remote
+public interface StandardNoteRemote {
+	//commit a change to a standard note, or insert a new standard note
+	public Integer updateStandardNote(StandardNoteDO standardNoteDO);
+	
+	//method to return a whole standard note
+	public StandardNoteDO getStandardNote(Integer standardNoteId, boolean unlock);
+	
+	 //method to query for standard notes
+	 public List query(HashMap fields, int first, int max) throws Exception;
+	 
+	 //a way for the servlet to get the system user id
+	 public Integer getSystemUserId();
+	 
+	 public void deleteStandardNote(Integer standardNoteId) throws Exception;
+}
