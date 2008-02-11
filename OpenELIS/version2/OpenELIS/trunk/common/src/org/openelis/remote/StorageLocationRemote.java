615d80f23ec220daf72caff9a2510ada2b68ce19
tbrady@uhl.uiowa.edu <tbrady@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-02-11 15:19:21 +0000

    added table to storage loc screen

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/StorageLocationRemote.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/StorageLocationRemote.java
@@ -10,10 +10,13 @@ import org.openelis.domain.StorageLocationDO;
 @Remote
 public interface StorageLocationRemote {
 //	commit a change to storage loc, or insert a new storage loc
-	public Integer updateStorageLoc(StorageLocationDO storageDO);
+	public Integer updateStorageLoc(StorageLocationDO storageDO, List storageLocationChildren);
 	
 	//method to return a whole storage loc
 	public StorageLocationDO getStorageLoc(Integer StorageId, boolean unlock);
+	
+	//method to return a child storage locs
+	public List getStorageLocChildren(Integer StorageId, boolean unlock);
 	
 	 //method to query for storage locs
 	 public List query(HashMap fields, int first, int max) throws Exception;
@@ -25,7 +28,7 @@ public interface StorageLocationRemote {
 	 public List autoCompleteLookupByName(String name, int maxResults);
 	 
 	 //auto complete lookup
-	 public List autoCompleteLookupById(Integer id);
+	 public Object[] autoCompleteLookupById(Integer id);
 	 
 	 public void deleteStorageLoc(Integer StorageLocId) throws Exception;
 }
