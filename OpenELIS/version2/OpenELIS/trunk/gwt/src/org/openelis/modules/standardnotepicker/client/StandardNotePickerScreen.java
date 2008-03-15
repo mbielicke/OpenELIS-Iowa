d9f37e2049f288f8577b8cf127a805f654125232
tschmidt@uhl.uiowa.edu <tschmidt@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-03-15 17:03:19 +0000

    Changes for Redesign

---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/standardnotepicker/client/StandardNotePickerScreen.java
@@ -0,0 +1,175 @@
+package org.openelis.modules.standardnotepicker.client;
+
+import org.openelis.gwt.common.FormRPC;
+import org.openelis.gwt.common.data.DataObject;
+import org.openelis.gwt.common.data.NumberObject;
+import org.openelis.gwt.common.data.PagedTreeField;
+import org.openelis.gwt.common.data.StringObject;
+import org.openelis.gwt.screen.ScreenAppButton;
+import org.openelis.gwt.screen.ScreenPagedTree;
+import org.openelis.gwt.screen.ScreenTextBox;
+import org.openelis.gwt.screen.ScreenVertical;
+import org.openelis.gwt.widget.AppButton;
+import org.openelis.gwt.widget.ButtonPanel;
+import org.openelis.modules.main.client.OpenELISScreenForm;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TreeItem;
+import com.google.gwt.user.client.ui.TreeListener;
+import com.google.gwt.user.client.ui.Widget;
+
+public class StandardNotePickerScreen extends OpenELISScreenForm implements TreeListener{
+
+	//ScreenWindow window;
+	public TextArea noteTextArea;
+	
+	public StandardNotePickerScreen(TextArea noteTextArea) {
+		super("org.openelis.modules.standardnote.server.StandardNotePickerService",false);
+		this.noteTextArea = noteTextArea;		
+        name="Standard Note Selection";
+	}
+	
+	public void onClick(Widget sender) {
+		String action = ((AppButton)sender).action;
+		if(action.equals("find")){
+			TextBox findTextBox = (TextBox)getWidget("findTextBox");
+			 String queryString = findTextBox.getText();
+			FormRPC queryRPC = (FormRPC) this.forms.get("queryByNameDescription");
+			queryRPC.setFieldValue("name", queryString);
+			queryRPC.setFieldValue("description", queryString);
+
+			StringObject name = new StringObject();
+	        StringObject desc = new StringObject();
+	        name.setValue(queryString);
+	        desc.setValue(queryString);
+	        
+	       // prepare the argument list for the getObject function
+	        DataObject[] args = new DataObject[] {name,desc}; 
+	        screenService.getObject("getTreeModel" , args, new AsyncCallback(){
+	            public void onSuccess(Object result){
+	            	ScreenVertical vp = (ScreenVertical) widgets.get("treeContainer");
+	            	ScreenPagedTree tree = (ScreenPagedTree)widgets.get("noteTree");
+	            	vp.clear();
+	            	vp.getPanel().add(tree);
+	                PagedTreeField treeField = (PagedTreeField)result;
+	                tree.load(treeField);
+	            }
+	            
+	            public void onFailure(Throwable caught){
+	                Window.alert(caught.getMessage());
+	            }
+	         });        
+		}
+		super.onClick(sender);
+	}
+	
+	public void afterDraw(boolean sucess) {
+        final ScreenPagedTree tree = (ScreenPagedTree)widgets.get("noteTree");
+        tree.controller.setTreeListener(this);
+        bpanel = (ButtonPanel) getWidget("buttons");
+        super.afterDraw(sucess);
+        
+        ScreenAppButton findButton = (ScreenAppButton)widgets.get("findButton");
+        findButton.enable(true);
+        ScreenTextBox findTextBox = (ScreenTextBox)widgets.get("findTextBox");
+        findTextBox.enable(true);
+        
+        final ScreenVertical vp = (ScreenVertical) widgets.get("treeContainer");
+        final HTML loadingHtml = new HTML();
+        loadingHtml.setHTML("<img src=\"Images/OSXspinnerGIF.gif\"> Loading...");
+        vp.clear();
+        vp.remove(tree);
+        vp.getPanel().add(loadingHtml);
+        //add(loadingHtml);
+        
+        message.setText("done");
+        
+        StringObject name = new StringObject();
+        StringObject desc = new StringObject();
+        name.setValue("");
+        desc.setValue("");
+        
+       // prepare the argument list for the getObject function
+        DataObject[] args = new DataObject[] {name,desc}; 
+        screenService.getObject("getTreeModel" , args, new AsyncCallback(){
+            public void onSuccess(Object result){
+            	vp.clear();
+            	vp.getPanel().add(tree);
+                PagedTreeField treeField = (PagedTreeField)result;
+                tree.load(treeField);
+            }
+            
+            public void onFailure(Throwable caught){
+                Window.alert(caught.getMessage());
+            }
+         });        
+	}
+	
+	public void onTreeItemSelected(TreeItem item) {
+		try{
+			int id = Integer.parseInt((String)item.getUserObject());
+			item.setState(!item.getState(), true);
+		
+		}catch(NumberFormatException e){
+			TextArea textArea = (TextArea)getWidget("noteText");
+			textArea.setText((String)item.getUserObject());
+		}
+	}
+	
+	public void onTreeItemStateChanged(TreeItem item) {
+		try{
+			Integer id = Integer.valueOf((String)item.getUserObject());
+			final TreeItem finalTreeItem = item;
+			finalTreeItem.removeItems();
+			
+			final ScreenPagedTree tree = (ScreenPagedTree)widgets.get("noteTree");
+			final HTML loadingHtml = new HTML();
+	        loadingHtml.setHTML("<img src=\"Images/OSXspinnerGIF.gif\"> Loading...");
+			finalTreeItem.addItem(loadingHtml);
+			
+			FormRPC queryRPC = (FormRPC) this.forms.get("queryByNameDescription");
+
+			StringObject name = new StringObject();
+	        StringObject desc = new StringObject();
+	        name.setValue(queryRPC.getFieldValue("name"));
+	        desc.setValue(queryRPC.getFieldValue("description"));
+	        
+	        NumberObject idObj = new NumberObject();
+			idObj.setType("integer");
+			idObj.setValue(id);
+	        
+	       // prepare the argument list for the getObject function
+	        DataObject[] args = new DataObject[] {idObj,name,desc}; 
+	        
+            screenService.getObject("getTreeModelSecondLevel", args, new AsyncCallback(){
+	            public void onSuccess(Object result){
+	               finalTreeItem.removeItems();
+	               tree.controller.model.addTextChildItems(finalTreeItem, (String)((StringObject)result).getValue());	           
+	            }
+	            
+	            public void onFailure(Throwable caught){
+	            	finalTreeItem.removeItems();
+	                Window.alert(caught.getMessage());
+	            }
+	         });        
+		
+		}catch(NumberFormatException e){
+			//this means that it is the bottom level...
+		}
+	}
+	
+	public void abort(int state) {
+		window.close();
+	}
+	
+	public void commit(int state) {
+		TextArea textArea = (TextArea)getWidget("noteText");
+		noteTextArea.setText(noteTextArea.getText()+textArea.getText());
+		
+		window.close();
+	}
+}
---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/standardnotepicker/server/StandardNotePickerService.java
@@ -0,0 +1,178 @@
+package org.openelis.modules.standardnotepicker.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openelis.domain.StandardNoteDO;
+import org.openelis.gwt.common.FormRPC;
+import org.openelis.gwt.common.LastPageException;
+import org.openelis.gwt.common.QueryNotFoundException;
+import org.openelis.gwt.common.RPCException;
+import org.openelis.gwt.common.data.AbstractField;
+import org.openelis.gwt.common.data.DataModel;
+import org.openelis.gwt.common.data.DataObject;
+import org.openelis.gwt.common.data.DataSet;
+import org.openelis.gwt.common.data.ModelField;
+import org.openelis.gwt.common.data.NumberObject;
+import org.openelis.gwt.common.data.PagedTreeField;
+import org.openelis.gwt.common.data.QueryNumberField;
+import org.openelis.gwt.common.data.QueryStringField;
+import org.openelis.gwt.common.data.StringField;
+import org.openelis.gwt.common.data.StringObject;
+import org.openelis.gwt.server.ServiceUtils;
+import org.openelis.gwt.services.AppScreenFormServiceInt;
+import org.openelis.gwt.widget.pagedtree.TreeModel;
+import org.openelis.gwt.widget.pagedtree.TreeModelItem;
+import org.openelis.persistence.CachingManager;
+import org.openelis.persistence.EJBFactory;
+import org.openelis.remote.CategoryRemote;
+import org.openelis.remote.StandardNoteRemote;
+import org.openelis.server.constants.Constants;
+import org.openelis.util.SessionManager;
+import org.openelis.util.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class StandardNotePickerService implements AppScreenFormServiceInt {
+
+	private static final long serialVersionUID = -2489317407834940845L;
+
+	public FormRPC abort(DataSet key, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FormRPC commitAdd(FormRPC rpcSend, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FormRPC commitDelete(DataSet key, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public DataModel commitQuery(FormRPC rpcSend, DataModel model) throws RPCException {
+		return null;
+	}
+
+	public FormRPC commitUpdate(FormRPC rpcSend, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FormRPC fetch(DataSet key, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FormRPC fetchForUpdate(DataSet key, FormRPC rpcReturn) throws RPCException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getXML() throws RPCException {
+		return ServiceUtils.getXML(Constants.APP_ROOT+"/Forms/standardNotePicker.xsl");
+	}
+    
+    public DataObject[] getXMLData() throws RPCException {
+        StringObject xml = new StringObject();
+        xml.setValue(ServiceUtils.getXML(Constants.APP_ROOT+"/Forms/standardNotePicker.xsl"));
+        return new DataObject[] {xml};
+    }
+	
+	public PagedTreeField getTreeModel(StringObject name, StringObject desc) throws RPCException{
+		StandardNoteRemote remote = (StandardNoteRemote)EJBFactory.lookup("openelis/StandardNoteBean/remote");
+		PagedTreeField returnTree = new PagedTreeField();
+		TreeModel treeModel = new TreeModel();
+		
+		QueryStringField nameField = new QueryStringField();
+		QueryStringField descField = new QueryStringField();
+		nameField.setValue(name.getValue());
+		descField.setValue(desc.getValue());
+		
+		HashMap fields = new HashMap();
+		fields.put("name", nameField);
+		fields.put("description", descField);
+		
+		List standardNoteCategories = new ArrayList();
+		try{
+			standardNoteCategories = remote.queryForType(fields);
+
+		}catch(Exception e){
+			throw new RPCException(e.getMessage());
+		}
+		
+		Iterator catItr = standardNoteCategories.iterator();
+		
+		int i=0;
+		while(catItr.hasNext()){
+			Object[] result = (Object[])catItr.next();
+			//standard note category id
+			Integer idParam = (Integer)result[0];
+			//standard note category name
+			String nameParam = (String)result[1];
+			
+			TreeModelItem treeModelItem = new TreeModelItem();
+			treeModelItem.setText(nameParam);
+			treeModelItem.setUserObject(String.valueOf(idParam));
+			treeModelItem.setHasDummyChild(true);
+			treeModel.addItem(treeModelItem);
+			
+			i++;
+		}	
+		returnTree.setValue(treeModel);
+	    return returnTree;
+	}
+
+	public StringObject getTreeModelSecondLevel(NumberObject type, StringObject name, StringObject desc) throws RPCException{
+		StandardNoteRemote remote = (StandardNoteRemote)EJBFactory.lookup("openelis/StandardNoteBean/remote");
+		
+		QueryNumberField typeField = new QueryNumberField();
+		QueryStringField nameField = new QueryStringField();
+		QueryStringField descField = new QueryStringField();
+		typeField.setType("integer");
+		typeField.setValue(String.valueOf(((Integer)type.getValue()).intValue()));
+		nameField.setValue(name.getValue());
+		descField.setValue(desc.getValue());
+		HashMap<String,AbstractField> fields = new HashMap<String, AbstractField>();
+		fields.put("type", typeField);
+		fields.put("name", nameField);
+		fields.put("description", descField);
+		List list = new ArrayList();
+		try{
+			list = remote.getStandardNoteByType(fields);
+
+		}catch(Exception e){
+			throw new RPCException(e.getMessage());
+		}
+	       StringObject returnString = new StringObject(); 
+	     try {
+	    	 Iterator itr = list.iterator();
+	            
+	         Document doc = XMLUtil.createNew("tree");
+	         while(itr.hasNext()){
+				 StandardNoteDO standardNoteDO = (StandardNoteDO) itr.next();
+				 
+				 Element root = doc.getDocumentElement();
+				 root.setAttribute("key", "menuList");
+				 root.setAttribute("height", "100%");
+				 root.setAttribute("vertical","true");           
+				      
+				  Element elem = doc.createElement("label");
+				  elem.setAttribute("text", standardNoteDO.getName()+" : "+standardNoteDO.getDescription()); 
+				  elem.setAttribute("value", standardNoteDO.getText()); 
+				  
+				  root.appendChild(elem); 
+	         }
+			 
+	         returnString.setValue(XMLUtil.toString(doc));
+			 return returnString;
+		}catch(Exception e){
+			returnString.setValue("");
+			return returnString;
+		}
+	}
+}
