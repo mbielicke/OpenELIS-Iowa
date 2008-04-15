eba823f7ff198ea860592267563dd30523ef9d4d
akampoow@uhl.uiowa.edu <akampoow@uhl.uiowa.edu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2008-04-15 21:57:57 +0000

    included the number button for querying

--- OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/label/client/LabelScreen.java
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/label/client/LabelScreen.java
@@ -70,7 +70,10 @@ public class LabelScreen extends OpenELISScreenForm {
           if (state == FormInt.DISPLAY || state == FormInt.DEFAULT) {
 
               FormRPC letterRPC = (FormRPC) this.forms.get("queryByLetter");
-              letterRPC.setFieldValue("label.name", letter.toLowerCase() + "*");
+              if(letter.equals("#"))
+                letterRPC.setFieldValue("label.name", "0* | 1* | 2* | 3* | 4* | 5* | 6* | 7* | 8* | 9*");
+              else
+               letterRPC.setFieldValue("label.name", letter.toLowerCase() + "*");
                
               commitQuery(letterRPC);
               
