573587a73eae9b52bc40e3e68f730e89cf78376e
tbrady <tbrady@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2009-04-29 20:49:49 +0000

    started coding the env sample screens fetch

--- OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/bean/SampleEnvironmentalBean.java
+++ OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/bean/SampleEnvironmentalBean.java
@@ -1,5 +1,31 @@
+/** Exhibit A - UIRF Open-source Based Public Software License.
+* 
+* The contents of this file are subject to the UIRF Open-source Based
+* Public Software License(the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+* openelis.uhl.uiowa.edu
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+* 
+* The Original Code is OpenELIS code.
+* 
+* The Initial Developer of the Original Code is The University of Iowa.
+* Portions created by The University of Iowa are Copyright 2006-2008. All
+* Rights Reserved.
+* 
+* Contributor(s): ______________________________________.
+* 
+* Alternatively, the contents of this file marked
+* "Separately-Licensed" may be used under the terms of a UIRF Software
+* license ("UIRF Software License"), in which case the provisions of a
+* UIRF Software License are applicable instead of those above. 
+*/
 package org.openelis.bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -13,13 +39,17 @@ import javax.persistence.Query;
 
 import org.jboss.annotation.security.SecurityDomain;
 import org.openelis.domain.SampleEnvironmentalDO;
-import org.openelis.entity.Sample;
 import org.openelis.entity.SampleEnvironmental;
+import org.openelis.gwt.common.LastPageException;
+import org.openelis.gwt.common.data.AbstractField;
 import org.openelis.local.LockLocal;
 import org.openelis.local.SampleEnvironmentalLocal;
 import org.openelis.manager.SampleDomainInt;
 import org.openelis.manager.SampleEnvironmentalManager;
+import org.openelis.metamap.SampleEnvironmentalMetaMap;
 import org.openelis.remote.SampleEnvironmentalRemote;
+import org.openelis.util.QueryBuilder;
+import org.openelis.utils.GetPage;
 
 @Stateless
 
@@ -34,6 +64,8 @@ public class SampleEnvironmentalBean implements SampleEnvironmentalRemote, Sampl
 
     //declare the locals
     @EJB private LockLocal lockBean;
+    
+    private static final SampleEnvironmentalMetaMap Meta = new SampleEnvironmentalMetaMap();
     
     public SampleEnvironmentalDO getEnvBySampleId(Integer sampleId){
         Query query = manager.createNamedQuery("SampleEnvironmental.SampleEnvironmentalBySampleId");
@@ -79,5 +111,36 @@ public class SampleEnvironmentalBean implements SampleEnvironmentalRemote, Sampl
     
     public void validateDomain() throws Exception {
         
+    }
+
+    public List query(ArrayList<AbstractField> fields, int first, int max) throws Exception {
+        StringBuffer sb = new StringBuffer();
+        QueryBuilder qb = new QueryBuilder();
+
+        qb.setMeta(Meta);
+      
+        qb.setSelect("distinct new org.openelis.domain.IdNameDO("+Meta.SAMPLE.getId()+") ");
+       
+        //this method is going to throw an exception if a column doesnt match
+        qb.addWhere(fields);      
+
+        //qb.setOrderBy(Meta.SAMPLE.getAccessionNumber());
+       
+        sb.append(qb.getEJBQL());
+
+        Query query = manager.createQuery(sb.toString());
+        
+        if(first > -1 && max > -1)
+         query.setMaxResults(first+max);
+        
+//      ***set the parameters in the query
+        qb.setQueryParams(query);
+        
+        List returnList = GetPage.getPage(query.getResultList(), first, max);
+        
+        if(returnList == null)
+         throw new LastPageException();
+        else
+         return returnList;
     }
 }
