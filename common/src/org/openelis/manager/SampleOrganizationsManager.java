232532b11859517b4154495a000eae61b9fff3d1
tbrady <tbrady@uhl.uiowa.edu>
2009-04-30 19:56:35 +0000

    added sample project and sample organization manager code, changed some code to remove the rest of the Security-EJB dependency

--- common/src/org/openelis/manager/SampleOrganizationsManager.java
+++ common/src/org/openelis/manager/SampleOrganizationsManager.java
@@ -1,12 +1,20 @@
 package org.openelis.manager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openelis.domain.SampleOrganizationDO;
 import org.openelis.gwt.common.RPC;
 
 public class SampleOrganizationsManager implements RPC {
-    
-    private static final long serialVersionUID = 1L;
-    
-    Integer  sampleId;
+
+    private static final long                           serialVersionUID = 1L;
+
+    Integer                                             sampleId;
+
+    protected ArrayList<SampleOrganizationDO>           items;
+    protected boolean                                   cached;
+    protected transient SampleOrganizationsManagerIOInt manager;
 
     /**
      * Creates a new instance of this object.
@@ -15,10 +23,29 @@ public class SampleOrganizationsManager implements RPC {
         SampleOrganizationsManager som;
 
         som = new SampleOrganizationsManager();
+        som.items = new ArrayList<SampleOrganizationDO>();
 
         return som;
     }
-    
+
+    /**
+     * Creates a new instance of this object with the specified sample id. Use this function to load an instance of this object from database.
+     */
+    public static SampleOrganizationsManager findBySampleId(Integer sampleId) {
+        SampleOrganizationsManager som = new SampleOrganizationsManager();
+        som.items = new ArrayList<SampleOrganizationDO>();
+
+        som.setSampleId(sampleId);
+        som.fetch();
+
+        return som;
+    }
+
+    public int count() {
+        fetch();
+        return items.size();
+    }
+
     public Integer getSampleId() {
         return sampleId;
     }
@@ -27,5 +54,55 @@ public class SampleOrganizationsManager implements RPC {
         this.sampleId = sampleId;
     }
 
+    public SampleOrganizationDO add() {
+        SampleOrganizationDO newOrg = new SampleOrganizationDO();
+        items.add(newOrg);
 
+        return newOrg;
+    }
+
+    public SampleOrganizationDO getSampleOrganizationAt(int i) {
+        fetch();
+        return items.get(i);
+
+    }
+
+    public void setSampleOrganizationAt(SampleOrganizationDO sampleOrg, int i) {
+        items.add(i, sampleOrg);
+    }
+
+    public SampleOrganizationsManagerIOInt getManager() {
+        return manager;
+    }
+
+    public void setManager(SampleOrganizationsManagerIOInt manager) {
+        this.manager = manager;
+    }
+
+    public void validate() {
+
+    }
+
+    // manager methods
+    public void update() {
+        manager().update(this);
+    }
+
+    protected void fetch() {
+        if (cached)
+            return;
+
+        cached = true;
+        List sampleOrgs = manager().fetch(sampleId);
+
+        for (int i = 0; i < sampleOrgs.size(); i++)
+            items.add((SampleOrganizationDO)sampleOrgs.get(i));
+    }
+
+    private SampleOrganizationsManagerIOInt manager() {
+        if (manager == null)
+            manager = ManagerFactory.getSampleOrganizationManagerIO();
+
+        return manager;
+    }
 }
