41c91aa363ba1fed83b3797408ba541fa7c0867d
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2009-09-25 22:22:17 +0000

    Changed the DataBaseUtil sublist not to use ArrayList.subList method -- not supported by GWT.

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/utilcommon/DataBaseUtil.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/utilcommon/DataBaseUtil.java
@@ -1,32 +1,33 @@
-/** Exhibit A - UIRF Open-source Based Public Software License.
-* 
-* The contents of this file are subject to the UIRF Open-source Based
-* Public Software License(the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-* openelis.uhl.uiowa.edu
-* 
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-* 
-* The Original Code is OpenELIS code.
-* 
-* The Initial Developer of the Original Code is The University of Iowa.
-* Portions created by The University of Iowa are Copyright 2006-2008. All
-* Rights Reserved.
-* 
-* Contributor(s): ______________________________________.
-* 
-* Alternatively, the contents of this file marked
-* "Separately-Licensed" may be used under the terms of a UIRF Software
-* license ("UIRF Software License"), in which case the provisions of a
-* UIRF Software License are applicable instead of those above. 
-*/
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
 package org.openelis.utilcommon;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.openelis.gwt.common.Datetime;
@@ -46,8 +47,8 @@ public class DataBaseUtil {
     }
 
     /*
-     * Convenience methods to covert a date or a datetime to datetime with
-     * date precision.
+     * Convenience methods to covert a date or a datetime to datetime with date
+     * precision.
      */
     public static Datetime toYD(Datetime yearToDay) {
         if (yearToDay != null) {
@@ -56,17 +57,17 @@ public class DataBaseUtil {
         }
         return yearToDay;
     }
-    
+
     public static Datetime toYD(Date yearToDay) {
         Datetime dt;
 
         dt = null;
-        if (yearToDay != null) 
+        if (yearToDay != null)
             dt = new Datetime(Datetime.YEAR, Datetime.DAY, yearToDay);
-        
+
         return dt;
     }
-    
+
     public static Datetime toYM(Datetime yearToMinute) {
         if (yearToMinute != null) {
             yearToMinute.startCode = Datetime.YEAR;
@@ -79,12 +80,12 @@ public class DataBaseUtil {
         Datetime dt;
 
         dt = null;
-        if (yearToMinute != null) 
+        if (yearToMinute != null)
             dt = new Datetime(Datetime.YEAR, Datetime.MINUTE, yearToMinute);
-        
+
         return dt;
     }
-    
+
     public static Datetime toHM(Datetime hourToMinute) {
         if (hourToMinute != null) {
             hourToMinute.startCode = Datetime.HOUR;
@@ -92,56 +93,66 @@ public class DataBaseUtil {
         }
         return hourToMinute;
     }
-    
+
     public static Datetime toHM(Date hourToMinute) {
         Datetime dt;
 
         dt = null;
-        if (hourToMinute != null) 
+        if (hourToMinute != null)
             dt = new Datetime(Datetime.HOUR, Datetime.MINUTE, hourToMinute);
-        
+
         return dt;
     }
-    
+
     /*
      * Convenience method to see if two objects are the different
      */
-    
+
     public static boolean isDifferent(Object a, Object b) {
         return (a == null && b != null) || (a != null && !a.equals(b));
     }
-    
-    public static String formatStorageLocation(String storageName, String StorageLocation, String storageUnitDesc){
+
+    public static String formatStorageLocation(String storageName,
+                                               String StorageLocation,
+                                               String storageUnitDesc) {
         return formatStorageLocation(storageName, StorageLocation, storageUnitDesc, null);
     }
-    
-    public static String formatStorageLocation(String storageName, String StorageLocation, String storageUnitDesc, String parentStorageLocationName){
+
+    public static String formatStorageLocation(String storageName,
+                                               String StorageLocation,
+                                               String storageUnitDesc,
+                                               String parentStorageLocationName) {
         String returnString = "";
-        
-        if(parentStorageLocationName != null){
-            returnString += parentStorageLocationName.trim() + ", " +
-                            storageUnitDesc.trim() + ", " +
-                            StorageLocation.trim();
-        }else{
-            returnString += storageName.trim() + ", " +
-                            storageUnitDesc.trim() + ", " +
+
+        if (parentStorageLocationName != null) {
+            returnString += parentStorageLocationName.trim() + ", " + storageUnitDesc.trim() +
+                            ", " + StorageLocation.trim();
+        } else {
+            returnString += storageName.trim() + ", " + storageUnitDesc.trim() + ", " +
                             StorageLocation.trim();
         }
-        
+
         return returnString;
     }
-    
+
     /*
      * For paged result list, this method returns a subList of the query list
      * starting at first for max number of results.
      */
-    public static List subList(List query, int first, int max){
+    public static List subList(List query, int first, int max) {
         int to;
-        
+        Iterator e;
+        ArrayList list;
+
         if (query == null || query.isEmpty() || first >= query.size())
             return null;
 
-        to = Math.max(first+max, query.size());
-        return new ArrayList(query.subList(first, to));
+        to = Math.min(first + max, query.size());
+        list = new ArrayList(to - first);
+        e = query.listIterator(first);
+        for (; first < to; first++ )
+            list.add(e.next());
+
+        return list;
     }
 }
