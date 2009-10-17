cdb95ffb1d272a98b9fe45bbc12175d627870bb5
Travis Schmidt <travis.schmidt@gmail.com>
2009-10-17 18:20:19 +0000

    Commit of OrganizationPrameter meta and entity

---
+++ ejb/src/org/openelis/entity/OrganizationParameter.java
@@ -0,0 +1,114 @@
+
+package org.openelis.entity;
+
+/**
+  * OrganizationParameter Entity POJO for database 
+  */
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.openelis.util.XMLUtil;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import org.openelis.utils.AuditUtil;
+import org.openelis.utils.Auditable;
+
+@Entity
+@Table(name="organization_parameter")
+@EntityListeners({AuditUtil.class})
+public class OrganizationParameter implements Auditable, Cloneable {
+  
+  @Id
+  @GeneratedValue
+  @Column(name="id")
+  private Integer id;             
+
+  @Column(name="organization_id")
+  private Integer organizationId;             
+
+  @Column(name="type_id")
+  private Integer typeId;             
+
+  @Column(name="value")
+  private String value;             
+
+
+  @Transient
+  private OrganizationParameter original;
+
+  
+  public Integer getId() {
+    return id;
+  }
+  protected void setId(Integer id) {
+    if((id == null && this.id != null) || 
+       (id != null && !id.equals(this.id)))
+      this.id = id;
+  }
+
+  public Integer getOrganizationId() {
+    return organizationId;
+  }
+  public void setOrganizationId(Integer organizationId) {
+    if((organizationId == null && this.organizationId != null) || 
+       (organizationId != null && !organizationId.equals(this.organizationId)))
+      this.organizationId = organizationId;
+  }
+
+  public Integer getTypeId() {
+    return typeId;
+  }
+  public void setTypeId(Integer typeId) {
+    if((typeId == null && this.typeId != null) || 
+       (typeId != null && !typeId.equals(this.typeId)))
+      this.typeId = typeId;
+  }
+
+  public String getValue() {
+    return value;
+  }
+  public void setValue(String value) {
+    if((value == null && this.value != null) || 
+       (value != null && !value.equals(this.value)))
+      this.value = value;
+  }
+
+  
+  public void setClone() {
+    try {
+      original = (OrganizationParameter)this.clone();
+    }catch(Exception e){}
+  }
+  
+  public String getChangeXML() {
+    try {
+      Document doc = XMLUtil.createNew("change");
+      Element root = doc.getDocumentElement();
+      
+      AuditUtil.getChangeXML(id,original.id,doc,"id");
+
+      AuditUtil.getChangeXML(organizationId,original.organizationId,doc,"organization_id");
+
+      AuditUtil.getChangeXML(typeId,original.typeId,doc,"type_id");
+
+      AuditUtil.getChangeXML(value,original.value,doc,"value");
+
+      if(root.hasChildNodes())
+        return XMLUtil.toString(doc);
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    return null;
+  }
+   
+  public String getTableName() {
+    return "organization_parameter";
+  }
+  
+}   
