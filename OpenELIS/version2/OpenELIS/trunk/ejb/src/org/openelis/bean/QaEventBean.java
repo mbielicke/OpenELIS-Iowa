21804ca79b3da127a55004729719d6fb3736b98b
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2009-10-24 22:17:35 +0000

    Cleaned QaEventBean and entity

--- OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/bean/QaEventBean.java
+++ OpenELIS/version2/OpenELIS/trunk/ejb/src/org/openelis/bean/QaEventBean.java
@@ -1,47 +1,48 @@
-/** Exhibit A - UIRF Open-source Based Public Software License.
-* 
-* The contents of this file are subject to the UIRF Open-source Based
-* Public Software License(the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-* openelis.uhl.uiowa.edu
-* 
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-* 
-* The Original Code is OpenELIS code.
-* 
-* The Initial Developer of the Original Code is The University of Iowa.
-* Portions created by The University of Iowa are Copyright 2006-2008. All
-* Rights Reserved.
-* 
-* Contributor(s): ______________________________________.
-* 
-* Alternatively, the contents of this file marked
-* "Separately-Licensed" may be used under the terms of a UIRF Software
-* license ("UIRF Software License"), in which case the provisions of a
-* UIRF Software License are applicable instead of those above. 
-*/
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
 package org.openelis.bean;
 
 import org.jboss.annotation.security.SecurityDomain;
 import org.openelis.domain.QaEventDO;
-import org.openelis.domain.QaEventView;
+import org.openelis.domain.QaEventVO;
 import org.openelis.domain.QaEventViewDO;
+import org.openelis.domain.ReferenceTable;
 import org.openelis.entity.QaEvent;
+import org.openelis.gwt.common.DatabaseException;
 import org.openelis.gwt.common.FieldErrorException;
 import org.openelis.gwt.common.LastPageException;
+import org.openelis.gwt.common.NotFoundException;
 import org.openelis.gwt.common.ValidationErrorsList;
 import org.openelis.gwt.common.SecurityModule.ModuleFlags;
-import org.openelis.gwt.common.data.deprecated.AbstractField;
+import org.openelis.gwt.common.data.QueryData;
 import org.openelis.local.LockLocal;
 import org.openelis.metamap.QaEventMetaMap;
 import org.openelis.remote.QaEventRemote;
-import org.openelis.util.QueryBuilder;
+import org.openelis.util.QueryBuilderV2;
 import org.openelis.utilcommon.DataBaseUtil;
-import org.openelis.utils.GetPage;
-import org.openelis.utils.ReferenceTableCache;
 import org.openelis.utils.SecurityInterceptor;
 
 import java.util.ArrayList;
@@ -54,169 +55,200 @@ import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.FlushModeType;
-import javax.persistence.NamedQuery;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 @Stateless
 @SecurityDomain("openelis")
 @RolesAllowed("qaevent-select")
-public class QaEventBean implements QaEventRemote{
+public class QaEventBean implements QaEventRemote {
 
     @PersistenceContext(name = "openelis")
-    private EntityManager manager;
-    
+    private EntityManager               manager;
+
     @Resource
-    private SessionContext ctx;
-    
+    private SessionContext              ctx;
+
     @EJB
-    private LockLocal lockBean;
-    
-    private static final QaEventMetaMap QaeMeta = new QaEventMetaMap();
-    
-    private static Integer qaEventRefTableId;
-    
+    private LockLocal                   lockBean;
+
+    private static final QaEventMetaMap meta = new QaEventMetaMap();
+
     public QaEventBean() {
-        qaEventRefTableId = ReferenceTableCache.getReferenceTable("qaevent");
     }
-    
-    public QaEventViewDO getQaEvent(Integer qaEventId) {
-        Query query = manager.createNamedQuery("QaEvent.QaEvent");
-        query.setParameter("id", qaEventId);
-        QaEventViewDO qaEvent = (QaEventViewDO) query.getSingleResult();
-        
-        return qaEvent;
-    }  
 
-    public List query(ArrayList<AbstractField> fields, int first, int max) throws Exception {
-        StringBuffer sb = new StringBuffer();
-             
-        QueryBuilder qb = new QueryBuilder();
-               
-        qb.setMeta(QaeMeta);        
-        qb.setSelect("distinct new org.openelis.domain.IdNameTestMethodDO("
-                     + QaeMeta.getId()+", "+QaeMeta.getName()
-                     +", "+QaeMeta.getQaEventTest().getName()
-                     +", "+QaeMeta.getQaEventTest().getMethod().getName() + ") ");        
-        
-        //this method is going to throw an exception if a column doesnt match
-        qb.addWhere(fields);
-                
-        qb.setOrderBy(QaeMeta.getName()+", "+QaeMeta.getQaEventTest().getName()
-                      +", "+QaeMeta.getQaEventTest().getMethod().getName());                           
-                
-        sb.append(qb.getEJBQL());                 
-        Query query = manager.createQuery(sb.toString());
-        
-        if(first > -1 && max > -1)
-            query.setMaxResults(first+max);
-                
-        //***set the parameters in the query
-        qb.setQueryParams(query);
-        
-        List returnList = GetPage.getPage(query.getResultList(), first, max);
-        if(returnList == null)
-         throw new LastPageException();
-        else
-         return returnList;
-    }
-   
-    @RolesAllowed("qaevent-update")
-    public Integer updateQaEvent(QaEventViewDO qaEventDO)throws Exception{ 
-        SecurityInterceptor.applySecurity(ctx.getCallerPrincipal().getName(), "qaevent", ModuleFlags.UPDATE);
-        QaEvent qaEvent;
+    public QaEventViewDO fetchById(Integer id) throws Exception {
         Query query;
-        Integer qaEventId;
+        QaEventViewDO data;
+
+        query = manager.createNamedQuery("QaEvent.FetchById");
+        query.setParameter("id", id);
+        try {
+            data = (QaEventViewDO)query.getSingleResult();
+        } catch (NoResultException e) {
+            throw new NotFoundException();
+        } catch (Exception e) {
+            throw new DatabaseException(e);
+        }
+        return data;
+    }
+    
+    public ArrayList<QaEventVO> fetchByName(String name) throws Exception {
+        Query query;
+    
+        query = manager.createNamedQuery("QaEvent.FetchByName");
+        query.setParameter("name", name);
+        return DataBaseUtil.toArrayList(query.getResultList());
+    }
+
+    public ArrayList<QaEventVO> fetchByTestId(Integer id) throws Exception {
+        Query query;
         
-        qaEventId = qaEventDO.getId();
-            
-        if(qaEventId != null){
-            //we need to call lock one more time to make sure their lock didnt expire and someone else grabbed the record
-            lockBean.validateLock(qaEventRefTableId,qaEventId);                        
-        }               
-        
-        validateQaEvent(qaEventDO);  
-        
+        query = manager.createNamedQuery("QaEvent.FetchByTestId");
+        query.setParameter("testId", id);
+        return DataBaseUtil.toArrayList(query.getResultList());
+    }
+
+    public ArrayList<QaEventVO> fetchByCommon() throws Exception {
+        Query query;
+
+        query = manager.createNamedQuery("QaEvent.FetchByName");
+        return DataBaseUtil.toArrayList(query.getResultList());
+    }
+
+    public ArrayList<QaEventVO> query(ArrayList<QueryData> fields, int first, int max) throws Exception {
+        Query query;
+        QueryBuilderV2 builder;
+        List list;
+
+        builder = new QueryBuilderV2();
+        builder.setMeta(meta);
+        builder.setSelect("distinct new org.openelis.domain.QaEventVO(" +
+                          meta.getId() + "," +
+                          meta.getName() + "," +
+                          meta.getDescription() + "," +
+                          meta.getTestId() + "," +
+                          meta.getTypeId() + "," +
+                          meta.getIsBillable() + "," +
+                          meta.getReportingSequence() + "," +
+                          meta.getTest().getName() + "," +
+                          meta.getTest().getMethod().getName() + ")");
+        builder.constructWhere(fields);
+        builder.setOrderBy(meta.getName() + "," + meta.getTest().getName());
+
+        query = manager.createQuery(builder.getEJBQL());
+        query.setMaxResults(first + max);
+        builder.setQueryParams(query, fields);
+
+        list = query.getResultList();
+        if (list.isEmpty())
+            throw new NotFoundException();
+        list = (ArrayList<QaEventVO>)DataBaseUtil.subList(list, first, max);
+        if (list == null)
+            throw new LastPageException();
+
+        return (ArrayList<QaEventVO>)list;
+    }
+
+    public QaEventViewDO add(QaEventViewDO data) throws Exception {
+        QaEvent entity;
+
+        checkSecurity(ModuleFlags.ADD);
+
+        validate(data);
+
         manager.setFlushMode(FlushModeType.COMMIT);
-        qaEvent = null;
-        
-        if(qaEventId == null){
-            qaEvent = new QaEvent();            
-        }else{
-            qaEvent = manager.find(QaEvent.class, qaEventId);
-        }
-        
-        qaEvent.setDescription(qaEventDO.getDescription());
-        qaEvent.setIsBillable(qaEventDO.getIsBillable());
-        qaEvent.setName(qaEventDO.getName());
-        qaEvent.setReportingSequence(qaEventDO.getReportingSequence());
-        qaEvent.setReportingText(qaEventDO.getReportingText());
-        qaEvent.setTestId(qaEventDO.getTestId());
-        qaEvent.setTypeId(qaEventDO.getTypeId());
-        
-        if(qaEvent.getId() == null){
-            manager.persist(qaEvent);
-        }
-                
-        lockBean.giveUpLock(qaEventRefTableId,qaEvent.getId()); 
-        return qaEvent.getId();
+
+        entity = new QaEvent();
+        entity.setName(data.getName());
+        entity.setDescription(data.getDescription());
+        entity.setTestId(data.getTestId());
+        entity.setTypeId(data.getTypeId());
+        entity.setIsBillable(data.getIsBillable());
+        entity.setReportingSequence(data.getReportingSequence());
+        entity.setReportingText(data.getReportingText());
+
+        manager.persist(entity);
+        data.setId(entity.getId());
+
+        return data;
     }
 
-    @RolesAllowed("qaevent-update")
-    public QaEventViewDO getQaEventAndLock(Integer qaEventId, String session) throws Exception {
-        SecurityInterceptor.applySecurity(ctx.getCallerPrincipal().getName(), "qaevent", ModuleFlags.UPDATE);
-        lockBean.getLock(qaEventRefTableId,qaEventId);        
-        return getQaEvent(qaEventId);
+    public QaEventViewDO update(QaEventViewDO data) throws Exception {
+        QaEvent entity;
+
+        if ( !data.isChanged())
+            return data;
+
+        checkSecurity(ModuleFlags.UPDATE);
+
+        validate(data);
+
+        lockBean.validateLock(ReferenceTable.QAEVENT, data.getId());
+
+        manager.setFlushMode(FlushModeType.COMMIT);
+        entity = manager.find(QaEvent.class, data.getId());
+        entity.setName(data.getName());
+        entity.setDescription(data.getDescription());
+        entity.setTestId(data.getTestId());
+        entity.setTypeId(data.getTypeId());
+        entity.setIsBillable(data.getIsBillable());
+        entity.setReportingSequence(data.getReportingSequence());
+        entity.setReportingText(data.getReportingText());
+
+        lockBean.giveUpLock(ReferenceTable.QAEVENT, data.getId());
+
+        return data;
     }
 
-    public QaEventViewDO getQaEventAndUnlock(Integer qaEventId, String session) {
-        lockBean.giveUpLock(qaEventRefTableId,qaEventId);                   
-        return getQaEvent(qaEventId);
+    public QaEventViewDO fetchForUpdate(Integer id) throws Exception {
+        lockBean.getLock(ReferenceTable.QAEVENT, id);
+        return fetchById(id);
     }
-    
-    public ArrayList<QaEventDO> fetchAllCommon(){
-        Query query = manager.createNamedQuery("QaEvent.FetchAllCommon");
-        
-        return DataBaseUtil.toArrayList(query.getResultList());
+
+    public QaEventViewDO abortUpdate(Integer id) throws Exception {
+        lockBean.giveUpLock(ReferenceTable.QAEVENT, id);
+        return fetchById(id);
     }
-    
-    public ArrayList<QaEventDO> fetchTestSpecificById(Integer testId){
-        Query query = manager.createNamedQuery("QaEvent.FetchAllTestSpecific");
-        query.setParameter("testId", testId);
-        
-        return DataBaseUtil.toArrayList(query.getResultList());
+
+    public void validate(QaEventDO data) throws Exception {
+        ValidationErrorsList list;
+
+        list = new ValidationErrorsList();
+
+        if (DataBaseUtil.isEmpty(data.getName())) {
+            list.add(new FieldErrorException("fieldRequiredException", meta.getName()));
+        } else {
+            ArrayList<QaEventVO> dups;
+            //
+            // do not allow duplicates names for the same test or
+            // for the general group (test = null)
+            //
+            try {
+                dups = fetchByName(data.getName());
+                for (QaEventVO dup : dups) {
+                    if (DataBaseUtil.isDifferent(data.getId(), dup.getId()) &&
+                        !DataBaseUtil.isDifferent(data.getName(), dup.getName()) &&
+                        !DataBaseUtil.isDifferent(data.getTestId(), dup.getTestId()))
+                        list.add(new FieldErrorException("fieldUniqueException", meta.getName()));
+                }
+            } catch (NotFoundException ignE) {
+            }
+        }
+
+        if (DataBaseUtil.isEmpty(data.getTypeId()))
+            list.add(new FieldErrorException("fieldRequiredException", meta.getTypeId()));
+
+        if (DataBaseUtil.isEmpty(data.getReportingText()))
+            list.add(new FieldErrorException("fieldRequiredException", meta.getReportingText()));
+
+        if (list.size() > 0)
+            throw list;
     }
-    
-    public ArrayList<QaEventView> fetchCommonByName(String name, int numberOfResults){
-        Query query = manager.createNamedQuery("QaEvent.FetchCommonByName");
-        query.setParameter("name", name);
-        query.setMaxResults(numberOfResults);
-        
-        return DataBaseUtil.toArrayList(query.getResultList());
+
+    private void checkSecurity(ModuleFlags flag) throws Exception {
+        SecurityInterceptor.applySecurity(ctx.getCallerPrincipal().getName(),
+                                          "qaevent", flag);
     }
-    
-    public ArrayList<QaEventView> fetchTestSpecificByName(String name, Integer testId, int numberOfResults){
-        Query query = manager.createNamedQuery("QaEvent.FetchTestSpecificByName");
-        query.setParameter("name", name);
-        query.setParameter("testId", testId);
-        query.setMaxResults(numberOfResults);
-        
-        return DataBaseUtil.toArrayList(query.getResultList());    
-    }
-    
-    private void validateQaEvent(QaEventViewDO qaEventDO) throws Exception{
-        ValidationErrorsList exceptionList;
-        
-        exceptionList = new ValidationErrorsList(); 
-        if(qaEventDO.getTypeId()==null){                       
-            exceptionList.add(new FieldErrorException("fieldRequiredException",QaeMeta.getTypeId()));
-        } 
-        if("".equals(qaEventDO.getName())){                       
-            exceptionList.add(new FieldErrorException("fieldRequiredException",QaeMeta.getName()));
-        } 
-        if("".equals(qaEventDO.getReportingText())){                       
-            exceptionList.add(new FieldErrorException("fieldRequiredException",QaeMeta.getReportingText()));
-        } 
-    } 
- 
 }
