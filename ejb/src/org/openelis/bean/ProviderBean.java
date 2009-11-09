d11102303fec4b09691e37e2ac6cde82fae868c7
dshirazi <dari.shirazi@gmail.com>
2009-11-09 17:13:48 +0000

    Cleaned up provider.

--- ejb/src/org/openelis/bean/ProviderBean.java
+++ ejb/src/org/openelis/bean/ProviderBean.java
@@ -1,28 +1,28 @@
-/** Exhibit A - UIRF Open-source Based Public Software License.
-* 
-* The contents of this file are subject to the UIRF Open-source Based
-* Public Software License(the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-* openelis.uhl.uiowa.edu
-* 
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-* 
-* The Original Code is OpenELIS code.
-* 
-* The Initial Developer of the Original Code is The University of Iowa.
-* Portions created by The University of Iowa are Copyright 2006-2008. All
-* Rights Reserved.
-* 
-* Contributor(s): ______________________________________.
-* 
-* Alternatively, the contents of this file marked
-* "Separately-Licensed" may be used under the terms of a UIRF Software
-* license ("UIRF Software License"), in which case the provisions of a
-* UIRF Software License are applicable instead of those above. 
-*/
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
 package org.openelis.bean;
 
 import java.util.ArrayList;
@@ -56,104 +56,95 @@ import org.openelis.utilcommon.DataBaseUtil;
 public class ProviderBean implements ProviderRemote, ProviderLocal {
 
     @PersistenceContext(name = "openelis")
-    private EntityManager manager;
-        
-    private static final ProviderMetaMap ProvMeta = new ProviderMetaMap(); 
-    
-    public ProviderDO fetchById(Integer providerId) {                 
+    private EntityManager                manager;
+
+    private static final ProviderMetaMap meta = new ProviderMetaMap();
+
+    public ProviderDO fetchById(Integer providerId) {
         Query query;
         ProviderDO provider;
-        
+
         query = manager.createNamedQuery("Provider.FetchById");
         query.setParameter("id", providerId);
-        provider = (ProviderDO) query.getSingleResult();// getting provider 
+        provider = (ProviderDO)query.getSingleResult();// getting provider
 
         return provider;
     }
 
     @SuppressWarnings("unchecked")
-	public ArrayList<IdFirstLastNameVO> query (ArrayList<QueryData> fields, int first, int max) throws Exception {                         
-       Query query;
-       QueryBuilderV2 qb = new QueryBuilderV2();
-       List list;
-                
-       qb.setMeta(ProvMeta);
-        
-       qb.setSelect("distinct new org.openelis.domain.IdLastNameFirstNameDO("+ProvMeta.getId()+", "+ProvMeta.getLastName()+", "+ProvMeta.getFirstName() + ") ");
-       
-       
-       qb.constructWhere(fields);   
-       
-       qb.setOrderBy(ProvMeta.getLastName()+", "+ProvMeta.getFirstName());       
-          
-       query = manager.createQuery(qb.getEJBQL());
-       
-       query.setMaxResults(first+max);
-       
-       QueryBuilderV2.setQueryParams(query,fields);
-       
-       list = query.getResultList();
-       if (list.isEmpty())
-           throw new NotFoundException();
-       list = (ArrayList<IdFirstLastNameVO>)DataBaseUtil.subList(list, first, max);
-       if (list == null)
-           throw new LastPageException();
+    public ArrayList<IdFirstLastNameVO> query(ArrayList<QueryData> fields, int first, int max)
+                                                                                              throws Exception {
+        Query query;
+        QueryBuilderV2 builder;
+        List list;
 
-       return (ArrayList<IdFirstLastNameVO>)list;
-        
+        builder = new QueryBuilderV2();
+        builder.setMeta(meta);
+        builder.setSelect("distinct new org.openelis.domain.IdFirstLastNameVO(" + meta.getId() +
+                          "," + meta.getLastName() + ", " + meta.getFirstName() + ") ");
+        builder.constructWhere(fields);
+        builder.setOrderBy(meta.getLastName() + "," + meta.getFirstName());
+
+        query = manager.createQuery(builder.getEJBQL());
+        query.setMaxResults(first + max);
+        builder.setQueryParams(query, fields);
+
+        list = query.getResultList();
+        if (list.isEmpty())
+            throw new NotFoundException();
+        list = (ArrayList<IdFirstLastNameVO>)DataBaseUtil.subList(list, first, max);
+        if (list == null)
+            throw new LastPageException();
+
+        return (ArrayList<IdFirstLastNameVO>)list;
     }
 
-    public ProviderDO add(ProviderDO data) throws Exception{
-    	Provider entity;
-                
+    public ProviderDO add(ProviderDO data) throws Exception {
+        Provider entity;
+
         manager.setFlushMode(FlushModeType.COMMIT);
         entity = new Provider();
-            
+
         entity.setFirstName(data.getFirstName());
         entity.setLastName(data.getLastName());
         entity.setMiddleName(data.getMiddleName());
         entity.setNpi(data.getNpi());
         entity.setTypeId(data.getTypeId());
-        
+
         manager.persist(entity);
-        
         data.setId(entity.getId());
-        
+
         return data;
     }
-    
-    public ProviderDO update(ProviderDO data) throws Exception{
-    	Provider entity;
-        
-        if(!data.isChanged())
-        	return data;
-        
+
+    public ProviderDO update(ProviderDO data) throws Exception {
+        Provider entity;
+
+        if ( !data.isChanged())
+            return data;
+
         manager.setFlushMode(FlushModeType.COMMIT);
-        entity = manager.find(Provider.class,data.getId());
-            
+        entity = manager.find(Provider.class, data.getId());
         entity.setFirstName(data.getFirstName());
         entity.setLastName(data.getLastName());
         entity.setMiddleName(data.getMiddleName());
         entity.setNpi(data.getNpi());
         entity.setTypeId(data.getTypeId());
-        
+
         return data;
     }
 
+    public void validate(ProviderDO providerDO) throws ValidationErrorsList {
+        ValidationErrorsList list;
+        
+        list = new ValidationErrorsList();
+        if (DataBaseUtil.isEmpty(providerDO.getLastName()))
+            list.add(new FieldErrorException("fieldRequiredException", meta.getLastName()));
 
-   public void validate(ProviderDO providerDO) throws ValidationErrorsList { 
-	   ValidationErrorsList vl = new ValidationErrorsList();
-       
-	   if("".equals(providerDO.getLastName())){           
-           vl.add(new FieldErrorException("fieldRequiredException",ProvMeta.getLastName()));
-       }
-       
-	   if(providerDO.getTypeId()==null){           
-           vl.add(new FieldErrorException("fieldRequiredException",ProvMeta.getTypeId()));
-       }
-	   
-	   if(vl.size() > 0)
-		   throw vl;
-   }
-   
+        if (DataBaseUtil.isEmpty(providerDO.getTypeId()))
+            list.add(new FieldErrorException("fieldRequiredException", meta.getTypeId()));
+
+        if (list.size() > 0)
+            throw list;
+    }
 }
