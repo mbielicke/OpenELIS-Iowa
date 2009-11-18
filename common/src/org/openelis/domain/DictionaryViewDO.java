3ef81f23a5da18c782bd71ecd994b422a66cb6cd
dshirazi <dari.shirazi@gmail.com>
2009-11-18 20:29:17 +0000

    Added comments to the dictionary view do.

--- common/src/org/openelis/domain/DictionaryViewDO.java
+++ common/src/org/openelis/domain/DictionaryViewDO.java
@@ -2,28 +2,34 @@ package org.openelis.domain;
 
 import org.openelis.utilcommon.DataBaseUtil;
 
+/**
+ * The class extends dictionary DO and adds a commonly used field related entry
+ * name. The additional fields are for read/display only and do not get
+ * committed to the database. Note: isChanged will reflect any changes to
+ * read/display fields.
+ */
+
 public class DictionaryViewDO extends DictionaryDO {
 
     private static final long serialVersionUID = 1L;
-    protected String relatedEntryName;
-    
-    public DictionaryViewDO(){
-        
+
+    protected String          relatedEntryName;
+
+    public DictionaryViewDO() {
     }
-    
-    public DictionaryViewDO(Integer id,Integer sortOrder, Integer categoryId, Integer relatedEntryId, String systemName,
-                            String isActive, String localAbbrev, String entry, String relatedEntryName){
-        super(id,sortOrder,categoryId, relatedEntryId, systemName, isActive, localAbbrev, entry);
+
+    public DictionaryViewDO(Integer id, Integer sortOrder, Integer categoryId,
+                            Integer relatedEntryId, String systemName, String isActive,
+                            String localAbbrev, String entry, String relatedEntryName) {
+        super(id, sortOrder, categoryId, relatedEntryId, systemName, isActive, localAbbrev, entry);
         setRelatedEntryName(relatedEntryName);
-        
     }
-    
+
     public String getRelatedEntryName() {
         return relatedEntryName;
     }
-    
+
     public void setRelatedEntryName(String relatedEntryName) {
         this.relatedEntryName = DataBaseUtil.trim(relatedEntryName);
     }
-
 }
