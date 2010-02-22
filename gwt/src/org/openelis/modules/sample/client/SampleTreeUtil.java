924adb3eb02370cc50634e2610dd2eb58a9af6f1
tbrady <tbrady@uhl.uiowa.edu>
2010-02-22 15:34:30 +0000

    reworked sampleTreeUtil, fixed bundle bug in analysis manager

--- gwt/src/org/openelis/modules/sample/client/SampleTreeUtil.java
+++ gwt/src/org/openelis/modules/sample/client/SampleTreeUtil.java
@@ -2,15 +2,15 @@ package org.openelis.modules.sample.client;
 
 import java.util.ArrayList;
 
-import org.openelis.cache.DictionaryCache;
 import org.openelis.domain.AnalysisViewDO;
 import org.openelis.domain.SampleItemViewDO;
+import org.openelis.gwt.common.ValidationErrorsList;
 import org.openelis.gwt.event.ActionEvent;
 import org.openelis.gwt.event.ActionHandler;
 import org.openelis.gwt.event.HasActionHandlers;
 import org.openelis.gwt.screen.Screen;
+import org.openelis.gwt.widget.Confirm;
 import org.openelis.gwt.widget.ScreenWindow;
-import org.openelis.gwt.widget.table.TableDataCell;
 import org.openelis.gwt.widget.tree.TreeDataItem;
 import org.openelis.gwt.widget.tree.TreeWidget;
 import org.openelis.manager.AnalysisManager;
@@ -18,24 +18,102 @@ import org.openelis.manager.SampleDataBundle;
 import org.openelis.manager.SampleManager;
 import org.openelis.modules.sample.client.SampleItemAnalysisTreeTab.Action;
 
-import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.Window;
 
-public class SampleTreeUtil implements HasActionHandlers{
-	
+public abstract class SampleTreeUtil extends Screen implements HasActionHandlers {
+
     protected TestPrepUtility testLookup;
     private HasActionHandlers parentScreen;
     private SampleManager     manager;
     private ScreenWindow      window;
     private TreeWidget        itemsTree;
-    
+    private Confirm           cancelAnalysisConfirm;
+
     public SampleTreeUtil(ScreenWindow window, TreeWidget itemsTree, HasActionHandlers parentScreen) {
-    	this.window = window;
-    	this.itemsTree = itemsTree;
-    	this.parentScreen = parentScreen;
+        this.window = window;
+        this.itemsTree = itemsTree;
+        this.parentScreen = parentScreen;
     }
-    
+
+    public void onAddItemButtonClick() {
+        TreeDataItem newRow;
+
+        newRow = itemsTree.createTreeItem("sampleItem");
+        newRow.toggle();
+
+        itemsTree.addRow(newRow);
+        itemsTree.select(newRow);
+    }
+
+    public void onAddAnalysisButtonClick() {
+        TreeDataItem selectedRow;
+        int selectedIndex;
+        TreeDataItem newRow;
+
+        newRow = itemsTree.createTreeItem("analysis");
+        newRow.toggle();
+
+        selectedIndex = itemsTree.getSelectedRow();
+        if (selectedIndex == -1)
+            onAddItemButtonClick();
+
+        selectedIndex = itemsTree.getSelectedRow();
+        selectedRow = itemsTree.getRow(selectedIndex);
+
+        if ( !"sampleItem".equals(selectedRow.leafType))
+            selectedRow = selectedRow.parent;
+
+        itemsTree.addChildItem(selectedRow, newRow);
+        itemsTree.select(newRow);
+    }
+
+    public void onRemoveRowButtonClick() {
+        final TreeDataItem selectedTreeRow;
+        SampleDataBundle bundle;
+        AnalysisViewDO anDO;
+        // int sampleItemIndex = -1;
+
+        try {
+            selectedTreeRow = itemsTree.getSelection();
+            if ("analysis".equals(selectedTreeRow.leafType)) {
+                if (selectedTreeRow.key != null) {
+                    if (cancelAnalysisConfirm == null) {
+                        cancelAnalysisConfirm = new Confirm(Confirm.Type.QUESTION,
+                                                            ((Screen)parentScreen).consts.get("cancelAnalysisMessage"),
+                                                            "No", "Yes");
+                        cancelAnalysisConfirm.addSelectionHandler(new SelectionHandler<Integer>() {
+                            public void onSelection(SelectionEvent<Integer> event) {
+                                switch (event.getSelectedItem().intValue()) {
+                                    case 1:
+                                        cancelAnalysisRow(itemsTree.getSelectedRow());
+                                        break;
+                                }
+                            }
+                        });
+                    }
+
+                    cancelAnalysisConfirm.show();
+
+                } else {
+                    bundle = (SampleDataBundle)selectedTreeRow.data;
+                    anDO = manager.getSampleItems()
+                                  .getAnalysisAt(bundle.getSampleItemIndex())
+                                  .getAnalysisAt(bundle.getAnalysisIndex());
+
+                    cleanupTestsWithPrep(anDO.getId());
+                    itemsTree.deleteRow(selectedTreeRow);
+                }
+            } else {
+                itemsTree.deleteRow(selectedTreeRow);
+            }
+        } catch (Exception e) {
+            Window.alert("removeRowButton: " + e.getMessage());
+        }
+    }
+
     public void testLookupFinished(ArrayList<SampleDataBundle> bundles) {
         window.setBusy();
         TreeDataItem itemRow, newRow;
@@ -47,28 +125,9 @@ public class SampleTreeUtil implements HasActionHandlers{
             if (i == 0) {
                 newRow = itemsTree.getSelection();
                 newRow.data = bundles.get(i);
-            } else {
-                newRow  = new TreeDataItem(2);
-    			newRow.leafType = "analysis";
-    			newRow.data = bundles.get(i);
-    			TreeDataItem results = new TreeDataItem();
-    			results.leafType = "result";
-    			results.data = bundles.get(i);
-    			results.cells.add(new TableDataCell("Results"));
-    			newRow.addItem(results);
-    			TreeDataItem qaevent = new TreeDataItem();
-    			qaevent.leafType = "qaevent";
-    			qaevent.data = bundles.get(i);
-    			qaevent.cells.add(new TableDataCell("QA Events"));
-    			newRow.addItem(qaevent);
-    			TreeDataItem note = new TreeDataItem();
-    			note.leafType = "note";
-    			note.data = bundles.get(i);
-    			note.cells.add(new TableDataCell("Notes"));
-    			newRow.addItem(note);	
-                itemsTree.addChildItem(itemRow, newRow,itemRow.getItems().size()-2);
-            }
-
+            } else 
+                newRow = addNewTreeRowFromBundle(itemRow, bundles.get(i));
+               
             updateAnalysisRow(newRow);
             itemsTree.refreshRow(newRow);
         }
@@ -76,6 +135,9 @@ public class SampleTreeUtil implements HasActionHandlers{
         itemsTree.fireEvents(true);
         if (newRow != null)
             itemsTree.select(newRow);
+        else if(bundles.size() == 0)
+            ActionEvent.fire(this, Action.REFRESH_TABS, itemsTree.getSelection().data);
+        
         window.clearStatus();
     }
 
@@ -103,21 +165,24 @@ public class SampleTreeUtil implements HasActionHandlers{
                 anDO = anMan.getAnalysisAt(bundle.getAnalysisIndex());
 
                 // this test points to the prep, we need to clean it up
-                if (analysisId.equals(anDO.getPreAnalysisId()))
+                if (analysisId.equals(anDO.getPreAnalysisId())){
                     anMan.unlinkPrepTest(bundle.getAnalysisIndex());
+                    updateAnalysisRow(treeItem);
+                }
             }
         } catch (Exception e) {
             Window.alert("cleanupTestsWithPrep: " + e.getMessage());
         }
     }
-
+    
     public void analysisTestChanged(Integer id, boolean panel) {
         SampleDataBundle analysisBundle;
         TestPrepUtility.Type type;
 
         if (testLookup == null) {
             testLookup = new TestPrepUtility();
-
+            testLookup.setScreen((Screen)parentScreen);
+            
             testLookup.addActionHandler(new ActionHandler<TestPrepUtility.Action>() {
                 public void onAction(ActionEvent<org.openelis.modules.sample.client.TestPrepUtility.Action> event) {
                     testLookupFinished((ArrayList<SampleDataBundle>)event.getData());
@@ -140,7 +205,7 @@ public class SampleTreeUtil implements HasActionHandlers{
             Window.alert("analysisTestChanged: " + e.getMessage());
         }
     }
-
+    
     public void cancelAnalysisRow(int selectedIndex) {
         TreeDataItem treeRow;
         SampleDataBundle bundle;
@@ -160,11 +225,14 @@ public class SampleTreeUtil implements HasActionHandlers{
             cleanupTestsWithPrep(anMan.getAnalysisAt(bundle.getAnalysisIndex()).getId());
 
             ActionEvent.fire(this, Action.REFRESH_TABS, bundle);
+            
+        }catch(ValidationErrorsList e){
+            ((Screen)parentScreen).showErrors(e);
         } catch (Exception e) {
             Window.alert("cancelAnalysisRow: " + e.getMessage());
         }
     }
-
+    
     public void updateSampleItemRow(TreeDataItem treeRow) {
         SampleDataBundle bundle;
         SampleItemViewDO siDO;
@@ -181,7 +249,7 @@ public class SampleTreeUtil implements HasActionHandlers{
             Window.alert("updateSampleItemRow: " + e.getMessage());
         }
     }
-
+    
     public void updateAnalysisRow(TreeDataItem treeRow) {
         SampleDataBundle bundle;
         AnalysisViewDO anDO;
@@ -201,23 +269,25 @@ public class SampleTreeUtil implements HasActionHandlers{
         }
     }
 
-    private String formatTreeString(String val) {
+    public String formatTreeString(String val) {
         if (val == null || "".equals(val))
             return "<>";
 
         return val.trim();
     }
-	
-    public void setManager(SampleManager manager){
-    	this.manager = manager;
+
+    public void setManager(SampleManager manager) {
+        this.manager = manager;
     }
-    
+
+    /**
+     * This method is called when the test prep util is done and the screen
+     * needs to rebuild the tree. This method is called to add a new row
+     * to the tree that the test prep util created.
+     */
+    public abstract TreeDataItem addNewTreeRowFromBundle(TreeDataItem parentRow, SampleDataBundle bundle);
+
     public HandlerRegistration addActionHandler(ActionHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void fireEvent(GwtEvent<?> event) {
-		// TODO Auto-generated method stub
-		
-	}
+        return addHandler(handler, ActionEvent.getType());
+    }
 }
