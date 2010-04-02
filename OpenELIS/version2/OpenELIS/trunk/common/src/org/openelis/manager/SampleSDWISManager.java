4a2d1f6b8c3c936e4eb87201a233ce657965af12
tbrady <tbrady@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2010-04-02 19:00:17 +0000

    added sample sdwis bean, manager, proxy, updated samplemeta and sdwis xml

---
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/manager/SampleSDWISManager.java
@@ -0,0 +1,61 @@
+package org.openelis.manager;
+
+import org.openelis.domain.SampleSDWISViewDO;
+import org.openelis.gwt.common.RPC;
+
+public class SampleSDWISManager implements RPC, SampleDomainInt {
+    private static final long                           serialVersionUID = 1L;
+    protected Integer sampleId;
+    protected SampleSDWISViewDO                     sdwis;
+
+    protected transient static SampleSDWISManagerProxy proxy;
+    
+    /**
+     * Creates a new instance of this object.
+     */
+    public static SampleSDWISManager getInstance() {
+        SampleSDWISManager ssm;
+
+        ssm = new SampleSDWISManager();
+        ssm.sdwis = new SampleSDWISViewDO();
+
+        return ssm;
+    }
+
+    public static SampleSDWISManager fetchBySampleId(Integer sampleId) throws Exception {
+        return proxy().fetch(sampleId);
+    }
+    
+    // setters/getters
+    public SampleSDWISViewDO getSDWIS() {
+        return sdwis;
+    }
+    
+    public void setSDWIS(SampleSDWISViewDO sdwisDO) {
+        sdwis = sdwisDO;
+    }
+    
+    public Integer getSampleId() {
+        return sampleId;
+    }
+
+    public void setSampleId(Integer sampleId) {
+        this.sampleId = sampleId;
+    }
+    
+    // manager methods
+    public SampleSDWISManager add() throws Exception {
+        return proxy().add(this);
+    }
+    
+    public SampleSDWISManager update() throws Exception {
+        return proxy().update(this);
+    }
+
+    private static SampleSDWISManagerProxy proxy(){
+        if(proxy == null) 
+            proxy = new SampleSDWISManagerProxy();
+        
+        return proxy;
+    }    
+}
