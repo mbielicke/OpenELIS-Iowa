a58d86ab47f2e3ce9f834fd92751c10b0a2798e3
tbrady <tbrady@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2010-04-14 19:44:43 +0000

    load the result default values from the backend, units are not longer required

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/ResultManagerRemote.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/ResultManagerRemote.java
@@ -7,7 +7,7 @@ import org.openelis.manager.AnalysisResultManager;
 @Remote
 public interface ResultManagerRemote {
     public AnalysisResultManager fetchByAnalysisIdForDisplay(Integer analysisId) throws Exception;
-    public AnalysisResultManager fetchByAnalysisId(Integer analysisId, Integer testId) throws Exception;
-    public AnalysisResultManager fetchByTestId(Integer testId) throws Exception;
+    public AnalysisResultManager fetchForUpdateWithAnalysisId(Integer analysisId, Integer testId) throws Exception;
+    public AnalysisResultManager fetchForUpdateWithTestId(Integer testId, Integer unitId) throws Exception;
     public AnalysisResultManager merge(AnalysisResultManager man) throws Exception;
 }
