83de6ea66d01d9a55e418a4fe7a68bf97b98bff4
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2010-04-20 15:49:01 +0000

    Added fetchByAnalysisId to WorksheetRemote.

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/WorksheetRemote.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/WorksheetRemote.java
@@ -29,10 +29,12 @@ import java.util.ArrayList;
 
 import javax.ejb.Remote;
 
+import org.openelis.domain.WorksheetDO;
 import org.openelis.domain.WorksheetViewDO;
 import org.openelis.gwt.common.data.QueryData;
 
 @Remote
 public interface WorksheetRemote {
     public ArrayList<WorksheetViewDO> query(ArrayList<QueryData> fields, int first, int max) throws Exception;
+    public ArrayList<WorksheetDO> fetchByAnalysisId(Integer id) throws Exception; 
 }
