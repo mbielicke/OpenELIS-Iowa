01037b51de7d8d0356068c555533f20584eb36f4
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2010-05-13 18:48:31 +0000

    Added a hash map to maintain a list of sample managers to be updated when a worksheet is commited.  This eliminates the problem of updating a sample manager multiple times within one transaction. Fixed a bug in the code that is supposed to empty the last batch of a worksheet if it only contains QCs.

--- OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/manager/WorksheetItemManagerProxy.java
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/manager/WorksheetItemManagerProxy.java
@@ -25,6 +25,8 @@
 */
 package org.openelis.manager;
 
+import java.util.HashMap;
+
 import org.openelis.gwt.common.ValidationErrorsList;
 import org.openelis.gwt.services.ScreenService;
 
@@ -39,12 +41,12 @@ public class WorksheetItemManagerProxy {
     public WorksheetItemManager fetchByWorksheetId(Integer worksheetId) throws Exception {
         return service.call("fetchWorksheetItemByWorksheetId", worksheetId);
     }
-    public WorksheetItemManager add(WorksheetItemManager manager) throws Exception {
+    public WorksheetItemManager add(WorksheetItemManager manager, HashMap<Integer,SampleManager> sManagers) throws Exception {
         assert false : "not supported";
         return null;
     }
 
-    public WorksheetItemManager update(WorksheetItemManager manager) throws Exception {
+    public WorksheetItemManager update(WorksheetItemManager manager, HashMap<Integer,SampleManager> sManagers) throws Exception {
         assert false : "not supported";
         return null;
     }
