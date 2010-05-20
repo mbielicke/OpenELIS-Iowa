1e24937f151dc455eab9f3c3fd27488dd562dba7
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2010-05-20 18:08:14 +0000

    Removed number of unused VO and DOs. Removed the metamap since we no longer use it.

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/domain/SecuritySystemUserDO.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/domain/SecuritySystemUserDO.java
@@ -1,28 +1,28 @@
-/** Exhibit A - UIRF Open-source Based Public Software License.
-* 
-* The contents of this file are subject to the UIRF Open-source Based
-* Public Software License(the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-* openelis.uhl.uiowa.edu
-* 
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-* 
-* The Original Code is OpenELIS code.
-* 
-* The Initial Developer of the Original Code is The University of Iowa.
-* Portions created by The University of Iowa are Copyright 2006-2008. All
-* Rights Reserved.
-* 
-* Contributor(s): ______________________________________.
-* 
-* Alternatively, the contents of this file marked
-* "Separately-Licensed" may be used under the terms of a UIRF Software
-* license ("UIRF Software License"), in which case the provisions of a
-* UIRF Software License are applicable instead of those above. 
-*/
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
 package org.openelis.domain;
 
 import org.openelis.gwt.common.RPC;
@@ -36,16 +36,10 @@ public class SecuritySystemUserDO implements RPC {
     protected boolean         isEmployee, isActive;
 
     public SecuritySystemUserDO() {
-
     }
 
-    public SecuritySystemUserDO(Integer id,
-                                String loginName,
-                                String lastName,
-                                String firstName,
-                                String initials,
-                                String isEmployee,
-                                String isActive) {
+    public SecuritySystemUserDO(Integer id, String loginName, String lastName, String firstName,
+                                String initials, String isEmployee, String isActive) {
         this.id = id;
         this.loginName = loginName;
         this.lastName = lastName;
@@ -57,13 +51,8 @@ public class SecuritySystemUserDO implements RPC {
             this.isActive = true;
     }
 
-    public SecuritySystemUserDO(Integer id,
-                                String loginName,
-                                String lastName,
-                                String firstName,
-                                String initials,
-                                boolean isEmployee,
-                                boolean isActive) {
+    public SecuritySystemUserDO(Integer id, String loginName, String lastName, String firstName,
+                                String initials, boolean isEmployee, boolean isActive) {
         this.id = id;
         this.loginName = loginName;
         this.lastName = lastName;
@@ -102,5 +91,4 @@ public class SecuritySystemUserDO implements RPC {
     public String getInitials() {
         return initials;
     }
-
 }
