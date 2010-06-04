956e40f1a9fbe4e96847435a2c0c2134ba3a5b3b
akampoow <akampoow@gmail.com>
2010-06-04 19:58:33 +0000

    Fixed a few bugs on Test and Fill Order screens. Made some changes to the code for the Order and Inventory Item and Shipping screens

--- gwt/src/org/openelis/modules/order/client/FillTab.java
+++ gwt/src/org/openelis/modules/order/client/FillTab.java
@@ -20,16 +20,15 @@ import org.openelis.manager.StorageLocationManager;
 import org.openelis.meta.OrderMeta;
 
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Widget;
 
 public class FillTab extends Screen {
 
-    private OrderManager          manager;
-    private TableWidget           table;
-    private boolean               loaded;
-    
-    private int                   numColumns;
-    
+    private OrderManager manager;
+    private TableWidget  table;
+    private boolean      loaded, hasExtraCols;
+
+    private int          numColumns;
+
     public FillTab(ScreenDefInt def, ScreenWindow window) {
         service = new ScreenService("controller?service=org.openelis.modules.order.server.OrderService");
 
@@ -45,20 +44,27 @@ public class FillTab extends Screen {
             public void onDataChange(DataChangeEvent event) {
                 table.load(getTableModel());
             }
-            
+
             public void onStateChange(StateChangeEvent<State> event) {
                 table.enable(true);
                 table.setQueryMode(false);
             }
         });
-        
+
         numColumns = table.getColumns().size();
 
         table.addBeforeCellEditedHandler(new BeforeCellEditedHandler() {
-            public void onBeforeCellEdited(BeforeCellEditedEvent event) {                                
-                event.cancel();                                                    
+            public void onBeforeCellEdited(BeforeCellEditedEvent event) {
+                event.cancel();
             }
         });
+
+        //
+        // This class is responsible for managing a table that can show
+        // variable number of columns. One table has InventoryReceiptReceivedDate
+        // and the other one does not
+        //
+        hasExtraCols = table.getColumnWidget(OrderMeta.getInventoryReceiptReceivedDate()) != null;
     }
 
     private ArrayList<TableDataRow> getTableModel() {
@@ -66,8 +72,8 @@ public class FillTab extends Screen {
         String location;
         InventoryXUseViewDO data;
         ArrayList<TableDataRow> model;
+
         TableDataRow row;
-        Widget widget;
         OrderFillManager man;
 
         model = new ArrayList<TableDataRow>();
@@ -77,30 +83,25 @@ public class FillTab extends Screen {
         try {
             man = manager.getFills();
             count = man.count();
+
             for (i = 0; i < count; i++ ) {
                 data = (InventoryXUseViewDO)man.getFillAt(i);
                 row = new TableDataRow(numColumns);
-                location = StorageLocationManager.getLocationForDisplay(data.getStorageLocationName(),
+                location = StorageLocationManager.getLocationForDisplay(
+                                                                        data.getStorageLocationName(),
                                                                         data.getStorageLocationUnitDescription(),
-                                                                        data.getStorageLocationLocation());                                
+                                                                        data.getStorageLocationLocation());
                 row.cells.get(0).setValue(data.getInventoryItemName());
                 row.cells.get(1).setValue(location);
                 row.cells.get(2).setValue(data.getQuantity());
                 row.cells.get(3).setValue(data.getInventoryLocationLotNumber());
                 row.cells.get(4).setValue(data.getInventoryLocationExpirationDate());
-                
-                widget = table.getColumnWidget(OrderMeta.getInventoryReceiptReceivedDate());                
-                if(widget != null) 
+
+                if (hasExtraCols) {
                     row.cells.get(5).setValue(data.getInventoryReceiptReceivedDate());
-                
-                widget = table.getColumnWidget(OrderMeta.getInventoryReceiptUnitCost());                
-                if(widget != null) 
                     row.cells.get(6).setValue(data.getInventoryReceiptUnitCost());
-                
-                widget = table.getColumnWidget(OrderMeta.getInventoryReceiptExternalReference());                
-                if(widget != null) 
-                    row.cells.get(7).setValue(data.getInventoryReceiptExternalReference());                                
-                
+                    row.cells.get(7).setValue(data.getInventoryReceiptExternalReference());
+                }
                 model.add(row);
             }
         } catch (Exception e) {
