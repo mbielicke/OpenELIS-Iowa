d6f0d367052cb8383f3d1f86f75d38909bade66d
akampoow <akampoow@gmail.com>
2011-03-25 20:05:59 +0000

    Changes made in order to fix the validation done for analyses and order items

--- common/src/org/openelis/domain/ResultViewDO.java
+++ common/src/org/openelis/domain/ResultViewDO.java
@@ -7,7 +7,7 @@ public class ResultViewDO extends ResultDO {
     private static final long serialVersionUID = 1L;
     
     protected String analyte;
-    protected Integer rowGroup,resultGroup;
+    protected Integer rowGroup,testAnalyteTypeId, resultGroup;
     
     public ResultViewDO(){
         
@@ -15,10 +15,12 @@ public class ResultViewDO extends ResultDO {
     
     public ResultViewDO(Integer id, Integer analysisId, Integer testAnalyteId, Integer testResultId, 
                         String isColumn, Integer sortOrder, String isReportable, Integer analyteId,
-                        Integer typeId, String value, String analyte, Integer rowGroup, Integer resultGroup){
+                        Integer typeId, String value, String analyte, Integer rowGroup, Integer testAnalyteTypeId, 
+                        Integer resultGroup){
         super(id, analysisId, testAnalyteId, testResultId, isColumn, sortOrder, isReportable, analyteId, typeId, value);
         setAnalyte(analyte);
         setRowGroup(rowGroup);
+        setTestAnalyteTypeId(testAnalyteTypeId);
         setResultGroup(resultGroup);
     }
 
@@ -36,6 +38,14 @@ public class ResultViewDO extends ResultDO {
 
     public void setRowGroup(Integer rowGroup) {
         this.rowGroup = rowGroup;
+    }
+    
+    public Integer getTestAnalyteTypeId() {
+        return testAnalyteTypeId;
+    }
+
+    public void setTestAnalyteTypeId(Integer testAnalyteTypeId) {
+        this.testAnalyteTypeId = testAnalyteTypeId;
     }
 
     public Integer getResultGroup() {
