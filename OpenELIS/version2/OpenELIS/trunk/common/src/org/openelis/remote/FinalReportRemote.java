04194fac42a7e0bfa3383229f18232571131ac5e
abasu <abasu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2011-05-27 20:20:46 +0000

    Added methods to generate final report for environmental samples on the web

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/FinalReportRemote.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/remote/FinalReportRemote.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 
 import javax.ejb.Remote;
 
+import org.openelis.domain.IdNameVO;
+import org.openelis.domain.SampleEnvironmentalWebVO;
 import org.openelis.gwt.common.ReportStatus;
 import org.openelis.gwt.common.data.QueryData;
 import org.openelis.report.Prompt;
 
 @Remote
 public interface FinalReportRemote {
-    
+
     public ArrayList<Prompt> getPromptsForSingle() throws Exception;
-    
+
     public ArrayList<Prompt> getPromptsForBatch() throws Exception;
-    
+
     public ReportStatus runReportForSingle(ArrayList<QueryData> paramList) throws Exception;
 
     public ReportStatus runReportForPreview(ArrayList<QueryData> paramList) throws Exception;
-    
+
     public ReportStatus runReportForBatch(ArrayList<QueryData> paramList) throws Exception;
+
+    public ReportStatus runReportForWeb(ArrayList<QueryData> paramList) throws Exception;
+
+    public ArrayList<SampleEnvironmentalWebVO> getSampleListForEnvironmental(ArrayList<QueryData> fields) throws Exception;
+
+    public ArrayList<IdNameVO> getProjectList(ArrayList<QueryData> fields) throws Exception;
 }
