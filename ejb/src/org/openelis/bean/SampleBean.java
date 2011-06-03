6f7812c9348c4692366cc6f32b0d759112b22f81
abasu <basuamrita718@gmail.com>
2011-06-03 13:52:37 +0000

    changes made for web final report

--- ejb/src/org/openelis/bean/SampleBean.java
+++ ejb/src/org/openelis/bean/SampleBean.java
@@ -1,28 +1,28 @@
-/** Exhibit A - UIRF Open-source Based Public Software License.
-* 
-* The contents of this file are subject to the UIRF Open-source Based
-* Public Software License(the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-* openelis.uhl.uiowa.edu
-* 
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific language governing rights and limitations
-* under the License.
-* 
-* The Original Code is OpenELIS code.
-* 
-* The Initial Developer of the Original Code is The University of Iowa.
-* Portions created by The University of Iowa are Copyright 2006-2008. All
-* Rights Reserved.
-* 
-* Contributor(s): ______________________________________.
-* 
-* Alternatively, the contents of this file marked
-* "Separately-Licensed" may be used under the terms of a UIRF Software
-* license ("UIRF Software License"), in which case the provisions of a
-* UIRF Software License are applicable instead of those above. 
-*/
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
 package org.openelis.bean;
 
 import java.util.ArrayList;
@@ -39,6 +39,7 @@ import javax.persistence.Query;
 
 import org.jboss.ejb3.annotation.SecurityDomain;
 import org.openelis.domain.IdAccessionVO;
+import org.openelis.domain.IdNameVO;
 import org.openelis.domain.SampleDO;
 import org.openelis.entity.Sample;
 import org.openelis.gwt.common.DataBaseUtil;
@@ -58,34 +59,47 @@ import org.openelis.util.QueryBuilderV2;
 public class SampleBean implements SampleLocal, SampleRemote {
 
     @PersistenceContext(unitName = "openelis")
-    private EntityManager manager;
+    private EntityManager           manager;
 
     private static final SampleMeta meta = new SampleMeta();
-    
+
     private static HashMap<String, String> wellOrgFieldMap, reportToAddressFieldMap;
-    
-    static {        
-       wellOrgFieldMap = new HashMap<String, String>();              
-       wellOrgFieldMap.put(SampleMeta.getWellOrganizationName(), SampleMeta.getWellOrganizationName());       
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressMultipleUnit(), SampleMeta.getWellReportToAddressMultipleUnit());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressStreetAddress(), SampleMeta.getWellReportToAddressStreetAddress());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressCity(), SampleMeta.getWellReportToAddressCity());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressState(), SampleMeta.getWellReportToAddressState());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressZipCode(), SampleMeta.getWellReportToAddressZipCode());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressWorkPhone(), SampleMeta.getWellReportToAddressWorkPhone());
-       wellOrgFieldMap.put(SampleMeta.getWellReportToAddressFaxPhone(), SampleMeta.getWellReportToAddressFaxPhone());
-       
-       reportToAddressFieldMap = new HashMap<String, String>();
-       reportToAddressFieldMap.put(SampleMeta.getWellReportToName(), SampleMeta.getWellReportToName());
-       reportToAddressFieldMap.put(SampleMeta.getAddressMultipleUnit(), SampleMeta.getAddressMultipleUnit());
-       reportToAddressFieldMap.put(SampleMeta.getAddressStreetAddress(), SampleMeta.getAddressStreetAddress());
-       reportToAddressFieldMap.put(SampleMeta.getAddressCity(), SampleMeta.getAddressCity());
-       reportToAddressFieldMap.put(SampleMeta.getAddressState(), SampleMeta.getAddressState());
-       reportToAddressFieldMap.put(SampleMeta.getAddressZipCode(), SampleMeta.getAddressZipCode());
-       reportToAddressFieldMap.put(SampleMeta.getAddressWorkPhone(), SampleMeta.getAddressWorkPhone());
-       reportToAddressFieldMap.put(SampleMeta.getAddressFaxPhone(), SampleMeta.getAddressFaxPhone());
+
+    static {
+        wellOrgFieldMap = new HashMap<String, String>();
+        wellOrgFieldMap.put(SampleMeta.getWellOrganizationName(),
+                            SampleMeta.getWellOrganizationName());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressMultipleUnit(),
+                            SampleMeta.getWellReportToAddressMultipleUnit());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressStreetAddress(),
+                            SampleMeta.getWellReportToAddressStreetAddress());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressCity(),
+                            SampleMeta.getWellReportToAddressCity());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressState(),
+                            SampleMeta.getWellReportToAddressState());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressZipCode(),
+                            SampleMeta.getWellReportToAddressZipCode());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressWorkPhone(),
+                            SampleMeta.getWellReportToAddressWorkPhone());
+        wellOrgFieldMap.put(SampleMeta.getWellReportToAddressFaxPhone(),
+                            SampleMeta.getWellReportToAddressFaxPhone());
+
+        reportToAddressFieldMap = new HashMap<String, String>();
+        reportToAddressFieldMap.put(SampleMeta.getWellReportToName(),
+                                    SampleMeta.getWellReportToName());
+        reportToAddressFieldMap.put(SampleMeta.getAddressMultipleUnit(),
+                                    SampleMeta.getAddressMultipleUnit());
+        reportToAddressFieldMap.put(SampleMeta.getAddressStreetAddress(),
+                                    SampleMeta.getAddressStreetAddress());
+        reportToAddressFieldMap.put(SampleMeta.getAddressCity(), SampleMeta.getAddressCity());
+        reportToAddressFieldMap.put(SampleMeta.getAddressState(), SampleMeta.getAddressState());
+        reportToAddressFieldMap.put(SampleMeta.getAddressZipCode(), SampleMeta.getAddressZipCode());
+        reportToAddressFieldMap.put(SampleMeta.getAddressWorkPhone(),
+                                    SampleMeta.getAddressWorkPhone());
+        reportToAddressFieldMap.put(SampleMeta.getAddressFaxPhone(),
+                                    SampleMeta.getAddressFaxPhone());
     }
-    
+
     public ArrayList<IdAccessionVO> query(ArrayList<QueryData> fields, int first, int max) throws Exception {
         String queryString, whereForFrom, sampleWhere, privateWellWhere;
         Query query;
@@ -95,28 +109,31 @@ public class SampleBean implements SampleLocal, SampleRemote {
 
         builder = new QueryBuilderV2();
         builder.setMeta(meta);
-        builder.setSelect("distinct new org.openelis.domain.IdAccessionVO(" + SampleMeta.getId() + ","+SampleMeta.getAccessionNumber()+") ");
+        builder.setSelect("distinct new org.openelis.domain.IdAccessionVO(" + SampleMeta.getId() +
+                          "," + SampleMeta.getAccessionNumber() + ") ");
         builder.constructWhere(fields);
         builder.setOrderBy(SampleMeta.getAccessionNumber());
-        
+
         whereForFrom = builder.getWhereClause();
-        
-        //for the well screen we have to link to the org table and the address table
-        //with the same textboxes.  So if they queried by these fields we need to add
-        //a link to the 2nd table
+
+        // for the well screen we have to link to the org table and the address
+        // table
+        // with the same textboxes. So if they queried by these fields we need
+        // to add
+        // a link to the 2nd table
         wellFields = new ArrayList<QueryData>();
-        privateWellWhere = createWhereFromWellFields(fields,wellFields);        
+        privateWellWhere = createWhereFromWellFields(fields, wellFields);
         builder.clearWhereClause();
         builder.constructWhere(fields);
         sampleWhere = builder.getWhereClause();
-        
-        queryString = builder.getSelectClause() + builder.getFromClause(whereForFrom)
-        + sampleWhere + privateWellWhere + builder.getOrderBy();
+
+        queryString = builder.getSelectClause() + builder.getFromClause(whereForFrom) +
+                      sampleWhere + privateWellWhere + builder.getOrderBy();
 
         query = manager.createQuery(queryString);
         query.setMaxResults(first + max);
-        
-        //add the well fields we created
+
+        // add the well fields we created
         fields.addAll(wellFields);
         builder.setQueryParams(query, fields);
 
@@ -126,17 +143,17 @@ public class SampleBean implements SampleLocal, SampleRemote {
             e.printStackTrace();
             throw e;
         }
-        
+
         if (list.isEmpty())
             throw new NotFoundException();
-        
+
         list = (ArrayList<IdAccessionVO>)DataBaseUtil.subList(list, first, max);
         if (list == null)
             throw new LastPageException();
 
         return (ArrayList<IdAccessionVO>)list;
     }
-    
+
     public SampleDO fetchById(Integer sampleId) throws Exception {
         Query query = manager.createNamedQuery("Sample.FetchById");
         query.setParameter("id", sampleId);
@@ -147,12 +164,12 @@ public class SampleBean implements SampleLocal, SampleRemote {
         } catch (Exception e) {
             throw new DatabaseException(e);
         }
-        
+
     }
-    
+
     public SampleDO fetchByAccessionNumber(Integer accessionNumber) throws Exception {
         Query query;
-        
+
         query = manager.createNamedQuery("Sample.FetchByAccessionNumber");
         query.setParameter("accession", accessionNumber);
         try {
@@ -163,35 +180,43 @@ public class SampleBean implements SampleLocal, SampleRemote {
             throw new DatabaseException(e);
         }
     }
-    
+
     public ArrayList<Object[]> fetchSamplesForFinalReportBatch() throws Exception {
         Query query;
-               
-        query = manager.createNamedQuery("Sample.FetchSamplesForFinalReportBatch");              
+
+        query = manager.createNamedQuery("Sample.FetchSamplesForFinalReportBatch");
         return DataBaseUtil.toArrayList(query.getResultList());
     }
-    
+
     public ArrayList<Object[]> fetchSamplesForFinalReportSingle(Integer sampleId) throws Exception {
         Query query;
-               
+
         query = manager.createNamedQuery("Sample.FetchSamplesForFinalReportSingle");
         query.setParameter("sampleId", sampleId);
         return DataBaseUtil.toArrayList(query.getResultList());
     }
-    
+
     public ArrayList<Object[]> fetchSamplesForFinalReportPreview(Integer sampleId) throws Exception {
         Query query;
-               
+
         query = manager.createNamedQuery("Sample.FetchSamplesForFinalReportPreview");
         query.setParameter("sampleId", sampleId);
         return DataBaseUtil.toArrayList(query.getResultList());
     }
 
+    public ArrayList<IdNameVO> fetchProjectsForOrganizations(ArrayList<Integer> organizationIdList) throws Exception {
+        Query query;
+
+        query = manager.createNamedQuery("Sample.FetchProjectsForOrganizations");
+        query.setParameter("organizationIds", organizationIdList);
+        return DataBaseUtil.toArrayList(query.getResultList());
+    }
+
     public SampleDO add(SampleDO data) {
         Sample entity;
-        
+
         manager.setFlushMode(FlushModeType.COMMIT);
-        
+
         entity = new Sample();
         entity.setNextItemSequence(data.getNextItemSequence());
         entity.setDomain(data.getDomain());
@@ -207,21 +232,21 @@ public class SampleBean implements SampleLocal, SampleRemote {
         entity.setPackageId(data.getPackageId());
         entity.setClientReference(data.getClientReference());
         entity.setReleasedDate(data.getReleasedDate());
-        
+
         manager.persist(entity);
         data.setId(entity.getId());
-        
+
         return data;
     }
 
     public SampleDO update(SampleDO data) throws Exception {
         Sample entity;
-        
-        if (!data.isChanged())
+
+        if ( !data.isChanged())
             return data;
-        
+
         manager.setFlushMode(FlushModeType.COMMIT);
-        
+
         entity = manager.find(Sample.class, data.getId());
         entity.setNextItemSequence(data.getNextItemSequence());
         entity.setDomain(data.getDomain());
@@ -237,53 +262,55 @@ public class SampleBean implements SampleLocal, SampleRemote {
         entity.setPackageId(data.getPackageId());
         entity.setClientReference(data.getClientReference());
         entity.setReleasedDate(data.getReleasedDate());
-        
+
         return data;
     }
-    
-    private String createWhereFromWellFields(ArrayList<QueryData> fields,ArrayList<QueryData> wellFields){
+
+    private String createWhereFromWellFields(ArrayList<QueryData> fields,
+                                             ArrayList<QueryData> wellFields) {
         int i;
         String whereClause;
         QueryFieldUtil qField;
-        QueryData field;        
-        
+        QueryData field;
+
         whereClause = "";
         //
-        // we extract the fields that belong only to sample private well 
+        // we extract the fields that belong only to sample private well
         // and its related organization/report to; we need to remove these
-        // fields from the list sent to the bean because we don't want them to 
+        // fields from the list sent to the bean because we don't want them to
         // be included in the where clause created from those fields, because we
-        // would get repetitive and thus erroneous clauses     
+        // would get repetitive and thus erroneous clauses
         //
-        for (i = 0; i < fields.size(); i++) {
+        for (i = 0; i < fields.size(); i++ ) {
             field = fields.get(i);
             qField = new QueryFieldUtil();
             qField.parse(field.query);
-            
+
             if (wellOrgFieldMap.get(field.key) != null) {
                 wellFields.add(fields.remove(i));
-                i--;
-            } else if (reportToAddressFieldMap.get(field.key) != null){
+                i-- ;
+            } else if (reportToAddressFieldMap.get(field.key) != null) {
                 wellFields.add(fields.remove(i));
-                i--;
+                i-- ;
             }
-        }       
-        
+        }
+
         //
-        // we create a where clause from the fields obtained from the previous code
-        // 
-        for (i = 0; i < wellFields.size(); i++) {
+        // we create a where clause from the fields obtained from the previous
+        // code
+        //
+        for (i = 0; i < wellFields.size(); i++ ) {
             field = wellFields.get(i);
             qField = new QueryFieldUtil();
             qField.parse(field.query);
-            
-            if (i%2 ==0) {
-                whereClause += " and ( "+QueryBuilderV2.getQueryNoOperand(qField, field.key);      
+
+            if (i % 2 == 0) {
+                whereClause += " and ( " + QueryBuilderV2.getQueryNoOperand(qField, field.key);
             } else {
-                whereClause += " or "+QueryBuilderV2.getQueryNoOperand(qField, field.key) + " ) ";
+                whereClause += " or " + QueryBuilderV2.getQueryNoOperand(qField, field.key) + " ) ";
             }
         }
         return whereClause;
-    }    
-    
+    }
+
 }
