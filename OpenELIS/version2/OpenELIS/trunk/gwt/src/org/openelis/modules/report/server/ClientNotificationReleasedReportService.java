08f57d73fd88eda5c65c59c8bea54ad82983611f
abasu <abasu@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2011-07-08 18:31:38 +0000

    Files added to include Client Email Notification Functionality

---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/report/server/ClientNotificationReleasedReportService.java
@@ -0,0 +1,32 @@
+package org.openelis.modules.report.server;
+
+import java.util.ArrayList;
+
+import org.openelis.gwt.common.ReportStatus;
+import org.openelis.gwt.common.data.Query;
+import org.openelis.persistence.EJBFactory;
+import org.openelis.remote.ClientNotificationReleasedReportRemote;
+import org.openelis.remote.VolumeReportRemote;
+import org.openelis.report.Prompt;
+import org.openelis.util.SessionManager;
+
+public class ClientNotificationReleasedReportService {
+    public ArrayList<Prompt> getPrompts() throws Exception{
+        return remote().getPrompts();      
+    }
+    
+    public ReportStatus runReport(Query query) throws Exception { 
+        ReportStatus st;
+        
+        st = remote().runReport(query.getFields());
+      // if (st.getStatus() == ReportStatus.Status.SAVED)
+         // SessionManager.getSession().setAttribute(st.getMessage(), st);
+
+        return st;
+    }
+    
+    private ClientNotificationReleasedReportRemote remote() {
+        return (ClientNotificationReleasedReportRemote)EJBFactory.lookup("openelis/ClientNotificationReleasedReportBean/remote");
+    } 
+
+}
