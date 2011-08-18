d2eb932d9ac31ff0251b352014060ab171cf86a4
abasu <basuamrita718@gmail.com>
2011-08-18 18:47:12 +0000

    Added SampleStatusReportBean for Sample Status Web Report. Removed methods from FinalReportBean

---
+++ ejb/src/org/openelis/bean/SampleStatusReportBean.java
@@ -0,0 +1,171 @@
+/** Exhibit A - UIRF Open-source Based Public Software License.
+* 
+* The contents of this file are subject to the UIRF Open-source Based
+* Public Software License(the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+* openelis.uhl.uiowa.edu
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+* 
+* The Original Code is OpenELIS code.
+* 
+* The Initial Developer of the Original Code is The University of Iowa.
+* Portions created by The University of Iowa are Copyright 2006-2008. All
+* Rights Reserved.
+* 
+* Contributor(s): ______________________________________.
+* 
+* Alternatively, the contents of this file marked
+* "Separately-Licensed" may be used under the terms of a UIRF Software
+* license ("UIRF Software License"), in which case the provisions of a
+* UIRF Software License are applicable instead of those above. 
+*/
+package org.openelis.bean;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.jboss.ejb3.annotation.SecurityDomain;
+import org.openelis.domain.IdAccessionVO;
+import org.openelis.domain.IdNameVO;
+import org.openelis.domain.SampleStatusWebReportVO;
+import org.openelis.gwt.common.DataBaseUtil;
+import org.openelis.gwt.common.data.QueryData;
+import org.openelis.local.DictionaryLocal;
+import org.openelis.local.SampleLocal;
+import org.openelis.meta.SampleMeta;
+import org.openelis.meta.SampleWebMeta;
+import org.openelis.remote.SampleStatusReportRemote;
+import org.openelis.util.QueryBuilderV2;
+import org.openelis.utils.EJBFactory;
+import org.openelis.utils.ReportUtil;
+
+@Stateless
+@SecurityDomain("openelis")
+public class SampleStatusReportBean implements SampleStatusReportRemote {
+    
+    @EJB
+    private SampleLocal                sample;
+
+   @EJB
+    private DictionaryLocal            dictionary;
+
+
+    @PersistenceContext(unitName = "openelis")
+    private EntityManager              manager;
+
+    private static Integer             sampleNotVerifiedId;
+
+    private static final SampleWebMeta meta = new SampleWebMeta();
+
+    @PostConstruct
+    public void init() {
+        try {
+            sampleNotVerifiedId = dictionary.fetchBySystemName("sample_not_verified").getId();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<SampleStatusWebReportVO> getSampleListForSampleStatusReport(ArrayList<QueryData> fields) throws Exception {
+        int id;
+        Query query;
+        QueryBuilderV2 builder;
+        ArrayList<SampleStatusWebReportVO> returnList;
+        ArrayList<IdAccessionVO> tempList;
+        ArrayList<Integer> idList;
+
+        returnList = new ArrayList<SampleStatusWebReportVO>();
+
+        builder = new QueryBuilderV2();
+
+        builder.setMeta(meta);
+        builder.setSelect("distinct new org.openelis.domain.IdAccessionVO(" +
+                          SampleWebMeta.getId() + ", " + SampleWebMeta.getAccessionNumber() + ") ");
+        builder.addWhere(SampleWebMeta.getStatusId() + " != " + sampleNotVerifiedId);
+        builder.constructWhere(fields);
+        builder.setOrderBy(SampleWebMeta.getAccessionNumber());
+
+        query = manager.createQuery(builder.getEJBQL());
+        builder.setQueryParams(query, fields);
+        tempList = DataBaseUtil.toArrayList(query.getResultList());
+
+        if (tempList == null || tempList.size() == 0)
+            return returnList;
+
+        idList = new ArrayList<Integer>();
+        /*
+         * From tempList we create an arrayList of sample ids, which is sent as
+         * a parameter for getting sample/analysis information.
+         */
+        for (int i = 0; i < tempList.size(); i++ ) {
+            id = tempList.get(i).getId();
+            idList.add(id);
+        }
+        /*
+         * Query all the three domains to get the sample/analysis information
+         * for the list of sample ids
+         */
+        returnList.addAll(sample.fetchSampleAnalysisInfoForSampleStatusReportEnvironmental(idList));
+        returnList.addAll(sample.fetchSampleAnalysisInfoForSampleStatusReportPrivateWell(idList));
+        returnList.addAll(sample.fetchSampleAnalysisInfoForSampleStatusReportSDWIS(idList));
+
+        /*
+         * Sort the list by accession # of samples. We do the sorting in the back end instead of doing it in the front, 
+         * since the comparator for rows in the table cannot sort correctly if the value in column based on which
+         * the comparison is made is null. In this case, since the analysis rows don't show accession number on the screen, 
+         * the value in the first column of these rows is null and that is the column used for sorting.
+         */
+        Collections.sort(returnList, new SampleComparator());
+        return returnList;
+    }
+    
+    public ArrayList<IdNameVO> getSampleStatusProjectList() throws Exception {
+        String clause;
+        ArrayList<Integer> list;
+        ArrayList<IdNameVO> projectList;
+        
+        projectList = new ArrayList<IdNameVO>();
+        clause = EJBFactory.getUserCache().getPermission().getModule("w_status").getClause();
+        list = ReportUtil.parseClauseAsArrayList(clause)
+                         .get(SampleMeta.getSampleOrgOrganizationId());
+        /*
+         * if clause is null, then the previous method returns an empty HashMap,
+         * so we need to check if the list is empty or not.
+         */
+        if (list == null)
+            return new ArrayList<IdNameVO>();
+        /*
+         * Adding projects from environment and private domain into projectList.
+         * Since Project name is of type char, so we can't use NamedNativeQuery
+         */
+        projectList.addAll(sample.fetchProjectsForOrganizations(list));
+        projectList.addAll(sample.fetchProjectsForPrivateOrganizations(list));
+        
+        Collections.sort(projectList, new ProjectComparator());
+        return projectList;
+    }
+    
+    class SampleComparator implements Comparator<SampleStatusWebReportVO> {
+        public int compare(SampleStatusWebReportVO s1, SampleStatusWebReportVO s2) {
+            return s1.getAccessionNumber() - s2.getAccessionNumber();
+        }
+    }
+    
+    class ProjectComparator implements Comparator<IdNameVO> {
+        public int compare(IdNameVO p1, IdNameVO p2) {
+            return p1.getId() - p2.getId();
+        }
+    }
+}
