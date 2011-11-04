246e30f6cb5863feffb2cf97de027d5a933a8406
mbielick <mbielicke@users.noreply.github.com>
2011-11-04 20:52:42 +0000

    Fixed SampleManger to only set the released date on sample if the date is null.

--- common/src/org/openelis/manager/SampleManager.java
+++ common/src/org/openelis/manager/SampleManager.java
@@ -458,7 +458,7 @@ public class SampleManager implements RPC, HasAuxDataInt {
             statusId = proxy().samReleasedId;
 
         if (statusId != null && statusId != oldStatusId) {
-            if (statusId == proxy().samReleasedId) {
+            if (statusId == proxy().samReleasedId && sample.getReleasedDate() == null) {
                 sample.setReleasedDate(proxy().getCurrentDatetime(Datetime.YEAR, Datetime.MINUTE));
             } else {
                 sample.setReleasedDate(null);
