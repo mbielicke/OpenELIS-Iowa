0e8c1e88d52a05c3105f9b5b60e23675ed40a1e6
akampoow <akampoow@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2011-12-02 17:45:39 +0000

    Fixed the errors of empty notes being added to the database and the wrong storage location for storing a kit being selected on Build Kits screen

--- OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/note/client/EditNoteScreen.java
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/note/client/EditNoteScreen.java
@@ -324,12 +324,17 @@ public class EditNoteScreen extends Screen implements HasActionHandlers<EditNote
         clearErrors();
         if (validate()) {
             note.setSubject(subject.getText());
-            //
-            // Convert bunch of repeated chars to reasonable white spaces
-            //
             trimText = text.getText();
-            trimText = trimText.replaceAll("\n{3, }","\n\n"); // 3 or more to 2
-            trimText = trimText.replaceAll(" {2, }"," ");     // 2 or more to 1
+            //
+            // a PatternSyntaxException is thrown if trimText is empty
+            //
+            if (!DataBaseUtil.isEmpty(trimText)) {
+                //
+                // Convert bunch of repeated chars to reasonable white spaces
+                //
+                trimText = trimText.replaceAll("\n{3, }","\n\n"); // 3 or more to 2
+                trimText = trimText.replaceAll(" {2, }"," ");     // 2 or more to 1
+            }
             note.setText(trimText);
             setState(State.DEFAULT);
             ActionEvent.fire(this, Action.OK, null);
