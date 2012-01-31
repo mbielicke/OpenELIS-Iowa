a0dd413acbaab7e4fc8761749b3b78109d27b2cb
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2012-01-31 20:01:22 +0000

    Moved the filter codes to lib. Cleanup class path and debug jboss 6.1 path.

--- OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/server/HostedFilter.java
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/server/HostedFilter.java
@@ -25,142 +25,29 @@
  */
 package org.openelis.server;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Logger;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.openelis.gwt.common.PermissionException;
 import org.openelis.gwt.common.SystemUserPermission;
-import org.openelis.gwt.server.ServiceUtils;
 import org.openelis.remote.UserCacheRemote;
-import org.openelis.util.SessionManager;
 
-public class HostedFilter implements Filter {
-    private static final long serialVersionUID = 1L;
+public class HostedFilter extends org.openelis.gwt.server.HostedFilter {
 
-    private String            user, password, locale;
-    private static Logger     log = Logger.getLogger(HostedFilter.class.getName());
-
-    public void init(FilterConfig config) throws ServletException {
-        ServiceUtils.props = "org.openelis.constants.OpenELISConstants";
-
-        locale = config.getInitParameter("Locale");
-        user = config.getInitParameter("User");
-        password = config.getInitParameter("Pass");
-    }
-
-    public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws IOException {
-        HttpServletRequest hreq = (HttpServletRequest)req;
-
-        //
-        // pass-through for images and if we are logged-in
-        //
-        if (hreq.getRequestURI().endsWith(".jpg") || hreq.getRequestURI().endsWith(".gif") ||
-            hreq.getSession().getAttribute("USER_NAME") != null) {
-            try {
-                chain.doFilter(req, response);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return;
-        }
-        //
-        // used for language binding
-        //
-        if (hreq.getParameter("locale") != null)
-            hreq.getSession().setAttribute("locale", req.getParameter("locale"));
-
-        //
-        // check to see if we have logged in session
-        //
-        if (hreq.getSession().getAttribute("USER_NAME") == null) {
-            try {
-                login(hreq, user, password, hreq.getRemoteAddr());
-                //
-                // register the session so we can access it statically in gwt
-                // code
-                //
-                SessionManager.setSession(hreq.getSession());
-
-                try {
-                    chain.doFilter(req, response);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return;
-            } catch (Exception e) {
-                ((HttpServletResponse)response).sendRedirect("BadUserIdOrPasswordInHostedMode.html");
-            }
-        }
-    }
-
-    public void destroy() {
-    }
-
-    /*
-     * log the user into the system by sending its credentials to JBOSS for
-     * authentication
-     */
-    private void login(HttpServletRequest req, String name, String password, String ipAddress) throws Exception {
+    protected void login(Properties props) throws Exception {
         InitialContext remotectx;
-        File propFile;
-        String parts, locale;
-        Properties props;
         UserCacheRemote remote;
         SystemUserPermission perm;
+        
+        remotectx = new InitialContext(props);
+        remote = (UserCacheRemote)remotectx.lookup("openelis/openelis.jar/UserCacheBean!org.openelis.remote.UserCacheRemote");
+        perm = remote.login(); 
 
-        try {
-            /*
-             * JBOSS dependent! Build the security principal by combining username;session-id;locale
-             * and then parse it in the back to get all the parts.
-             * see UserCacheBean.
-             * see OpenELISLDAPModule
-             * see OpenELISRolesModule 
-             */
-            locale = (String) req.getSession().getAttribute("locale");
-            parts = name + ";" + req.getSession().getId() + ";" + (locale==null?"en":locale);
-
-            propFile = new File("/usr/pub/http/var/jndi/jndi.properties");
-            props = new Properties();
-            props.load(new FileInputStream(propFile));
-            props.setProperty(InitialContext.INITIAL_CONTEXT_FACTORY,
-                              "org.jboss.security.jndi.LoginInitialContextFactory");
-            props.setProperty(InitialContext.SECURITY_PROTOCOL, "jboss-standard");
-            props.setProperty(Context.SECURITY_PRINCIPAL, parts);
-            props.setProperty(InitialContext.SECURITY_CREDENTIALS, password);
-
-            remotectx = new InitialContext(props);
-            remote = (UserCacheRemote)remotectx.lookup("openelis/openelis.jar/UserCacheBean!org.openelis.remote.UserCacheRemote");
-            
-            perm = remote.login();
-            //
-            // check to see if she has connect permission
-            //
-            if ( !perm.hasConnectPermission())
-                throw new PermissionException("NoPermission.html");
-            
-            req.getSession().setAttribute("UserPermission", perm);
-            req.getSession().setAttribute("jndiProps", props);
-            req.getSession().setAttribute("USER_NAME", name);
-
-            log.info("Login attempt for " + name + " succeeded");
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.info("Login attempt for " + name + " failed ");
-            throw e;
-        }
+        //
+        // check to see if she has connect permission
+        //
+        if (!perm.hasConnectPermission())
+          throw new PermissionException("NoPermission.html");
     }
 }
