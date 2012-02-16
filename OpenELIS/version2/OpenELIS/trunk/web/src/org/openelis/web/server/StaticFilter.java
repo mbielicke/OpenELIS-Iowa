375e18f9ae4cdc1399c223c43ffea59004e024f5
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2012-02-16 21:21:52 +0000

    Changed the hosted and static filter classes to use the new library base code. Added all the required login/filter tags to web.xml files

--- OpenELIS/version2/OpenELIS/trunk/web/src/org/openelis/web/server/StaticFilter.java
+++ OpenELIS/version2/OpenELIS/trunk/web/src/org/openelis/web/server/StaticFilter.java
@@ -1,5 +1,5 @@
 /**
-s * Exhibit A - UIRF Open-source Based Public Software License.
+ * Exhibit A - UIRF Open-source Based Public Software License.
  * 
  * The contents of this file are subject to the UIRF Open-source Based Public
  * Software License(the "License"); you may not use this file except in
@@ -25,254 +25,29 @@ s * Exhibit A - UIRF Open-source Based Public Software License.
  */
 package org.openelis.web.server;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Properties;
-import java.util.logging.Logger;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.openelis.gwt.common.PermissionException;
 import org.openelis.gwt.common.SystemUserPermission;
-import org.openelis.gwt.server.ServiceUtils;
 import org.openelis.remote.UserCacheRemote;
-import org.openelis.util.SessionManager;
-import org.openelis.util.XMLUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-/**
- * Elis controller is the entry point for Elis Web Application. This object
- * controls the workflow for processing request and response.
- * 
- */
-public class StaticFilter implements Filter {
+public class StaticFilter extends org.openelis.gwt.server.StaticFilter {
 
-    private static final long    serialVersionUID = 1L;
-    private static Logger        log              = Logger.getLogger("org.openelis.auth");
-    private static String        AppRoot;
-    private static int           LOGIN_LOCK_TIME  = 1000 * 60 * 10,     // minutes to lock user out
-                                 LOGIN_TRY_IP_CNT = 7,                  // max # of bad ip counts before being locked out
-                                 LOGIN_TRY_NM_CNT = 4;                  // max # of bad name counts before being locked out
-
-    public void init(FilterConfig config) throws ServletException {
-        AppRoot = config.getInitParameter("AppRoot");
-        ServiceUtils.props = "org.openelis.constants.OpenELISConstants";
-    }
-
-    public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws IOException {
-        String error = null;
-        HttpServletRequest hreq = (HttpServletRequest)req;
-
-        //
-        // register this session with SessionManager so we can access it
-        // statically in gwt code
-        //
-        SessionManager.setSession(hreq.getSession());
-
-        //
-        // pass-through for images and if we are logged-in
-        //
-        if (hreq.getRequestURI().endsWith(".jpg") || hreq.getRequestURI().endsWith(".gif") ||
-            hreq.getSession().getAttribute("USER_NAME") != null) {
-            try {
-                chain.doFilter(req, response);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return;
-        }
-
-        //
-        // used for language binding
-        //
-        if (hreq.getParameter("locale") != null)
-            hreq.getSession().setAttribute("locale", req.getParameter("locale"));
-
-        //
-        // check to see if we are coming from login screen
-        //
-        if (hreq.getParameter("username") != null) {            
-            try {
-                login(hreq, hreq.getParameter("username"), req.getParameter("password"),  
-                      hreq.getRemoteAddr());
-                try {
-                    chain.doFilter(req, response);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return;
-            } catch (Exception e) {
-                error = "authFailure";
-            }
-        }
-        //
-        // ask them to authenticate
-        //
-        try {
-            Document doc;
-            Element action;
-            
-            doc = XMLUtil.createNew("login");
-            action = doc.createElement("action");
-            action.appendChild(doc.createTextNode("OpenELIS.html"));
-            doc.getDocumentElement().appendChild(action);
-            if (error != null) {
-                Element errorEL = doc.createElement("error");
-                errorEL.appendChild(doc.createTextNode(error));
-                doc.getDocumentElement().appendChild(errorEL);
-            }
-            ((HttpServletResponse)response).setHeader("pragma", "no-cache");
-            ((HttpServletResponse)response).setHeader("Cache-Control", "no-cache");
-            ((HttpServletResponse)response).setHeader("Cache-Control", "no-store");
-            ((HttpServletResponse)response).setDateHeader("Expires", 0);
-            ((HttpServletResponse)response).setContentType("text/html");
-            ((HttpServletResponse)response).setCharacterEncoding("UTF-8");
-            response.getWriter().write(ServiceUtils.getXML(AppRoot + "login.xsl", doc));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-
-    public void destroy() {
-        System.out.println("in static filter distroy");
-    }
-
-    /*
-     * log the user into the system by sending its credentials to JBOSS for
-     * authentication
-     */
-    
-    private void login(HttpServletRequest req, String name, String password, String ipAddress) throws Exception {
-        File propFile;
-        String parts, locale;
-        Properties props;
-        InitialContext localctx, remotectx;
+    protected void login(Properties props) throws Exception {
+        InitialContext remotectx;
         UserCacheRemote remote;
         SystemUserPermission perm;
-        
-        try {
-            if (! LoginAttempt.isValid(name, ipAddress))
-                throw new PermissionException();
 
-            /*
-             * JBOSS dependent! Build the security principal by combining username;session-id;locale
-             * and then parse it in the back to get all the parts.
-             * see UserCacheBean.
-             * see OpenELISLDAPModule
-             * see OpenELISRolesModule 
-             */
-            locale = (String) req.getSession().getAttribute("locale");
-            parts = name + ";" + req.getSession().getId() + ";" + (locale==null?"en":locale);
+        remotectx = new InitialContext(props);
+        remote = (UserCacheRemote)remotectx.lookup("openelis/openelis.jar/UserCacheBean!org.openelis.remote.UserCacheRemote");
+        perm = remote.login();
 
-            localctx = new InitialContext();
-            propFile = new File((String)localctx.lookup( ("java:comp/env/openelisJNDI")));
-            props = new Properties();
-            props.load(new FileInputStream(propFile));
-            props.setProperty(InitialContext.INITIAL_CONTEXT_FACTORY, "org.jboss.security.jndi.LoginInitialContextFactory");
-            props.setProperty(InitialContext.SECURITY_PROTOCOL, "jboss-standard");
-            props.setProperty(Context.SECURITY_PRINCIPAL, parts);
-            props.setProperty(InitialContext.SECURITY_CREDENTIALS, password);
-
-            remotectx = new InitialContext(props);            
-            remote = (UserCacheRemote)remotectx.lookup("openelis/openelis.jar/UserCacheBean!org.openelis.remote.UserCacheRemote");
-            perm = remote.login();
-            //
-            // check to see if she has connect permission
-            //
-            if (!perm.hasConnectPermission())
-                throw new PermissionException();
-
-            req.getSession().setAttribute("UserPermission", perm);
-            req.getSession().setAttribute("jndiProps", props);
-            req.getSession().setAttribute("USER_NAME", name);
-
-            LoginAttempt.success(name, ipAddress);
-        } catch (Exception e) {
-            LoginAttempt.fail(name, ipAddress);
-            throw e;
-        }
-    }
-    
-    /*
-     * Simple class to manage login attempts
-     */
-    private static class LoginAttempt {
-        int tries;
-        long lastTime;
-
-        private static HashMap<String, LoginAttempt> failed = new HashMap<String, StaticFilter.LoginAttempt>();
-    
-        /**
-         * Checks to see if the user from ip address has exceeded the number of attempts trying
-         * to login into the system. 
-         */
-        public static boolean isValid(String name, String ipAddress) {
-            long cutoff;
-            LoginAttempt la;
-            
-            cutoff = System.currentTimeMillis() - LOGIN_LOCK_TIME;
-            
-            la = failed.get(ipAddress);
-            if (la != null && la.lastTime >= cutoff && la.tries >= LOGIN_TRY_IP_CNT)
-                return false;
-
-            la = failed.get(name);
-            if (la != null && la.lastTime >= cutoff && la.tries >= LOGIN_TRY_NM_CNT)
-                return false;
-
-            return true;
-        }
-        
-        /**
-         * Clears the failed list for the user and ip address.
-         * TODO: need a sliding window remove for clearing the ip address for better security.
-         */
-        public static void success(String name, String ipAddress) {
-            failed.remove(ipAddress);
-            failed.remove(name);
-
-            log.info("Login attempt for "+ name +" - "+ ipAddress + " succeeded");
-        }
-
-        /**
-         * Adds/increments the number of failed attempts from user and ip address.
-         */
-        public static void fail(String name, String ipAddress) {
-            long now;
-            LoginAttempt li, ln;
-            
-            now = System.currentTimeMillis();
-
-            li = failed.get(ipAddress);
-            if (li == null) {
-                li = new LoginAttempt();
-                failed.put(ipAddress, li);
-            }
-            li.lastTime = now;
-            li.tries = Math.min(li.tries+1, 9999);
-
-            ln = failed.get(name);
-            if (ln == null) {
-                ln = new LoginAttempt();
-                failed.put(name, ln);
-            }
-            ln.lastTime = now;
-            ln.tries = Math.min(ln.tries+1, 9999);
-
-            log.info("Login attempt for "+ name +" ["+ ln.tries +"]"+" - "+ ipAddress +" ["+ li.tries +"] failed ");
-        }
+        //
+        // check to see if she has connect permission
+        //
+        if ( !perm.hasConnectPermission())
+            throw new PermissionException("NoPermission.html");
     }
 }
