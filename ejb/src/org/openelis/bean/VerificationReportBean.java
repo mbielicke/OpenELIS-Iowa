0027a85e2aa5b096cfdd770fb6e3dc04b55f1d88
mbielick <mbielicke@users.noreply.github.com>
2012-09-06 20:31:46 +0000

    Changed the duplex printing option in Verification Report to be one that both HP and Lanier printers will accept.

--- ejb/src/org/openelis/bean/VerificationReportBean.java
+++ ejb/src/org/openelis/bean/VerificationReportBean.java
@@ -208,7 +208,7 @@ public class VerificationReportBean implements VerificationReportRemote {
             status.setPercentComplete(100);
 
             if (ReportUtil.isPrinter(printer)) {
-                printstat = ReportUtil.print(tempFile, printer, 1, "sides=two-sided-long-edge");
+                printstat = ReportUtil.print(tempFile, printer, 1, "Duplex=DuplexNoTumble");
                 status.setMessage(printstat).setStatus(ReportStatus.Status.PRINTED);
             } else {
                 tempFile = ReportUtil.saveForUpload(tempFile);
