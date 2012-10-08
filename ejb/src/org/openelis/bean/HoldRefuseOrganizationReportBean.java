f042387c6abb4c88689b4fb8bb40bb53f5ffd3b4
abasu <basuamrita718@gmail.com>
2012-10-08 16:09:30 +0000

    Files changed/added for Hold/Refuse Organization Report

---
+++ ejb/src/org/openelis/bean/HoldRefuseOrganizationReportBean.java
@@ -0,0 +1,182 @@
+/** Exhibit A - UIRF Open-source Based Public Software License.
+* 
+* The contents of this file are subject to the UIRF Open-source Based
+* Public Software License(the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+* openelis.uhl.uiowa.edu
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+* 
+* The Original Code is OpenELIS code.
+* 
+* The Initial Developer of the Original Code is The University of Iowa.
+* Portions created by The University of Iowa are Copyright 2006-2008. All
+* Rights Reserved.
+* 
+* Contributor(s): ______________________________________.
+* 
+* Alternatively, the contents of this file marked
+* "Separately-Licensed" may be used under the terms of a UIRF Software
+* license ("UIRF Software License"), in which case the provisions of a
+* UIRF Software License are applicable instead of those above. 
+*/
+package org.openelis.bean;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+import org.jboss.ejb3.annotation.SecurityDomain;
+import org.openelis.domain.OptionListItem;
+import org.openelis.gwt.common.ReportStatus;
+import org.openelis.gwt.common.data.QueryData;
+import org.openelis.local.PrinterCacheLocal;
+import org.openelis.local.SessionCacheLocal;
+import org.openelis.remote.HoldRefuseOrganizationReportRemote;
+import org.openelis.report.Prompt;
+import org.openelis.utils.ReportUtil;
+
+@Stateless
+@SecurityDomain("openelis")
+@Resource(name = "jdbc/OpenELISDB", type = DataSource.class, authenticationType = javax.annotation.Resource.AuthenticationType.CONTAINER, mappedName = "java:/OpenELISDS")
+
+public class HoldRefuseOrganizationReportBean implements HoldRefuseOrganizationReportRemote {
+
+    @Resource
+    private SessionContext  ctx;
+
+    @EJB
+    private SessionCacheLocal session;
+
+    @EJB
+    private PrinterCacheLocal printers;
+
+    /*
+     * Returns the prompt for a single re-print
+     */
+    public ArrayList<Prompt> getPrompts() throws Exception {
+        ArrayList<OptionListItem> prn;
+        ArrayList<Prompt> p;
+
+        try {
+            p = new ArrayList<Prompt>();
+
+            prn = printers.getListByType("pdf");
+            prn.add(0, new OptionListItem("-view-", "View in PDF"));
+            p.add(new Prompt("PRINTER", Prompt.Type.ARRAY).setPrompt("Printer:")
+                                                          .setWidth(200)
+                                                          .setOptionList(prn)
+                                                          .setMutiSelect(false)
+                                                          .setRequired(true));
+            return p;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    /*
+     * Execute the report and send its output to specified location
+     */
+    public ReportStatus runReport(ArrayList<QueryData> paramList) throws Exception {
+        URL url;
+        File tempFile;
+        HashMap<String, QueryData> param;
+        HashMap<String, Object> jparam;
+        Connection con;
+        ReportStatus status;
+        JasperReport jreport;
+        JasperPrint jprint;
+        JRExporter jexport;
+        String printer, dir, printstat;
+
+        /*
+         * push status into session so we can query it while the report is
+         * running
+         */
+        status = new ReportStatus();
+        session.setAttribute("HoldRefuseOrganizationReport", status);
+
+        /*
+         * recover all the params and build a specific where clause
+         */
+        param = ReportUtil.getMapParameter(paramList);
+
+        printer = ReportUtil.getSingleParameter(param, "PRINTER");
+
+        /*
+         * start the report
+         */
+        con = null;
+        try {
+            status.setMessage("Initializing report");
+
+            con = ReportUtil.getConnection(ctx);
+            url = ReportUtil.getResourceURL("org/openelis/report/holdRefuseOrganization/main.jasper");
+            dir = ReportUtil.getResourcePath(url);
+
+            tempFile = File.createTempFile("test", ".pdf", new File("/tmp"));
+
+            jparam = new HashMap<String, Object>();
+            jparam.put("SUBREPORT_DIR", dir);
+
+            status.setMessage("Loading report");
+
+            jreport = (JasperReport)JRLoader.loadObject(url);
+            jprint = JasperFillManager.fillReport(jreport, jparam, con);
+            jexport = new JRPdfExporter();
+            jexport.setParameter(JRExporterParameter.OUTPUT_STREAM, new FileOutputStream(tempFile));
+            jexport.setParameter(JRExporterParameter.JASPER_PRINT, jprint);
+
+            status.setMessage("Outputing report").setPercentComplete(20);
+
+            jexport.exportReport();
+
+            status.setPercentComplete(100);
+
+            if (ReportUtil.isPrinter(printer)) {
+                printstat = ReportUtil.print(tempFile, printer, 1);
+                status.setMessage(printstat).setStatus(ReportStatus.Status.PRINTED);
+            } else {
+                tempFile = ReportUtil.saveForUpload(tempFile);
+                status.setMessage(tempFile.getName())
+                      .setPath(ReportUtil.getSystemVariableValue("upload_stream_directory"))
+                      .setStatus(ReportStatus.Status.SAVED);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return status;
+    }
+
+
+}
