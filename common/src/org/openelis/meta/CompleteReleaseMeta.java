74181695ce79efb3e2337d8d85eb1400cbdbd701
Travis Schmidt <travis.schmidt@gmail.com>
2012-12-12 19:34:16 +0000

    Branch to upgrade OpenELIS

--- common/src/org/openelis/meta/CompleteReleaseMeta.java
+++
@@ -1,9 +0,0 @@
-package org.openelis.meta;
-
-public class CompleteReleaseMeta extends SampleMeta {
-
-	@Override
-	public String buildFrom(String where) {
-		return " Sample _sample, IN (_sample.sampleItem) _sampleItem, IN (_sampleItem.analysis) _analysis LEFT JOIN _analysis.test _test LEFT JOIN _test.method _method";
-	}
-}
