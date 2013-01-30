9d8017fa75380b5273abe0e3a3ebecafb84307b5
dshirazi <dshirazi@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2013-01-30 20:39:57 +0000

    Initial Branch for the new manager classes removing the need for proxy and adding bulk load

---
+++ OpenELIS/version2/OpenELIS/branches/newSampleManager/ejb/src/org/openelis/bean/PrinterCacheBean.java
@@ -0,0 +1,124 @@
+package org.openelis.bean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Asynchronous;
+import javax.ejb.Singleton;
+import javax.print.DocFlavor;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+
+import org.jboss.security.annotation.SecurityDomain;
+import org.jboss.ejb3.annotation.TransactionTimeout;
+import org.jboss.security.annotation.SecurityDomain;
+import org.openelis.gwt.common.OptionListItem;
+import org.openelis.utils.Printer;
+
+/**
+ * This class provides a simple cached interface to the printers.
+ */
+@Singleton
+@SecurityDomain("openelis")
+public class PrinterCacheBean {
+
+    protected ArrayList<Printer>       printerList;
+    protected HashMap<String, Printer> printerHash;
+    
+    private static final Logger       log = Logger.getLogger("openelis");
+
+    @PostConstruct
+    public void init() {
+        printerList = new ArrayList<Printer>();
+        printerHash = new HashMap<String, Printer>();
+        refresh();
+    }
+
+    /**
+     * Returns the list of printers defined for this server
+     */
+    public ArrayList<OptionListItem> getList() {
+        ArrayList<OptionListItem> tempList;
+
+        tempList = new ArrayList<OptionListItem>();
+        for (Printer p : printerList) 
+            tempList.add(new OptionListItem(p.getName(), p.getDescription()));        
+
+        return tempList;
+    }
+
+    /**
+     * Returns a printer specified by name
+     */
+    public Printer getPrinterByName(String name) {
+        return printerHash.get(name);
+    }
+
+    /**
+     * Returns a list of printers that match the specified type. Currently the types are
+     * "pdf", "zpl", and "unk". 
+     */
+    public ArrayList<OptionListItem> getListByType(String type) {
+        ArrayList<OptionListItem> tempList;
+
+        tempList = new ArrayList<OptionListItem>();
+        if (type != null) {
+            for (Printer p : printerList) {
+                if (type.equals(p.getType()))
+                    tempList.add(new OptionListItem(p.getName(), p.getDescription()));
+            }
+        }
+        return tempList;
+    }    
+
+    /**
+     * Method to refresh the printer list cache
+     */
+    @Asynchronous
+    @TransactionTimeout(30)
+    public void refresh() {
+        String name, type;
+        Printer printer;
+        PrintService printers[];
+        ArrayList<Printer> tempList;
+        HashMap<String, Printer> tempHash;
+
+        tempList = new ArrayList<Printer>();
+        tempHash = new HashMap<String, Printer>();
+
+        try {
+            printers = PrintServiceLookup.lookupPrintServices(null, null);
+            for (PrintService p : printers) {
+                name = p.getName();
+                /*
+                 * This is a patch until we can figure out how to distinguish barcode
+                 * printers from their mime type
+                 */
+                type = "unk";
+                if (name.contains("_bar")) {
+                    type = "zpl";
+                } else if (p.getSupportedDocFlavors() != null) {
+                    for (DocFlavor f : p.getSupportedDocFlavors())
+                        if (f.getMimeType().contains("application/pdf")) {
+                            type = "pdf";
+                            break;
+                        }
+                }
+                printer = new Printer(name, name, type, p);
+                tempList.add(printer);
+                tempHash.put(name, printer);
+            }
+        } catch (Exception ioE) {
+            log.log(Level.SEVERE, "Could not look up print services ", ioE);
+            tempList = null;
+        } finally {
+            if (tempList != null) {
+                printerList = tempList;
+                printerHash = tempHash;
+            }
+        }
+    }
+}
---
+++ OpenELIS/version2/OpenELIS/branches/newSampleManager/ejb/src/org/openelis/utils/Audit.java
@@ -0,0 +1,155 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.utils;
+
+import java.util.ArrayList;
+
+import org.openelis.domain.Constants;
+import org.openelis.gwt.common.DataBaseUtil;
+import org.openelis.util.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * This class is used by entities and AuditUtil to manage a list of field for
+ * auditing purposes. The field values that are saved in the history table
+ * represent the previous snapshot; the latest values are stored in their
+ * respective table.
+ */
+public class Audit {
+
+    protected class Field {
+        String link, name, value;
+    }
+
+    protected ArrayList<Field> fields;
+    protected Integer          operation, referenceId, referenceTableId;
+
+    public Audit(Integer operation) {
+        this.operation = operation;
+    }
+
+    /**
+     * Get the primary key for the current record that is being archived.
+     */
+    public Integer getReferenceId() {
+        return referenceId;
+    }
+
+    /**
+     * Set the primary key for the current record that is being archived.
+     */
+    public void setReferenceId(Integer referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    /**
+     * Get the reference table constant key for the current record that is being
+     * archived.
+     */
+    public Integer getReferenceTableId() {
+        return referenceTableId;
+    }
+
+    /**
+     * Set the reference table constant key for the current record that is being
+     * archived.
+     */
+    public void setReferenceTableId(Integer referenceTableId) {
+        this.referenceTableId = referenceTableId;
+    }
+
+    /**
+     * Adds a field given by field name and current and original values to the
+     * list of xml elements, that can later retrieved through getXML(boolean)
+     */
+    public Audit setField(String fieldName, Object currentValue, Object originalValue) {
+        return setField(fieldName, currentValue, originalValue, null);
+    }
+
+    /**
+     * Adds a field given by field name, current and original values, and the
+     * reference table id to the list of xml elements. The linked table id is
+     * used to specify the referenced table id that the field value points to.
+     */
+    public Audit setField(String fieldName, Object currentValue, Object originalValue,
+                          Integer linkedTableId) {
+        Field f;
+
+        if ( (Constants.audit().UPDATE.equals(operation) && isDifferent(originalValue,
+                                                                        currentValue)) ||
+            Constants.audit().DELETE.equals(operation)) {
+            f = new Field();
+            f.name = fieldName;
+            f.value = (originalValue == null) ? "" : originalValue.toString();
+            f.link = (linkedTableId == null) ? "" : linkedTableId.toString();
+            if (fields == null)
+                fields = new ArrayList<Field>();
+            fields.add(f);
+        }
+        return this;
+    }
+
+    /**
+     * This method creates a list of xml elements that represent the changes
+     * made to certain fields. The withDifferences flag indicates that elements
+     * should be created regardless of whether or not the field was changed.
+     */
+    public String getXML() {
+        Document doc;
+        Element root, elem;
+
+        if (fields != null) {
+            try {
+                doc = XMLUtil.createNew("doc");
+                root = doc.getDocumentElement();
+
+                for (Field f : fields) {
+                    elem = doc.createElement(f.name);
+                    if ( !"".equals(f.link))
+                        elem.setAttribute("refTable", f.link);
+                    elem.appendChild(doc.createTextNode(f.value));
+                    root.appendChild(elem);
+                }
+                if (root.hasChildNodes())
+                    return XMLUtil.toString(doc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /*
+     * Overridden because the string value from the original clone contains
+     * trailing spaces, thus trimming.
+     */
+    private boolean isDifferent(Object a, Object b) {
+        if (b instanceof String)
+            return DataBaseUtil.isDifferent(a, ((String)b).trim());
+        return DataBaseUtil.isDifferent(a, b);
+    }
+}
