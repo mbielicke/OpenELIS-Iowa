b9f90c5ae5b2fd5f66868675c21ff1029a3ab19a
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2013-03-12 19:53:57 +0000

    This is the project upgraded to JBoss7 but before starting the upgrade to new widgets (GWT 2.5 UI Binder)

---
+++ OpenELIS/version2/OpenELIS/tags/OpenELIS-JBoss-7/web/src/org/openelis/web/cache/DictionaryCacheService.java
@@ -0,0 +1,55 @@
+package org.openelis.web.cache;
+
+import org.openelis.domain.DictionaryDO;
+import org.openelis.gwt.screen.Callback;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class DictionaryCacheService implements DictionaryCacheServiceInt,
+                                   DictionaryCacheServiceIntAsync {
+    
+    static DictionaryCacheService instance;
+    
+    DictionaryCacheServiceIntAsync service;
+    
+    public static DictionaryCacheService get() {
+        if(instance == null)
+            instance = new DictionaryCacheService();
+        
+        return instance;
+    }
+    
+    private DictionaryCacheService() {
+        service = (DictionaryCacheServiceIntAsync)GWT.create(DictionaryCacheServiceInt.class);
+    }
+
+    @Override
+    public void getById(Integer id, AsyncCallback<DictionaryDO> callback) {
+        service.getById(id, callback);
+    }
+
+    @Override
+    public void getBySystemName(String systemName, AsyncCallback<DictionaryDO> callback) {
+        service.getBySystemName(systemName, callback);
+    }
+
+    @Override
+    public DictionaryDO getBySystemName(String systemName) throws Exception {
+        Callback<DictionaryDO> callback;
+        
+        callback = new Callback<DictionaryDO>();
+        service.getBySystemName(systemName, callback);
+        return callback.getResult();
+    }
+
+    @Override
+    public DictionaryDO getById(Integer id) throws Exception {
+        Callback<DictionaryDO> callback;
+        
+        callback = new Callback<DictionaryDO>();
+        service.getById(id, callback);
+        return callback.getResult();
+    }
+
+}
