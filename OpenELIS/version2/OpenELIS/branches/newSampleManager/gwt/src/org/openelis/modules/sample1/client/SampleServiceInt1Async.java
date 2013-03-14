cda4a159e6ee08d0951b0c9b0300c6c247fec405
akampoow <akampoow@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2013-03-14 18:25:52 +0000

    The code that uses the new SampleManager and the features in GWT 2.5.

---
+++ OpenELIS/version2/OpenELIS/branches/newSampleManager/gwt/src/org/openelis/modules/sample1/client/SampleServiceInt1Async.java
@@ -0,0 +1,60 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.modules.sample1.client;
+
+import java.util.ArrayList;
+
+import org.openelis.gwt.common.data.Query;
+import org.openelis.manager.SampleManager1;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * ScreenServiceIntAsync is the Asynchronous version of the ScreenServiceInt
+ * interface.
+ */
+public interface SampleServiceInt1Async {
+
+    public void fetchByIds(ArrayList<Integer> sampleIds, SampleManager1.Load elements[],
+                           AsyncCallback<ArrayList<SampleManager1>> callback) throws Exception;
+
+    public void fetchForUpdate(ArrayList<Integer> sampleIds, SampleManager1.Load elements[],
+                               AsyncCallback<ArrayList<SampleManager1>> callback) throws Exception;
+
+    public void fetchByAnalyses(ArrayList<Integer> analysisIds, SampleManager1.Load elements[],
+                                AsyncCallback<ArrayList<SampleManager1>> callback) throws Exception;
+
+    public void getInstance(String domain, AsyncCallback<SampleManager1> callback) throws Exception;
+
+    public void add(SampleManager1 sm, boolean ignoreWarnings,
+                    AsyncCallback<SampleManager1> callback) throws Exception;
+
+    public void update(SampleManager1 sm, boolean ignoreWarnings,
+                       AsyncCallback<SampleManager1> callback) throws Exception;
+    
+    public void fetchByQuery(Query query, SampleManager1.Load elements[],
+                              AsyncCallback<ArrayList<SampleManager1>> callback) throws Exception;
+}
---
+++ OpenELIS/version2/OpenELIS/branches/newSampleManager/gwt/src/org/openelis/modules/sample1/server/SampleServlet1.java
@@ -0,0 +1,66 @@
+/**
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * Copyright (C) The University of Iowa. All Rights Reserved.
+ */
+package org.openelis.modules.sample1.server;
+
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+
+import org.openelis.bean.SampleManager1Bean;
+import org.openelis.domain.IdAccessionVO;
+import org.openelis.gwt.common.data.Query;
+import org.openelis.gwt.server.RemoteServlet;
+import org.openelis.manager.SampleManager1;
+import org.openelis.manager.SampleManager1.Load;
+import org.openelis.modules.sample1.client.SampleServiceInt1;
+
+@WebServlet("/openelis/sample1")
+public class SampleServlet1 extends RemoteServlet implements SampleServiceInt1 {
+
+    private static final long serialVersionUID = 1L;
+    
+    @EJB
+    private SampleManager1Bean sampleManager1;
+    
+    public SampleManager1 getInstance(String domain) throws Exception {
+        return sampleManager1.getInstance(domain);
+    }
+
+    public ArrayList<SampleManager1> fetchByIds(ArrayList<Integer> sampleIds,
+                                                SampleManager1.Load ...elements) throws Exception {
+        return sampleManager1.fetchByIds(sampleIds, elements);
+    }
+    
+    public ArrayList<SampleManager1> fetchByQuery(Query query, SampleManager1.Load... elements) throws Exception {
+        return sampleManager1.fetchByQuery(query, elements);
+    }
+    
+    public ArrayList<SampleManager1> fetchForUpdate(ArrayList<Integer> sampleIds, Load... elements) throws Exception {
+        return sampleManager1.fetchForUpdate(sampleIds, elements);
+    }
+    
+    public ArrayList<SampleManager1> fetchByAnalyses(ArrayList<Integer> analysisIds,
+                                                     SampleManager1.Load... elements) throws Exception {
+        return sampleManager1.fetchByAnalyses(analysisIds, elements);
+    }
+    
+    public SampleManager1 add(SampleManager1 sm, boolean ignoreWarnings) throws Exception {
+        return sampleManager1.add(sm, ignoreWarnings);
+    }
+    
+    public SampleManager1 update(SampleManager1 sm, boolean ignoreWarnings) throws Exception {
+        return sampleManager1.add(sm, ignoreWarnings);
+    }
+}
