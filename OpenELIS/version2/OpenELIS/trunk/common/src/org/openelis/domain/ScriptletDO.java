bc41ee1580eb45536efd96a57dd82d6c23f4eecb
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2013-11-05 17:11:12 +0000

    Commit of Scriptlet setup

--- OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/domain/ScriptletDO.java
+++ OpenELIS/version2/OpenELIS/trunk/common/src/org/openelis/domain/ScriptletDO.java
@@ -25,7 +25,10 @@
  */
 package org.openelis.domain;
 
+import java.util.Date;
+
 import org.openelis.ui.common.DataBaseUtil;
+import org.openelis.ui.common.Datetime;
 
 /**
  * Class represents the fields in database table scriptlet.
@@ -35,15 +38,20 @@ public class ScriptletDO extends DataObject {
     private static final long serialVersionUID = 1L;
 
     protected Integer         id;
-    protected String          name, codeSource;
+    protected String          name, bean, isActive;
+    protected Datetime        activeBegin,activeEnd;
+    
 
     public ScriptletDO() {
     }
 
-    public ScriptletDO(Integer id, String name, String codeSource) {
+    public ScriptletDO(Integer id, String name, String bean, String isActive, Date activeBegin, Date activeEnd) {
         setId(id);
         setName(name);
-        setCodeSource(codeSource);
+        setBean(bean);
+        setIsActive(isActive);
+        setActiveBegin(DataBaseUtil.toYD(activeBegin));
+        setActiveEnd(DataBaseUtil.toYD(activeEnd));
         _changed = false;
     }
 
@@ -64,12 +72,39 @@ public class ScriptletDO extends DataObject {
         _changed = true;
     }
 
-    public String getCodeSource() {
-        return codeSource;
+    public String getBean() {
+        return bean;
     }
 
-    public void setCodeSource(String codeSource) {
-        this.codeSource = DataBaseUtil.trim(codeSource);
+    public void setBean(String bean) {
+        this.bean = DataBaseUtil.trim(bean);
+        _changed = true;
+    }
+    
+    public String getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(String isActive) {
+        this.isActive = DataBaseUtil.trim(isActive);
+        _changed = true;
+    }
+
+    public Datetime getActiveBegin() {
+        return activeBegin;
+    }
+
+    public void setActiveBegin(Datetime activeBegin) {
+        this.activeBegin = DataBaseUtil.toYD(activeBegin);
+        _changed = true;
+    }
+
+    public Datetime getActiveEnd() {
+        return activeEnd;
+    }
+
+    public void setActiveEnd(Datetime activeEnd) {
+        this.activeEnd = DataBaseUtil.toYD(activeEnd);
         _changed = true;
     }
 }
