a0107447bd682b04f699bbb56895c238a8de3535
Travis Schmidt <travis.schmidt@gmail.com>
2013-11-13 19:34:30 +0000

    commit to Follow Branch

--- common/src/org/openelis/domain/Constants.java
+++ common/src/org/openelis/domain/Constants.java
@@ -142,7 +142,7 @@ public class Constants implements Serializable {
                         ORDER_TEST_ANALYTE = 111, ORDER_SAMPLE_NOTE = 112,
                         EXCHANGE_LOCAL_TERM = 113, EXCHANGE_EXTERNAL_TERM = 114,
                         EXCHANGE_CRITERIA = 115, EXCHANGE_PROFILE = 116, EVENT_LOG = 117,
-                        QC_LOT = 118, SAMPLE_NEONATAL = 119, CASE = 120;
+                        QC_LOT = 118, SAMPLE_NEONATAL = 119, CASE = 120, PROCEDURE = 121;
     }
 
     /**
