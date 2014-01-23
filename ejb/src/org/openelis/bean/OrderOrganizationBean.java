548156f9686732fd97fbbce3bb67291d00f0d057
nfalat <nfalat11@gmail.com>
2014-01-23 15:29:28 +0000

    created new test manager. It is not currently connected to anything. fixed bug in order organization validation.

--- ejb/src/org/openelis/bean/OrderOrganizationBean.java
+++ ejb/src/org/openelis/bean/OrderOrganizationBean.java
@@ -124,9 +124,9 @@ public class OrderOrganizationBean {
 
         list = new ValidationErrorsList();
         if (DataBaseUtil.isEmpty(data.getTypeId()))
-            list.add(new FormErrorException(Messages.get().order_organizationRequiredException(DataBaseUtil.toString(data.getId()))));
+            list.add(new FormErrorException(Messages.get().order_organizationTypeRequiredException(DataBaseUtil.toString(data.getOrderId()))));
         if (DataBaseUtil.isEmpty(data.getOrganizationId()))
-            list.add(new FormErrorException(Messages.get().order_organizationTypeRequiredException(DataBaseUtil.toString(data.getId()))));
+            list.add(new FormErrorException(Messages.get().order_organizationRequiredException(DataBaseUtil.toString(data.getOrderId()))));
         
         if (list.size() > 0)
             throw list;    
