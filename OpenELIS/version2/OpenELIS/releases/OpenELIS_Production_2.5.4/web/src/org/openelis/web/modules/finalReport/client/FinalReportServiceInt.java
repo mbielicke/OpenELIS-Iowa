1357154ecb7a87131f66f1b8ef940cd2a7d3b15e
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-04-28 15:22:17 +0000

    Release Date: 2014-04-21 Air Quality Reports. Bug Fixes.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_2.5.4/web/src/org/openelis/web/modules/finalReport/client/FinalReportServiceInt.java
@@ -0,0 +1,43 @@
+package org.openelis.web.modules.finalReport.client;
+
+import java.util.ArrayList;
+
+import org.openelis.domain.FinalReportWebVO;
+import org.openelis.domain.IdNameVO;
+import org.openelis.ui.common.Prompt;
+import org.openelis.ui.common.ReportStatus;
+import org.openelis.ui.common.data.Query;
+
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.google.gwt.user.client.rpc.XsrfProtectedService;
+
+@RemoteServiceRelativePath("finalReport")
+public interface FinalReportServiceInt extends XsrfProtectedService {
+
+    ArrayList<Prompt> getPromptsForBatch() throws Exception;
+
+    ArrayList<Prompt> getPromptsForBatchReprint() throws Exception;
+
+    ReportStatus runReportForSingle(Query query) throws Exception;
+
+    ReportStatus runReportForPreview(Query query) throws Exception;
+
+    ReportStatus runReportForBatch(Query query) throws Exception;
+
+    ReportStatus runReportForBatchReprint(Query query) throws Exception;
+
+    ReportStatus runReportForWeb(Query query) throws Exception;
+
+    ArrayList<FinalReportWebVO> getSampleEnvironmentalList(Query query) throws Exception;
+
+    ArrayList<FinalReportWebVO> getSamplePrivateWellList(Query query) throws Exception;
+
+    ArrayList<FinalReportWebVO> getSampleSDWISList(Query query) throws Exception;
+
+    ArrayList<IdNameVO> getEnvironmentalProjectList() throws Exception;
+
+    ArrayList<IdNameVO> getPrivateWellProjectList() throws Exception;
+
+    ArrayList<IdNameVO> getSDWISProjectList() throws Exception;
+
+}
