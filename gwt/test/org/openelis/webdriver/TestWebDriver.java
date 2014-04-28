c2f3e24c0c4c8a6ca31e733a2050d5ae75d1c1ee
mbielick <mbielicke@users.noreply.github.com>
2014-04-28 21:24:39 +0000

    Merged with revision 8023.

---
+++ gwt/test/org/openelis/webdriver/TestWebDriver.java
@@ -0,0 +1,40 @@
+package org.openelis.webdriver;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openelis.modules.main.client.LoginPage;
+import org.openelis.modules.method.client.MethodPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestWebDriver {
+    static WebDriver driver;
+    
+    @BeforeClass
+    public static void init() {
+        driver = new FirefoxDriver();
+    }
+    
+    @Before
+    public void load() {
+        driver.get("http://192.168.111.202:8080/openelis");
+    }
+    
+    @AfterClass
+    public static void destroy() {
+        driver.quit();
+    }
+    
+    @Test
+    public  void testMethod() {
+        MethodPage page = new LoginPage(driver).login().clickMaintenance().clickMethod();
+        
+        String value = page.clickAdd().typeInName("My Name").getNameText();
+        
+        assertEquals("My Name",value);        
+    }
+}
