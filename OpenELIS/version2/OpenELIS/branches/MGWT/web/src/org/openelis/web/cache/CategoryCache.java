ea7ec68536dc27643793448e0b0c9bd1242451e5
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-07-18 18:36:33 +0000

    Branch to create mobile and tablet versions of web

---
+++ OpenELIS/version2/OpenELIS/branches/MGWT/web/src/org/openelis/web/cache/CategoryCache.java
@@ -0,0 +1,103 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.web.cache;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.openelis.domain.CategoryCacheVO;
+import org.openelis.domain.DictionaryDO;
+
+import com.google.gwt.user.client.Window;
+
+/**
+ * Class provides cache service handling for front end GWT classes. Cache
+ * objects in GWT instance are cached for the duration of the session and are
+ * not updated -- if objects in the back-end are updated, the user will need to
+ * restart the session to get updated objects.
+ */
+
+public class CategoryCache {    
+    protected static HashMap<String, CategoryCacheVO> cache;
+    
+    static {
+        cache = new HashMap<String, CategoryCacheVO>();
+    }
+
+    public static ArrayList<CategoryCacheVO> getBySystemNames(String... systemNames) throws Exception {
+        int i, j;
+        ArrayList<CategoryCacheVO> partial, full;
+        String partialNames[];
+        CategoryCacheVO entry;
+
+        //
+        // make a list of category system names that we don't have in the
+        // cache
+        full = new ArrayList<CategoryCacheVO>();
+        partialNames = new String[systemNames.length];
+        for (i = 0, j = 0; i < systemNames.length; i++ ) {
+            entry = cache.get(systemNames[i]);
+            if (entry == null)             
+                partialNames[j++] = systemNames[i];
+            else
+                full.add(entry);
+        }
+        try {
+            if (j > 0) {
+                partial = CategoryCacheService.get().getBySystemNames(partialNames);
+
+                for (CategoryCacheVO cat : partial) {
+                    full.add(cat);
+                    cache.put(cat.getSystemName(), cat);                    
+                    for (DictionaryDO data : cat.getDictionaryList()) 
+                        DictionaryCache.add(data);                    
+                }
+            }
+        } catch (Exception e) {
+            throw new Exception("CategoryCache.getBySystemNamesInt error." + e.getMessage());
+        }
+
+        return full;
+    }
+
+    public static ArrayList<DictionaryDO> getBySystemName(String systemName) {
+        CategoryCacheVO cat;
+
+        cat = cache.get(systemName);
+        if (cat == null) {
+            try {
+                cat = CategoryCacheService.get().getBySystemName(systemName);
+                cache.put(systemName, cat);
+                for (DictionaryDO entry : cat.getDictionaryList())
+                    DictionaryCache.add(entry);
+            } catch (Exception e) {
+                Window.alert(e.getMessage());
+                return null;
+            }
+        } 
+        return cat.getDictionaryList();
+    }
+}
