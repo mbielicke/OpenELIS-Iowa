180ab2b7b5c4be1012c19be17d8e8a84a0209a12
akampoow <akampoow@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-08-12 16:08:00 +0000

    This is for converting the SDWIS sample login screen to the new framework.

---
+++ OpenELIS/version2/OpenELIS/branches/sdwisLoginScreen/gwt/src/org/openelis/modules/completeRelease1/client/QueryByWorksheetLookupUI.java
@@ -0,0 +1,150 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.modules.completeRelease1.client;
+
+import org.openelis.constants.Messages;
+import org.openelis.ui.event.DataChangeEvent;
+import org.openelis.ui.event.StateChangeEvent;
+import org.openelis.ui.screen.Screen;
+import org.openelis.ui.screen.Screen.Validation.Status;
+import org.openelis.ui.screen.ScreenHandler;
+import org.openelis.ui.screen.State;
+import org.openelis.ui.widget.Button;
+import org.openelis.ui.widget.TextBox;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * This class is used to allow specifying the id of a worksheet for fetching the
+ * analyses linked to it
+ */
+public abstract class QueryByWorksheetLookupUI extends Screen {
+
+    @UiTemplate("QueryByWorksheetLookup.ui.xml")
+    interface QueryByWorksheetLookupUIBinder extends UiBinder<Widget, QueryByWorksheetLookupUI> {
+    };
+
+    private static QueryByWorksheetLookupUIBinder uiBinder = GWT.create(QueryByWorksheetLookupUIBinder.class);
+
+    @UiField
+    protected TextBox<Integer>                    worksheetNum;
+    
+    @UiField
+    protected Button                              okButton, cancelButton;
+    
+    protected Integer                             worksheetId; 
+
+    public QueryByWorksheetLookupUI() {
+        initWidget(uiBinder.createAndBindUi(this));
+        initialize();
+    }
+
+    public void initialize() {
+        addScreenHandler(worksheetNum, "worksheetNum", new ScreenHandler<Integer>() {
+            public void onDataChange(DataChangeEvent event) {
+                worksheetNum.setValue(null);
+            }
+
+            public void onValueChange(ValueChangeEvent<Integer> event) {
+                worksheetId = event.getValue();
+            }
+
+            public void onStateChange(StateChangeEvent event) {
+                worksheetNum.setEnabled(true);
+            }
+        });
+        
+        addScreenHandler(okButton, "okButton", new ScreenHandler<Object>() {
+            public void onStateChange(StateChangeEvent event) {
+                okButton.setEnabled(true);
+            }
+        });
+
+        addScreenHandler(cancelButton, "cancelButton", new ScreenHandler<Object>() {
+            public void onStateChange(StateChangeEvent event) {
+                cancelButton.setEnabled(true);
+            }
+        });
+    }
+    
+    public void setData() {
+        clearErrors();
+        setState(state);
+        fireDataChange();
+    }
+    
+    public void setState(State state) {
+        this.state = state;
+        bus.fireEventFromSource(new StateChangeEvent(state), this);
+    }
+    
+    /**
+     * overridden to respond to the user clicking "ok"
+     */
+    public abstract void ok();
+
+    /**
+     * overridden to respond to the user clicking "cancel"
+     */
+    public abstract void cancel();
+    
+    /**
+     * returns the worksheet id entered by the user 
+     */
+    public Integer getWorksheetId() {
+        return worksheetId;
+    }
+
+    @UiHandler("okButton")
+    protected void ok(ClickEvent event) {
+        Validation validation;
+
+        finishEditing();
+
+        validation = validate();
+        
+        if (validation.getStatus() == Status.ERRORS) {
+            setError(Messages.get().gen_correctErrors());
+            return;
+        }
+            
+        window.close();
+        ok();
+    }
+
+    @UiHandler("cancelButton")
+    protected void cancel(ClickEvent event) {
+        window.close();
+        cancel();
+    }
+}
---
+++ OpenELIS/version2/OpenELIS/branches/sdwisLoginScreen/gwt/src/org/openelis/modules/panel/client/PanelServiceIntAsync.java
@@ -0,0 +1,44 @@
+package org.openelis.modules.panel.client;
+
+import java.util.ArrayList;
+
+import org.openelis.domain.IdNameVO;
+import org.openelis.domain.IdVO;
+import org.openelis.domain.PanelDO;
+import org.openelis.domain.TestMethodVO;
+import org.openelis.ui.common.data.Query;
+import org.openelis.manager.PanelItemManager;
+import org.openelis.manager.PanelManager;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface PanelServiceIntAsync {
+
+    void abortUpdate(Integer id, AsyncCallback<PanelManager> callback);
+
+    void add(PanelManager man, AsyncCallback<PanelManager> callback);
+
+    void fetchAuxIdsByPanelId(Integer panelId, AsyncCallback<ArrayList<IdVO>> callback);
+
+    void fetchById(Integer id, AsyncCallback<PanelManager> callback);
+
+    void fetchByName(String name, AsyncCallback<ArrayList<PanelDO>> callback);
+
+    void fetchByNameSampleTypeWithTests(Query query, AsyncCallback<ArrayList<TestMethodVO>> callback);
+
+    void fetchByNameWithTests(String name, AsyncCallback<ArrayList<TestMethodVO>> callback);
+
+    void fetchForUpdate(Integer id, AsyncCallback<PanelManager> callback);
+
+    void fetchItemByPanelId(Integer id, AsyncCallback<PanelItemManager> callback);
+
+    void fetchTestIdsByPanelId(Integer panelId, AsyncCallback<ArrayList<IdVO>> callback);
+
+    void fetchWithItems(Integer id, AsyncCallback<PanelManager> callback);
+
+    void fetchAll(AsyncCallback<ArrayList<PanelDO>> callback);
+
+    void query(Query query, AsyncCallback<ArrayList<IdNameVO>> callback);
+
+    void update(PanelManager man, AsyncCallback<PanelManager> callback);
+}
