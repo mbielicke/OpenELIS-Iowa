893e6c0fd5869f199920752342f0bd6d69580ed9
Travis Schmidt <travis.schmidt@gmail.com>
2014-08-15 22:03:28 +0000

    Share project "OpenELIS-Portal" into "https://openelis.shl.uiowa.edu/svn"

---
+++ portal/src/org/openelis/portal/client/OpenELISPortalEntry.java
@@ -0,0 +1,13 @@
+package org.openelis.portal.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class OpenELISPortalEntry implements EntryPoint {
+
+	public void onModuleLoad() {
+
+	}
+}
