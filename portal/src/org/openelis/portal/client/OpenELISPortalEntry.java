55e754cafe3da46540017084ed55c5e0f43cc1cf
Travis Schmidt <travis.schmidt@gmail.com>
2014-08-15 22:48:39 +0000

    Setting up skeleton of Portal project

--- portal/src/org/openelis/portal/client/OpenELISPortalEntry.java
+++ portal/src/org/openelis/portal/client/OpenELISPortalEntry.java
@@ -1,13 +1,58 @@
 package org.openelis.portal.client;
 
+import static org.openelis.portal.client.Logger.remote;
+
+import java.util.logging.Level;
+
+import org.openelis.portal.client.resources.Resources;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.Event.NativePreviewHandler;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class OpenELISPortalEntry implements EntryPoint {
+public class OpenELISPortalEntry implements EntryPoint, NativePreviewHandler {
 
-	public void onModuleLoad() {
+    /**
+     * This is invoked by the GWT JavaScript to start the Application
+     */
+    public void onModuleLoad() {
+        // All Events will flow this this handler first before any other
+        // handlers.
+        Event.addNativePreviewHandler(this);
+        Resources.INSTANCE.style().ensureInjected();
+        try {
+            RootPanel.get("main").clear();
+            RootLayoutPanel.get().add(((IsWidget)GWT.create(PortalEntry.class)).asWidget());
 
-	}
+            SessionTimer.start();
+        } catch (Throwable e) {
+            remote.log(Level.SEVERE,e.getMessage(),e);
+            Window.alert("Unable to start app : " + e.getMessage());
+        }
+    }
+
+    /**
+     * All events created by the application will flow through here. The event
+     * can be inspected for type and other user input then certain actions can
+     * be taken such as preventing default browser before or even cancelling
+     * events
+     */
+    public void onPreviewNativeEvent(NativePreviewEvent event) {
+        // This check is to prevent FireFox from highlighting HTML Elements when
+        // mouseDown is combined with the ctrl key
+        if (event.getTypeInt() == Event.ONMOUSEDOWN && event.getNativeEvent().getCtrlKey())
+            event.getNativeEvent().preventDefault();
+        if (event.getTypeInt() == Event.ONMOUSEWHEEL && event.getNativeEvent().getShiftKey())
+            event.getNativeEvent().preventDefault();
+
+    }
 }
---
+++ portal/src/org/openelis/portal/client/resources/Style.java
@@ -0,0 +1,17 @@
+package org.openelis.portal.client.resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface Style extends CssResource {
+
+    String heading();
+    String AppBackground();
+    String HeaderLabel();
+    String header_nav();
+    String header_logo();
+    String header_section();
+    String section_label();
+    String label();
+    String link();
+    String link_hover();    
+}
