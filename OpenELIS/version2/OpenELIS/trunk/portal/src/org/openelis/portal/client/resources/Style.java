ac5cd96514c0c629515eefc46fe9945b0130251e
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-08-20 16:39:50 +0000

    Commit of Style resouces to Portal

--- OpenELIS/version2/OpenELIS/trunk/portal/src/org/openelis/portal/client/resources/Style.java
+++ OpenELIS/version2/OpenELIS/trunk/portal/src/org/openelis/portal/client/resources/Style.java
@@ -4,14 +4,27 @@ import com.google.gwt.resources.client.CssResource;
 
 public interface Style extends CssResource {
 
-    String heading();
-    String AppBackground();
-    String HeaderLabel();
-    String header_nav();
-    String header_logo();
-    String header_section();
-    String section_label();
-    String label();
-    String link();
-    String link_hover();    
+	public String AppBackground();
+	
+	public String WhiteContentPanel();
+	
+	public String Form();
+	
+	public String FormTitle();
+	
+	public String Prompt();
+	
+	public String WarningLabel();
+	
+	public String favorite();
+	
+	public String LeftMenuPanePanelClosed();
+	
+	public String LeftMenuPanePanelOpen();
+	
+	public String favoritesMenuContainer();
+	
+	public String ScreenLabelLink();
+	
+    public String Title();
 }
