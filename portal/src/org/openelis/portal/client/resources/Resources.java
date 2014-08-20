7d3da85d7ddc3cb40a614205553ad38d52489ea3
Travis Schmidt <travis.schmidt@gmail.com>
2014-08-20 15:41:46 +0000

    Made Resources extend UIResources

--- portal/src/org/openelis/portal/client/resources/Resources.java
+++ portal/src/org/openelis/portal/client/resources/Resources.java
@@ -1,9 +1,11 @@
 package org.openelis.portal.client.resources;
 
+import org.openelis.ui.resources.UIResources;
+
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 
-public interface Resources extends ClientBundle {
+public interface Resources extends UIResources {
     
     public static final Resources INSTANCE = GWT.create(Resources.class);
 
