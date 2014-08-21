aedf45bfb148facf53ef94b227a47db236cd8850
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-08-21 19:41:02 +0000

    Changes to Style and injected styles

--- OpenELIS/version2/OpenELIS/trunk/portal/src/org/openelis/portal/client/OpenELISPortalEntry.java
+++ OpenELIS/version2/OpenELIS/trunk/portal/src/org/openelis/portal/client/OpenELISPortalEntry.java
@@ -31,6 +31,8 @@ public class OpenELISPortalEntry implements EntryPoint, NativePreviewHandler {
         // handlers.
         Event.addNativePreviewHandler(this);
         Resources.INSTANCE.style().ensureInjected();
+        Resources.INSTANCE.tabpanel().ensureInjected();
+        Resources.INSTANCE.general().ensureInjected();
         try {
             RootPanel.get("main").clear();
             RootLayoutPanel.get().add((Screen)GWT.create(DesktopScreen.class));
