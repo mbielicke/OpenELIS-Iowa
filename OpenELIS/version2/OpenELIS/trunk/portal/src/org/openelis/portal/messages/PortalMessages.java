fd91cab66b735b77762ccf626d0e5559f736ea0a
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-08-22 16:59:41 +0000

    Commit of PortalMessages

---
+++ OpenELIS/version2/OpenELIS/trunk/portal/src/org/openelis/portal/messages/PortalMessages.java
@@ -0,0 +1,17 @@
+package org.openelis.portal.messages;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/home/tschmidt/luna/workspace/OpenELIS-Portal/src/org/openelis/portal/messages/PortalMessages.properties'.
+ */
+public interface PortalMessages extends org.openelis.ui.messages.UIMessages {
+  
+  /**
+   * Translated "Final Report".
+   * 
+   * @return translated "Final Report"
+   */
+  @DefaultMessage("Final Report")
+  @Key("finalReport")
+  String finalReport();
+}
