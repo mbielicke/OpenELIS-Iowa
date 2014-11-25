accfb847d59c78e6c895b1dd6b64ddfe9366b535
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-11-25 19:38:35 +0000

    Release Date: 2014-10-09 Bug Fixes.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_2.6.4/gwt/src/org/openelis/modules/sample1/client/QAEventRemovedEvent.java
@@ -0,0 +1,70 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.modules.sample1.client;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * The event is used to inform the handler that a qa event was removed from
+ * either the sample or an analysis. The unique identifier for the qa event is
+ * specified through uid.
+ */
+public class QAEventRemovedEvent extends GwtEvent<QAEventRemovedEvent.Handler> {
+
+    private static Type<QAEventRemovedEvent.Handler> TYPE;
+    private String                                   uid;
+
+    public QAEventRemovedEvent(String uid) {
+        this.uid = uid;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    @Override
+    public Type<QAEventRemovedEvent.Handler> getAssociatedType() {
+        return (Type)TYPE;
+    }
+
+    public static Type<QAEventRemovedEvent.Handler> getType() {
+        if (TYPE == null) {
+            TYPE = new Type<QAEventRemovedEvent.Handler>();
+        }
+        return TYPE;
+    }
+
+    public static interface Handler extends EventHandler {
+        public void onQAEventRemoved(QAEventRemovedEvent event);
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    @Override
+    protected void dispatch(Handler handler) {
+        handler.onQAEventRemoved(this);
+    }
+}
