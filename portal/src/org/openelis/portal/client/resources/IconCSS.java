e89cb6f75c4bd894819e9da6f375dca2e997e3e0
Travis Schmidt <travis.schmidt@gmail.com>
2014-12-12 21:32:46 +0000

    Branch making and testing small refactor steps

--- portal/src/org/openelis/portal/client/resources/IconCSS.java
+++
@@ -1,9 +0,0 @@
-package org.openelis.portal.client.resources;
-
-public interface IconCSS extends org.openelis.ui.resources.IconCSS {
-    String helpImage();
-    String removeImage();
-    String reportImage();
-    String emailImage();
-    String spreadsheetImage();
-}
--- portal/src/org/openelis/portal/client/resources/PortalButton.java
+++
@@ -1,7 +0,0 @@
-package org.openelis.portal.client.resources;
-
-import org.openelis.ui.resources.ButtonCSS;
-
-public interface PortalButton extends ButtonCSS {
-
-}
