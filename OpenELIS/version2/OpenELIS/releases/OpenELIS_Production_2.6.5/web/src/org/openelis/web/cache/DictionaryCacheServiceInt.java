64b77b6419869bd6dbfea6e4cf302423e1d1a562
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-01-07 22:18:48 +0000

    Release Date: 2014-11-25 Bug fixes and improvements to SDWIS Additional Scanner, SDWIS Violation Scanner, and Air Quality Export.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_2.6.5/web/src/org/openelis/web/cache/DictionaryCacheServiceInt.java
@@ -0,0 +1,16 @@
+package org.openelis.web.cache;
+
+import org.openelis.domain.DictionaryDO;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.google.gwt.user.client.rpc.XsrfProtectedService;
+
+@RemoteServiceRelativePath("dictionaryCache")
+public interface DictionaryCacheServiceInt extends XsrfProtectedService {
+
+    DictionaryDO getBySystemName(String systemName) throws Exception;
+
+    DictionaryDO getById(Integer id) throws Exception;
+
+}
