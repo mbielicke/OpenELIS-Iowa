adb651498b28b94b93b6e14c56f1db4f7bc8cdd0
Travis Schmidt <travis.schmidt@gmail.com>
2015-02-10 20:09:16 +0000

    Added Arquillian Suite Extension for single deployment test runs

---
+++ ejb/test/org/openelis/deployment/TestSuite.java
@@ -0,0 +1,13 @@
+package org.openelis.deployment;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.openelis.bean.TestMethodBeanArq;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestMethodBeanArq.class
+})
+public class TestSuite {
+
+}
