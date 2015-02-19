ad9417519a9e7bfdde29254b76018dd473caf8fc
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-02-19 15:26:41 +0000

    Release Date: 2015-02-15 New Web Portal. Bug Fixes.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_2.6.6/portal/src/org/openelis/portal/client/TimerServiceInt.java
@@ -0,0 +1,15 @@
+package org.openelis.portal.client;
+
+import org.openelis.ui.common.Datetime;
+
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.google.gwt.user.client.rpc.XsrfProtectedService;
+
+@RemoteServiceRelativePath("timer")
+public interface TimerServiceInt extends XsrfProtectedService  {
+
+    void keepAlive();
+    
+    Datetime getLastAccess();
+
+}
