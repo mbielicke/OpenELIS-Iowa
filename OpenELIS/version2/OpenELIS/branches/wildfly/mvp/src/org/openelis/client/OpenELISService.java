f1d7344cd1a8448ddeea6d0174628948addcdaaf
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-02-21 03:58:17 +0000

    Share project "OpenELIS-MVP" into "https://openelis.shl.uiowa.edu/svn"

---
+++ OpenELIS/version2/OpenELIS/branches/wildfly/mvp/src/org/openelis/client/OpenELISService.java
@@ -0,0 +1,55 @@
+package org.openelis.client;
+
+import org.openelis.domain.Constants;
+import org.openelis.ui.screen.Callback;
+import org.openelis.ui.services.TokenService;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.HasRpcToken;
+
+public class OpenELISService implements OpenELISServiceInt, OpenELISServiceIntAsync {
+    
+    static OpenELISService instance;
+    
+    OpenELISServiceIntAsync service;
+    
+    public static OpenELISService get() {
+        if(instance == null)
+            instance = new OpenELISService();
+        
+        return instance;
+    }
+    
+    private OpenELISService() {
+        service = (OpenELISServiceIntAsync)GWT.create(OpenELISServiceInt.class);
+        ((HasRpcToken)service).setRpcToken(TokenService.getToken());
+    }
+
+    @Override
+    public void getConstants(AsyncCallback<Constants> callback) {
+        service.getConstants(callback);
+    }
+
+    @Override
+    public void logout(AsyncCallback<Void> callback) {
+        service.logout(callback);
+    }
+
+    @Override
+    public Constants getConstants() throws Exception {
+        Callback<Constants> callback;
+        
+        callback = new Callback<Constants>();
+        service.getConstants(callback);
+        return callback.getResult();
+    }
+
+    @Override
+    public void logout() {
+        Callback<Void> callback;
+        
+        callback = new Callback<Void>();
+        service.logout(callback);
+    }    
+}
