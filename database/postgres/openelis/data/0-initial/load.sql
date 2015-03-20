3f1051ba8094c6e3092a9c46f36d9cc7251fe0aa
akampoow <akampoow@gmail.com>
2015-03-20 21:34:18 +0000

    This is for creating the screen for doing data entry from TRFs and attaching them to samples.

--- database/postgres/openelis/data/0-initial/load.sql
+++
@@ -1,8 +0,0 @@
-\copy category from 'category.dat' with delimiter as '|';
-\copy dictionary from 'dictionary.dat' with delimiter as '|';
-\copy system_variable from 'system_variable.dat' with delimiter as '|';
-
-select setval('category_id_seq', max(id)) from category;
-select setval('dictionary_id_seq', max(id)) from dictionary;
-select setval('system_variable_id_seq', max(id)) from system_variable;
-
