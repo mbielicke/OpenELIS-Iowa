657c8b58faf99d138c03dca267e146f01db93962
akampoow <akampoow@gmail.com>
2015-03-30 16:54:36 +0000

    Commtting before switching to trunk to fix a bug on Analysis tab.

---
+++ gwt/src/org/openelis/modules/report/dataView1/client/PrivateWellTabUI.java
@@ -0,0 +1,57 @@
+/** Exhibit A - UIRF Open-source Based Public Software License.
+* 
+* The contents of this file are subject to the UIRF Open-source Based
+* Public Software License(the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+* openelis.uhl.uiowa.edu
+* 
+* Software distributed under the License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific language governing rights and limitations
+* under the License.
+* 
+* The Original Code is OpenELIS code.
+* 
+* The Initial Developer of the Original Code is The University of Iowa.
+* Portions created by The University of Iowa are Copyright 2006-2008. All
+* Rights Reserved.
+* 
+* Contributor(s): ______________________________________.
+* 
+* Alternatively, the contents of this file marked
+* "Separately-Licensed" may be used under the terms of a UIRF Software
+* license ("UIRF Software License"), in which case the provisions of a
+* UIRF Software License are applicable instead of those above. 
+*/
+package org.openelis.modules.report.dataView1.client;
+
+import org.openelis.ui.screen.Screen;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Widget;
+
+public class PrivateWellTabUI extends Screen {
+    @UiTemplate("PrivateWellTab.ui.xml")
+    interface PrivateWellTabUIBinder extends UiBinder<Widget, PrivateWellTabUI> {
+    };
+
+    private static PrivateWellTabUIBinder uiBinder = GWT.create(PrivateWellTabUIBinder.class);
+    
+    protected Screen                 parentScreen;
+    
+    protected EventBus               parentBus;
+    
+    public PrivateWellTabUI(Screen parentScreen) {
+        this.parentScreen = parentScreen;
+        this.parentBus = parentScreen.getEventBus();
+        initWidget(uiBinder.createAndBindUi(this));
+        initialize();
+    }
+    
+    public void initialize() {
+        
+    }
+}
