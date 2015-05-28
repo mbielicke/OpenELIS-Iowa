f8fb8d663c56824dd91411bd4f7d62197ee98d73
mbielick <mbielicke@users.noreply.github.com>
2015-05-28 15:46:04 +0000

    Removed "dba" qualifier to ease comparison between Informix and PostgreSQL files.

--- database/informix/security/permissions.sql
+++ database/informix/security/permissions.sql
@@ -1,43 +1,43 @@
 grant dba to "dba";
 grant connect to "public";
 
-revoke all on "dba".application from "public" as "dba";
-revoke all on "dba".lock from "public" as "dba";
-revoke all on "dba".section from "public" as "dba";
-revoke all on "dba".system_module from "public" as "dba";
-revoke all on "dba".system_user from "public" as "dba";
-revoke all on "dba".system_user_module from "public" as "dba";
-revoke all on "dba".system_user_section from "public" as "dba";
+revoke all on application from "public";
+revoke all on lock from "public";
+revoke all on section from "public";
+revoke all on system_module from "public";
+revoke all on system_user from "public";
+revoke all on system_user_module from "public";
+revoke all on system_user_section from "public";
 
-grant select,update,insert,delete,index on "dba".application to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".application to "jboss_dev" as "dba";
-grant select,index on "dba".application to "public" as "dba";
+grant select,update,insert,delete,index on application to "jboss_demo";
+grant select,update,insert,delete,index on application to "jboss_dev";
+grant select,index on application to "public";
 
-grant select,update,insert,delete,index on "dba".lock to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".lock to "jboss_dev" as "dba";
-grant select,index on "dba".lock to "public" as "dba";
+grant select,update,insert,delete,index on lock to "jboss_demo";
+grant select,update,insert,delete,index on lock to "jboss_dev";
+grant select,index on lock to "public";
 
-grant select,update,insert,delete,index on "dba".section to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".section to "jboss_dev" as "dba";
-grant select,index on "dba".section to "public" as "dba";
+grant select,update,insert,delete,index on section to "jboss_demo";
+grant select,update,insert,delete,index on section to "jboss_dev";
+grant select,index on section to "public";
 
-grant select,update,insert,delete,index on "dba".system_module to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".system_module to "jboss_dev" as "dba";
-grant select,index on "dba".system_module to "public" as "dba";
+grant select,update,insert,delete,index on system_module to "jboss_demo";
+grant select,update,insert,delete,index on system_module to "jboss_dev";
+grant select,index on system_module to "public";
 
-grant select,update,insert,delete,index on "dba".system_user to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".system_user to "jboss_dev" as "dba";
-grant select,index on "dba".system_user to "public" as "dba";
+grant select,update,insert,delete,index on system_user to "jboss_demo";
+grant select,update,insert,delete,index on system_user to "jboss_dev";
+grant select,index on system_user to "public";
 
-grant select,update,insert,delete,index on "dba".system_user_module to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".system_user_module to "jboss_dev" as "dba";
-grant select,index on "dba".system_user_module to "public" as "dba";
+grant select,update,insert,delete,index on system_user_module to "jboss_demo";
+grant select,update,insert,delete,index on system_user_module to "jboss_dev";
+grant select,index on system_user_module to "public";
 
-grant select,update,insert,delete,index on "dba".system_user_section to "jboss_demo" as "dba";
-grant select,update,insert,delete,index on "dba".system_user_section to "jboss_dev" as "dba";
-grant select,index on "dba".system_user_section to "public" as "dba";
+grant select,update,insert,delete,index on system_user_section to "jboss_demo";
+grant select,update,insert,delete,index on system_user_section to "jboss_dev";
+grant select,index on system_user_section to "public";
 
-grant execute on function "dba".ejb_roles (varchar,varchar) to "public" as "dba";
-grant execute on function "dba".getgroups (char,char) to "public" as "dba";
-grant execute on function "dba".getmodules (char,char) to "public" as "dba";
-grant execute on function "dba".getsqls (char,char) to "public" as "dba";
+grant execute on function ejb_roles (varchar,varchar) to "public";
+grant execute on function getgroups (char,char) to "public";
+grant execute on function getmodules (char,char) to "public";
+grant execute on function getsqls (char,char) to "public";
