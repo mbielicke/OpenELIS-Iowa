0acc967e79ddd20699e97daea4041ba9492399d8
akampoow <akampoow@gmail.com>
2015-06-29 16:29:43 +0000

    Fixed the bug where, on Analyte Parameter screen, the error shown on trying to select a unit before selecting a sample type wasn't getting removed on clicking in some other cell. See ticket #944.

--- gwt/src/org/openelis/modules/analyteParameter1/client/AnalyteParameterScreenUI.java
+++ gwt/src/org/openelis/modules/analyteParameter1/client/AnalyteParameterScreenUI.java
@@ -554,11 +554,17 @@ public class AnalyteParameterScreenUI extends Screen {
                             disableAndEnableUnits(sampleTypeId);
                         }
                     }
-                    if (isState(ADD))
-                        setDone(Messages.get().gen_enterInformationPressCommit());
-                    else 
-                        clearStatus();
                 }
+                
+                /*
+                 * this makes sure that if an error is not supposed to be shown
+                 * on clicking a cell, any previously shown error gets removed,
+                 * no matter which cell is clicked
+                 */
+                if (isState(ADD))
+                    setDone(Messages.get().gen_enterInformationPressCommit());
+                else 
+                    clearStatus();
             }
         });
 
