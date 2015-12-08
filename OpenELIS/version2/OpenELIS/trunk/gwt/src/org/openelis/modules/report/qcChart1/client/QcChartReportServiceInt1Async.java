1512570d20cb66b92ae8a38cb3a2eb490f83490e
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-12-08 22:47:07 +0000

    Updated QC Chart to output raw data to Excel if no computations are defined for the specified QC type.

---
+++ OpenELIS/version2/OpenELIS/trunk/gwt/src/org/openelis/modules/report/qcChart1/client/QcChartReportServiceInt1Async.java
@@ -0,0 +1,17 @@
+package org.openelis.modules.report.qcChart1.client;
+
+import org.openelis.domain.QcChartReportViewVO;
+import org.openelis.ui.common.ReportStatus;
+import org.openelis.ui.common.data.Query;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface QcChartReportServiceInt1Async {
+
+    void fetchData(Query query, AsyncCallback<QcChartReportViewVO> callback);
+
+    void recompute(QcChartReportViewVO data, AsyncCallback<QcChartReportViewVO> callback);
+
+    void runReport(QcChartReportViewVO data, AsyncCallback<ReportStatus> callback);
+
+}
