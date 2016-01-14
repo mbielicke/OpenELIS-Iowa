87208b86d03d3869ff9b6d90fc0203f78c138d4e
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2016-01-14 22:30:44 +0000

    Release Date: 2015-12-10. Updated ToDoList screen with Excel export. Distillation worksheet labels. Worksheet printing via PDF templates. Change 'Revision' to 'Corrected Report' on clinical final reports. Bug fixes. Code cleanup.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_3.0.16/portal/src/org/openelis/portal/modules/dataView/server/DataViewServlet.java
@@ -0,0 +1,105 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.portal.modules.dataView.server;
+
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+
+import org.openelis.bean.DataViewReportBean;
+import org.openelis.bean.SessionCacheBean;
+import org.openelis.domain.DataView1VO;
+import org.openelis.domain.IdNameVO;
+import org.openelis.portal.modules.dataView.client.DataViewServiceInt;
+import org.openelis.ui.common.ReportStatus;
+import org.openelis.ui.server.RemoteServlet;
+
+@WebServlet("/openelisweb/dataView")
+public class DataViewServlet extends RemoteServlet implements DataViewServiceInt {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private SessionCacheBean  session;
+
+    @EJB
+    private DataViewReportBean     dataViewReport;
+
+    public ArrayList<IdNameVO> fetchProjectListForPortal() throws Exception {
+        try {
+            return dataViewReport.fetchProjectListForPortal();
+        } catch (Exception anyE) {
+            throw serializeForGWT(anyE);
+        }
+    }
+
+    public DataView1VO fetchAnalyteAndAuxField(DataView1VO data) throws Exception {
+        /*
+         * these flags are set here to make sure that not reportable results or
+         * aux data won't be shown though the portal even if someone manages to
+         * set them in the client
+         */
+        data.setIncludeNotReportableResults("N");
+        data.setIncludeNotReportableAuxData("N");
+        try {
+            return dataViewReport.fetchAnalyteAndAuxFieldForPortal(data);
+        } catch (Exception anyE) {
+            throw serializeForGWT(anyE);
+        }
+    }
+
+    public ReportStatus runReportForPortal(DataView1VO data) throws Exception {
+        ReportStatus st;
+
+        /*
+         * these flags are set here to make sure that not reportable results or
+         * aux data won't be shown through the portal even if someone manages to
+         * set them in the client
+         */
+        data.setIncludeNotReportableResults("N");
+        data.setIncludeNotReportableAuxData("N");
+        try {
+            st = dataViewReport.runReportForPortal(data);
+        } catch (Exception anyE) {
+            throw serializeForGWT(anyE);
+        }
+
+        if (st.getStatus() == ReportStatus.Status.SAVED)
+            getThreadLocalRequest().getSession().setAttribute(st.getMessage(), st);
+
+        return st;
+    }
+
+    @Override
+    public ReportStatus getStatus() throws Exception {
+        try {
+            return (ReportStatus)session.getAttribute("DataViewReportStatus");
+        } catch (Exception anyE) {
+            throw serializeForGWT(anyE);
+        }
+    }
+}
---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_3.0.16/portal/src/org/openelis/portal/modules/main/client/Navigation.java
@@ -0,0 +1,59 @@
+package org.openelis.portal.modules.main.client;
+
+import org.openelis.ui.widget.Button;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.ResizeComposite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class Navigation extends ResizeComposite {
+
+    @UiTemplate("Navigation.ui.xml")
+    interface NavigationUiBinder extends UiBinder<Widget, Navigation> {
+    };
+
+    protected static final NavigationUiBinder uiBinder = GWT.create(NavigationUiBinder.class);
+
+    @UiField
+    Button                                    finalReport, dataView, sampleStatus, testRequest,
+                    emailNotification, /* cases, */logout, changePassword;
+
+    public Navigation() {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    public Button finalReport() {
+        return finalReport;
+    }
+
+    public Button dataView() {
+        return dataView;
+    }
+
+    public Button sampleStatus() {
+        return sampleStatus;
+    }
+
+    public Button testRequest() {
+        return testRequest;
+    }
+
+    public Button emailNotification() {
+        return emailNotification;
+    }
+
+    public Button logout() {
+        return logout;
+    }
+
+    public Button changePassword() {
+        return changePassword;
+    }
+
+    // public Button cases() {
+    // return cases;
+    // }
+}
