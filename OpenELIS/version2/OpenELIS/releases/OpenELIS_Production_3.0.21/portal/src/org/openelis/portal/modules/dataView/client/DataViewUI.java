28132abcffbf87771289399a68e544b23e7b16d4
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2016-06-27 18:36:11 +0000

    Release Date: 2016-06-06. .5 x1 small storage labels printed from worksheet. Added query by "Bill-To" on DataView. Added View TRF option to SampleTracking, Complete/Release, and To-Do List. Added query by organization id to Send-Out order. Added query by and display of test id to Send-Out order. Added "Adding...Complete" message to WorksheetBuilderLookup when adding analyses. Added "Move Top" and "Move Bottom" move row buttons to WorksheetBuilder. Added security feature to require permissions to the "patient" module to view any records which include patient data. Added event logging to internal reports that display patient data as well as the Final Report and Test status modules for external users. Added a check to disallow data values > "2200-01-01 00:00:00" when entering result or aux data values. Added code to map non-printable characters to the "?" character when loading eorders from the HL7 xml. Increased the number of records per page to 500 on the Sample Tracking and Second Data Entry screens. Bug fixes.

---
+++ OpenELIS/version2/OpenELIS/releases/OpenELIS_Production_3.0.21/portal/src/org/openelis/portal/modules/dataView/client/DataViewUI.java
@@ -0,0 +1,194 @@
+package org.openelis.portal.modules.dataView.client;
+
+import org.openelis.ui.widget.Button;
+import org.openelis.ui.widget.CheckBox;
+import org.openelis.ui.widget.MultiDropdown;
+import org.openelis.ui.widget.TextBox;
+import org.openelis.ui.widget.calendar.Calendar;
+import org.openelis.ui.widget.table.Table;
+
+import com.google.gwt.user.client.ui.DeckPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface DataViewUI extends IsWidget {
+
+    public void initialize();
+
+    public TextBox<Integer> getAccessionFrom();
+
+    public TextBox<Integer> getAccessionTo();
+
+    public TextBox<String> getPwsId();
+
+    public TextBox<String> getClientReference();
+
+    public TextBox<String> getEnvCollector();
+
+    public TextBox<String> getSdwisCollector();
+
+    public TextBox<String> getPatientFirst();
+
+    public TextBox<String> getPatientLast();
+
+    public Calendar getCollectedFrom();
+
+    public Calendar getCollectedTo();
+
+    public Calendar getReleasedFrom();
+
+    public Calendar getReleasedTo();
+
+    public Calendar getPatientBirthFrom();
+
+    public Calendar getPatientBirthTo();
+
+    public MultiDropdown<Integer> getProjectCode();
+
+    public CheckBox getAccession();
+
+    public CheckBox getSampleCollected();
+
+    public CheckBox getSampleReceived();
+
+    public CheckBox getSampleReleased();
+
+    public CheckBox getSampleStatus();
+
+    public CheckBox getProjectId();
+
+    public CheckBox getClientReferenceHeader();
+
+    public CheckBox getSampleType();
+
+    public CheckBox getSource();
+
+    public CheckBox getOrganizationName();
+
+    public CheckBox getOrganizationApt();
+
+    public CheckBox getOrganizationAddress();
+
+    public CheckBox getOrganizationCity();
+
+    public CheckBox getOrganizationState();
+
+    public CheckBox getOrganizationZip();
+
+    public CheckBox getAnalysisTest();
+
+    public CheckBox getAnalysisMethod();
+
+    public CheckBox getAnalysisRevision();
+
+    public CheckBox getAnalysisUnit();
+
+    public CheckBox getAnalysisStarted();
+
+    public CheckBox getAnalysisCompleted();
+
+    public CheckBox getAnalysisReleased();
+
+    public CheckBox getAnalysisQA();
+
+    public CheckBox getPatientFirstName();
+
+    public CheckBox getPatientLastName();
+
+    public CheckBox getPatientBirth();
+
+    public CheckBox getPatientGender();
+
+    public CheckBox getPatientRace();
+
+    public CheckBox getPatientEthnicity();
+
+    public CheckBox getPatientPhone();
+
+    public CheckBox getProviderLastName();
+
+    public CheckBox getProviderFirstName();
+
+    public CheckBox getPwsIdHeader();
+
+    public CheckBox getPwsName();
+
+    public CheckBox getSdwisCollectorHeader();
+
+    public CheckBox getSdwisLocation();
+
+    public CheckBox getFacilityId();
+
+    public CheckBox getSdwisSampleType();
+
+    public CheckBox getSampleCategory();
+
+    public CheckBox getSamplePointId();
+
+    public CheckBox getEnvCollectorHeader();
+
+    public CheckBox getEnvLocation();
+
+    public CheckBox getEnvLocationCity();
+
+    public CheckBox getCollectorPhone();
+
+    public CheckBox getSampleDescription();
+
+    public Table getAnalyteTable();
+
+    public Table getAuxTable();
+
+    public Button getContinueButton();
+
+    public Button getResetButton();
+
+    public Button getBackButton();
+
+    public Button getSelectAllSampleFieldsButton();
+
+    public Button getSelectAllOrgFieldsButton();
+
+    public Button getSelectAllAnalysisFieldsButton();
+
+    public Button getSelectAllPatientFieldsButton();
+
+    public Button getSelectAllSdwisFieldsButton();
+
+    public Button getSelectAllEnvironmentalFieldsButton();
+
+    public Button getSelectAllAnalytesButton();
+
+    public Button getUnselectAllAnalytesButton();
+
+    public Button getSelectAllAuxButton();
+
+    public Button getUnselectAllAuxButton();
+
+    public Button getRunReportButton();
+
+    public DeckPanel getDeck();
+
+    public void setCollectedError(String error);
+
+    public void setReleasedError(String error);
+
+    public void setAccessionError(String error);
+
+    public void setClientReferenceError(String error);
+
+    public void setProjectError(String error);
+
+    public void setEnvCollectorError(String error);
+
+    public void setSdwisCollectorError(String error);
+
+    public void setPwsError(String error);
+
+    public void setPatientFirstError(String error);
+
+    public void setPatientLastError(String error);
+
+    public void setPatientBirthError(String error);
+
+    public void clearErrors();
+}
