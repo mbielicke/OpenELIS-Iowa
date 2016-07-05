1f04b7eec46420e1f2f0abaf01948ea9a3897a90
akampoow <akampoow@gmail.com>
2016-07-05 16:11:23 +0000

    Implemented the Animal domain.

---
+++ gwt/src/org/openelis/modules/animalSampleLogin1/client/AnimalSampleLoginEntry.java
@@ -0,0 +1,81 @@
+/**
+ * Exhibit A - UIRF Open-source Based Public Software License.
+ * 
+ * The contents of this file are subject to the UIRF Open-source Based Public
+ * Software License(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * openelis.uhl.uiowa.edu
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * 
+ * The Original Code is OpenELIS code.
+ * 
+ * The Initial Developer of the Original Code is The University of Iowa.
+ * Portions created by The University of Iowa are Copyright 2006-2008. All
+ * Rights Reserved.
+ * 
+ * Contributor(s): ______________________________________.
+ * 
+ * Alternatively, the contents of this file marked "Separately-Licensed" may be
+ * used under the terms of a UIRF Software license ("UIRF Software License"), in
+ * which case the provisions of a UIRF Software License are applicable instead
+ * of those above.
+ */
+package org.openelis.modules.animalSampleLogin1.client;
+
+import static org.openelis.modules.main.client.Logger.remote;
+
+import java.util.logging.Level;
+
+import org.openelis.constants.Messages;
+import org.openelis.modules.main.client.OpenELIS;
+import org.openelis.modules.main.client.ScreenBus;
+import org.openelis.modules.main.client.event.ShowScreenHandler;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
+import com.google.gwt.user.client.Window;
+
+public class AnimalSampleLoginEntry implements EntryPoint, ShowScreenHandler {
+
+    @Override
+    public void onModuleLoad() {
+        ScreenBus.get().addHandler(ScreenBus.SAMPLE_ANIMAL, this);
+    }
+
+    @Override
+    public void showScreen() {
+        GWT.runAsync(new RunAsyncCallback() {
+            public void onSuccess() {
+                try {
+                    addScreen();
+                } catch (Throwable e) {
+                    remote().log(Level.SEVERE, e.getMessage(), e);
+                    Window.alert(e.getMessage());
+                }
+            }
+
+            public void onFailure(Throwable caught) {
+                remote().log(Level.SEVERE, caught.getMessage(), caught);
+                Window.alert(caught.getMessage());
+            }
+        });
+    }
+
+    public AnimalSampleLoginScreenUI addScreen() throws Exception {
+        org.openelis.ui.widget.Window window;
+        AnimalSampleLoginScreenUI screen;
+
+        window = new org.openelis.ui.widget.Window();
+        screen = new AnimalSampleLoginScreenUI(window);
+        window.setName(Messages.get().sampleAnimal_login());
+        window.setSize("770px", "700px");
+        window.setContent(new AnimalSampleLoginScreenUI(window));
+        OpenELIS.getBrowser().addWindow(window, "animalSampleLogin");
+
+        return screen;
+    }
+}
